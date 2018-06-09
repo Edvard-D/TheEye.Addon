3dcbaa24f8e38c161a6c123ba196ce2996ee4dba
EdvardXYZ <eddumansky@gmail.com>
2018-06-09 22:18:51 +0300

    Convert system folders to solution folders
    The solution explorer does not update to reflect the system folders a file is moved, renamed, or deleted.

---
+++ FrameType.lua
@@ -0,0 +1,17 @@
+TheEyeAddon.Frames.FrameType =
+{
+	"Button",
+	"Cooldown",
+	"ColorSelect",
+	"EditBox",
+	"Frame",
+	"GameTooltip",
+	"MessageFrame",
+	"Minimap",
+	"Model",
+	"ScrollFrame",
+	"ScrollingMessage",
+	"SimpleHTML",
+	"Slider",
+	"StatusBar"
+}
