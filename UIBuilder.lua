4f8642224ec848c185e2563fdc4f7a2b5709ad22
EdvardXYZ <eddumansky@gmail.com>
2018-06-10 14:11:49 +0300

    Define Initialize function

--- UIBuilder.lua
+++ UIBuilder.lua
@@ -2,6 +2,7 @@ local TEA = TheEyeAddon
 TEA.UIBuilder = {}
 
 local setmetatable = setmetatable
+local UIParent = UIParent
 
 
 function TEA.UIBuilder:Create(instance)
@@ -12,4 +13,9 @@ function TEA.UIBuilder:Create(instance)
 	self.__index = self
 
 	return instance
+end
+
+function TEA.UIBuilder:Initialize()
+	local mindFlayID = 15407
+	TEA.UIObjects.Icon:Create(nil, UIParent, TEA.UIObjects.IconObjectType.Spell, mindFlayID, false, nil, false, false)
 end
