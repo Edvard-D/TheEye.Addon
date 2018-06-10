99e04a090a23e3211c04e35b8d7681a4db03ae57
EdvardXYZ <eddumansky@gmail.com>
2018-06-10 11:46:31 +0300

    Define Create function

--- FontString.lua
+++ FontString.lua
@@ -1,2 +1,17 @@
 local TEA = TheEyeAddon
 TEA.UIObjects.FontString = {}
+
+
+function TEA.UIObjects.FontString:Create(instance, parentFrame, layer, text)
+	local instance = instance or
+	{
+		parentFrame:CreateFontString(nil, layer)
+	}
+	instance:SetText(text)
+
+
+	setmetatable(instance, TEA.UIObjects.FontString)
+	self.__index = self
+
+	return instance
+end
