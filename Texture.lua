8e0953d936326b15d949b94d25422ed4631dc8bc
EdvardXYZ <eddumansky@gmail.com>
2018-06-10 09:14:36 +0300

    Create and set texture

--- Texture.lua
+++ Texture.lua
@@ -2,11 +2,16 @@ _TEA = TheEyeAddon
 _TEA.UIObjects.Texture = {}
 
 
-function _TEA.UIObjects.Texture:Create(instance, parentFrame)
+function _TEA.UIObjects.Texture:Create(instance, parentFrame, layer, fileID)
 	instance = instance or
 	{
-		frame = TheEyeAddon.UIObjects.FrameBase("Frame", nil, parentFrame)
+		frame = TheEyeAddon.UIObjects.FrameBase("Frame", nil, parentFrame),
+		texture = frame:CreateTexture(nil, layer)
 	}
+
+	instance.texture:SetTexture(fileID)
+
+
 	setmetatable(instance, _TEA.UIObjects.Texture)
 	self.__index = self
 
