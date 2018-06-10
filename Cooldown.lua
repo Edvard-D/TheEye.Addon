11ba2dd6f9867328dc2b12282518277a84b4bd53
EdvardXYZ <eddumansky@gmail.com>
2018-06-10 12:39:27 +0300

    Define Create function

--- Cooldown.lua
+++ Cooldown.lua
@@ -1,2 +1,19 @@
 local TEA = TheEyeAddon
 TEA.UIObjects.Cooldown = {}
+
+local setmetatable = setmetatable
+
+
+function TEA.UIObjects.Cooldown:Create(instance, parentFrame, layer, isReversed)
+	local instance = instance or
+	{
+		TEA.UIObjects.FrameBase:Create(nil, "Cooldown", nil, parentFrame)
+	}
+	instance:SetReverse(isReversed)
+
+
+	setmetatable(instance, TEA.UIObjects.Cooldown)
+	self.__index = self
+
+	return instance
+end
