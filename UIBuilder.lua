e10fda80c013ece97d81a2dc6dfb008aaf3fa589
EdvardXYZ <eddumansky@gmail.com>
2018-06-10 13:58:13 +0300

    Define Create function

--- UIBuilder.lua
+++ UIBuilder.lua
@@ -1,2 +1,15 @@
 local TEA = TheEyeAddon
 TEA.UIBuilder = {}
+
+local setmetatable = setmetatable
+
+
+function TEA.UIBuilder:Create(instance)
+	local instance = instance or {}
+
+
+	setmetatable(instance, TEA.UIBuilder)
+	self.__index = self
+
+	return instance
+end
