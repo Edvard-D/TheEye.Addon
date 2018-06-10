bf4f3c729d103b93600f992c2d65c3cb0695a7be
EdvardXYZ <eddumansky@gmail.com>
2018-06-10 09:26:24 +0300

    Remove frame variable
    Texture:Create should be useable to add a texture to an existing frame, not create a new frame. WoW Texture objects aren't frames.

--- Texture.lua
+++ Texture.lua
@@ -5,8 +5,7 @@ _TEA.UIObjects.Texture = {}
 function _TEA.UIObjects.Texture:Create(instance, parentFrame, layer, fileID)
 	instance = instance or
 	{
-		frame = _TEA.UIObjects.FrameBase:Create("Frame", nil, parentFrame),
-		texture = frame:CreateTexture(nil, layer)
+		texture = parentFrame:CreateTexture(nil, layer)
 	}
 
 	instance.texture:SetTexture(fileID)
