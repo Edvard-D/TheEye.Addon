fcfcd0fab427be33fe9f3a60ca6f03de6066e050
EdvardXYZ <eddumansky@gmail.com>
2018-06-10 10:11:46 +0300

    Remove extra space after function name

--- TypeHelpers.lua
+++ TypeHelpers.lua
@@ -6,7 +6,7 @@ function _TEA.TypeHelpers:Protect(table)
 	return setmetatable ({}, {
 		self:__index = tbl
 		self:__newindex = function (t, key, value)
-			error ("Cannot change " .. tostring(table) ..  " value " .. tostring(key) .. " to " .. tostring(value)".", 2)
+			error("Cannot change " .. tostring(table) ..  " value " .. tostring(key) .. " to " .. tostring(value)".", 2)
 		end
 	})
 end
