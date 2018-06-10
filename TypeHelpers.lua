365edaa53906b21d70388552eb35a6c4a0aadda7
EdvardXYZ <eddumansky@gmail.com>
2018-06-10 10:02:56 +0300

    Define Protect function
    Should  be called by types to prevent them from being changed at runtime.

--- TypeHelpers.lua
+++ TypeHelpers.lua
@@ -1,2 +1,12 @@
 _TEA = TheEyeAddon
 _TEA.TypeHelpers = {}
+
+
+function _TEA.TypeHelpers:Protect(table)
+	return setmetatable ({}, {
+		self:__index = tbl
+		self:__newindex = function (t, key, value)
+			error ("Cannot change " .. tostring(table) ..  " value " .. tostring(key) .. " to " .. tostring(value)".", 2)
+		end
+	})
+end
