345239440a511f9f7d498010f9f79661c3087bb2
EdvardXYZ <eddumansky@gmail.com>
2018-06-10 14:39:13 +0300

    Fix Protect function

--- TypeHelpers.lua
+++ TypeHelpers.lua
@@ -5,10 +5,11 @@ local setmetatable = setmetatable
 
 
 function TEA.TypeHelpers:Protect(table)
-	return setmetatable ({}, {
-		self:__index = tbl
-		self:__newindex = function (t, key, value)
-			error("Cannot change " .. tostring(table) ..  " value " .. tostring(key) .. " to " .. tostring(value)".", 2)
-		end
-	})
+	return setmetatable({}, {
+     __index = table,
+     __newindex = function(table, key, value)
+                    error("Cannot change " .. tostring(table) ..  " value " .. tostring(key) .. " to " .. tostring(value)".", 2)
+                  end,
+     __metatable = false
+   })
 end
