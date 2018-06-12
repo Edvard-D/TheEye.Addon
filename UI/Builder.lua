806103aaeee64e8be458e770ae1e3664381b9db0
EdvardXYZ <eddumansky@gmail.com>
2018-06-12 14:52:06 +0300

    Remove test code

--- UI/Builder.lua
+++ UI/Builder.lua
@@ -5,12 +5,4 @@ local UIParent = UIParent
 
 
 function TEA.UI.Builder:Initialize()
-	local mindFlayID = 15407
-	local mindFlay = TEA.UI.Objects.Factories.Icon:Create(
-		UIParent,
-		50, 50,
-		"CENTER", "CENTER", 0, 0,
-		TEA.UI.Objects.IconObjectType.spell, mindFlayID,
-		true, "TEST", TEA.UI.Fonts.Templates.Icon.Default,
-		false, nil)
 end
