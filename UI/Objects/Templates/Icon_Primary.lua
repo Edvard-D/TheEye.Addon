cf0eabd999072fce7a54c22f805104ac67496ea0
EdvardXYZ <eddumansky@gmail.com>
2018-06-12 21:20:24 +0300

    Assign template values

--- UI/Objects/Templates/Icon_Primary.lua
+++ UI/Objects/Templates/Icon_Primary.lua
@@ -1,2 +1,10 @@
 local TheEyeAddon = TheEyeAddon
-TheEyeAddon.UI.Objects.Templates.Icon_Primary = {}
+TheEyeAddon.UI.Objects.Templates.Icon_Primary =
+{
+    width = 50,
+    height = 50,
+    point = "CENTER",
+    relativePoint = "CENTER",
+    offsetX = 0,
+    offsetY = 0,
+}
