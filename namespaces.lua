25d00ccdd976b69c19df144f6fec0a9f46b00e4f
EdvardXYZ <eddumansky@gmail.com>
2018-06-12 14:48:21 +0300

    Add comment explaining purpose of file

--- namespaces.lua
+++ namespaces.lua
@@ -1,3 +1,5 @@
+-- Used to create tables for folders in the file structure as they
+-- are part of the program hierarchy but don't have a dedicated file.
 TheEyeAddon = {}
     TheEyeAddon.UI = {}
         TheEyeAddon.UI.Fonts = {}
