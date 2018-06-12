0f564872845954d88612dc46d9b2b28d36a25b7b
EdvardXYZ <eddumansky@gmail.com>
2018-06-12 12:24:10 +0300

    Define font value and setter function

--- UI/Fonts.lua
+++ UI/Fonts.lua
@@ -1,2 +1,14 @@
 local TEA = TheEyeAddon
 TEA.UI.Fonts = {}
+
+
+TEA.UI.Fonts.Icon = {}
+TEA.UI.Fonts.Icon.Default =
+{
+    font = "Fonts\\MORPHEUS.TTF",
+    size = 20,
+
+    SetFont = function(fontInstance)
+        fontInstance:SetFont(font, size)
+    end
+}
