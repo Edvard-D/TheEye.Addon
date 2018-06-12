991d32d655670bb9e55d1788081a731a192d63dd
EdvardXYZ <eddumansky@gmail.com>
2018-06-12 14:37:09 +0300

    Simplify cooldown appearance
    A gold edge and bling flash don't fit the aesthetic of TheEye.

--- UI/Objects/Factories/Cooldown.lua
+++ UI/Objects/Factories/Cooldown.lua
@@ -7,6 +7,8 @@ function TEA.UI.Objects.Factories.Cooldown:Create(parentFrame, width, height, is
 		"Cooldown", parentFrame, "CooldownFrameTemplate")
 
 	instance:SetReverse(isReversed)
+	instance:SetDrawBling(false)
+	instance:SetDrawEdge(false)
 
 	return instance
 end
