3d67e144d3694f3b9890134401a6ee1f275a1ee4
EdvardXYZ <eddumansky@gmail.com>
2018-06-12 13:19:13 +0300

    Set attachment points

--- UI/Objects/Factories/FontString.lua
+++ UI/Objects/Factories/FontString.lua
@@ -7,6 +7,7 @@ function TEA.UI.Objects.Factories.FontString:Create(parentFrame, layer, text, fo
 
 	fontTemplate.SetFont(instance)
 	instance:SetText(text)
+	instance:SetPoint("CENTER", parentFrame, "CENTER", 0, 0)
 	
 	return instance
 end
