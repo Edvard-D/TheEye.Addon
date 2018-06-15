d3ed354d8f104731c2018b9ad87462707efe2743
EdvardXYZ <eddumansky@gmail.com>
2018-06-15 07:22:05 +0300

    Define RegisterToEvents function

--- EventHandlers/Helpers.lua
+++ EventHandlers/Helpers.lua
@@ -1,1 +1,8 @@
 local TheEyeAddon = TheEyeAddon
+
+
+local function RegisterToEvents(eventHandler)
+    for k,v in pairs(eventHandler.registerTo) do
+        eventHandler.frame:RegisterEvent(eventHandler.registerTo[k])
+    end
+end
