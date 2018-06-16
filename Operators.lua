1f1626b6081f461c48d58428513d5de7bfb12b8d
EdvardXYZ <eddumansky@gmail.com>
2018-06-16 13:26:26 +0300

    Define comparison operator functions

--- Operators.lua
+++ Operators.lua
@@ -9,3 +9,43 @@ function TheEyeAddon.Operators.EqualTo(value1, value2)
         return false
     end
 end
+
+function TheEyeAddon.Operators.NotEqualTo(value1, value2)
+    if value1 ~= value2 then
+        return true
+    else
+        return false
+    end
+end
+
+function TheEyeAddon.Operators.GreaterThan(value1, value2)
+    if value1 > value2 then
+        return true
+    else
+        return false
+    end
+end
+
+function TheEyeAddon.Operators.LessThan(value1, value2)
+    if value1 < value2 then
+        return true
+    else
+        return false
+    end
+end
+
+function TheEyeAddon.Operators.GreaterThanEqualTo(value1, value2)
+    if value1 >= value2 then
+        return true
+    else
+        return false
+    end
+end
+
+function TheEyeAddon.Operators.LessThanEqualTo(value1, value2)
+    if value1 <= value2 then
+        return true
+    else
+        return false
+    end
+end
