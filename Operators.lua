4c4941a5ec51359236cdda0965b5ad4a2f1bd725
EdvardXYZ <eddumansky@gmail.com>
2018-06-16 09:41:23 +0300

    Define EqualTo function

--- Operators.lua
+++ Operators.lua
@@ -1,2 +1,11 @@
 local TheEyeAddon = TheEyeAddon
 TheEyeAddon.Operators = {}
+
+
+function TheEyeAddon.Operators.EqualTo(value1, value2)
+    if value1 == value2 then
+        return true
+    else
+        return false
+    end
+end
