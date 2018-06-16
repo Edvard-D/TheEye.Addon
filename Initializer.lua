d804c0f03bfb11d8e7479d691750116a562c8c66
EdvardXYZ <eddumansky@gmail.com>
2018-06-16 19:17:51 +0300

    Initialize UI if spec changed to Shadow

--- Initializer.lua
+++ Initializer.lua
@@ -3,13 +3,22 @@ local TheEyeAddon = TheEyeAddon
 
 TheEyeAddon.Initializer = TheEyeAddon.UI.Objects.Factories.Frame:Create("Frame", UIParent, nil, nil)
 TheEyeAddon.Initializer:RegisterEvent("ADDON_LOADED")
+TheEyeAddon.Initializer:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
 
 
-local function OnEvent(event, addonName)
+local function OnEvent(event, arg1)
     local _, class, _ = UnitClass("player")
     local spec = GetSpecialization()
-    if addonName == "TheEye.Addon" and class == "PRIEST" and spec == 3 then
-        TheEyeAddon.UI.Builder:Initialize()
+
+    if class == "PRIEST" then
+        if event == "ACTIVE_TALENT_GROUP_CHANGED" or
+        (event == "ADDON_LOADED" and arg1 == "TheEye.Addon") then
+            if spec == 3 then
+                TheEyeAddon.UI.Builder:Initialize()
+            else
+                --TODO: disable addon
+            end
+        end
     end
 end
 
