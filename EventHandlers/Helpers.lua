c39fa59feef53480d0103457c0abcb48a3dc2e4e
EdvardXYZ <eddumansky@gmail.com>
2018-06-16 12:33:34 +0300

    Change EventHandler data structure
    The previous data structure for EventHandler listeners was:
    
    EventHandler.listeners
    
    The new structure is:
    
    EventHandler.Comparisons.Values.listeners
    
    This will allow for value comparisons to happen once per comparison/
    value combo when there's a relevant WoW event, rather than once for per
    listener, per WoW event.

--- EventHandlers/Helpers.lua
+++ EventHandlers/Helpers.lua
@@ -1,6 +1,9 @@
 local TheEyeAddon = TheEyeAddon
 
 
+local next = next
+
+
 local function RegisterToEvents(eventHandler)
     if eventHandler.frame == nil then
         eventHandler.frame = TheEyeAddon.UI.Objects.Factories.Frame:Create("Frame", nil, UIParent)
@@ -12,9 +15,32 @@ local function RegisterToEvents(eventHandler)
     end
 end
 
+local function GetComparisonValues(eventHandler, comparison)
+    if eventHandler.Comparisons == nil then
+        eventHandler.Comparisons = {}
+    end
+
+    if eventHandler.Comparisons[comparison] == nil then
+        eventHandler.Comparisons[comparison] = {}
+    end
+    return eventHandler.Comparisons[comparison]
+end
+
+local function GetValueListeners(eventHandler, comparison, value)
+    local values = GetComparisonValues(eventHandler, comparison)
+
+    if values[value] == nil then
+        values[value] = {}
+    end
+    return values[value]
+end
+
+
 function TheEyeAddon.EventHandlers:RegisterListener(eventHandler, listener)
-    if table.hasvalue(eventHandler.listeners, listener) == false then
-        table.insert(eventHandler.listeners, listener)
+    local listeners = GetValueListeners(eventHandler, listener.comparison, listener.validValue)
+
+    if table.hasvalue(listeners, listener) == false then
+        table.insert(listeners, listener)
 
         eventHandler.listenerCount = eventHandler.listenerCount + 1
         if eventHandler.listenerCount == 1 then -- If the value was 0 before
@@ -26,7 +52,15 @@ function TheEyeAddon.EventHandlers:RegisterListener(eventHandler, listener)
 end
 
 function TheEyeAddon.EventHandlers:UnregisterListener(eventHandler, listener)
-    table.removevalue(eventHandler.listeners, listener)
+    local listeners = GetValueListeners(eventHandler, listener.comparison, listener.validValue)
+    table.removevalue(listeners, listener)
+    if next(listeners) == nil then -- This value has no listeners
+        local values = GetComparisonValues(eventHandler, listener.comparison)
+        table.removevalue(values, listener.validValue)
+        if next(values) == nil then -- This comparison has no values
+            table.removevalue(eventHandler.Comparisons, listener.comparison)
+        end
+    end
 
     eventHandler.listenerCount = eventHandler.listenerCount - 1
     if eventHandler.listenerCounter == 0 then -- If the value was greater than 0 before
