da985ada6738af984a6eb0c2ba6549a397bee1e4
EdvardXYZ <eddumansky@gmail.com>
2018-06-16 17:32:37 +0300

    Initialize addon on event
    Saved variables defined in the .toc file are not available until the
    ADDON_LOADED event fires.

--- Initializer.lua
+++ Initializer.lua
@@ -1,4 +1,15 @@
 local TheEyeAddon = TheEyeAddon
-TheEyeAddon.Initializer = TheEyeAddon.UI.Objects.Factories.Frame:Create("Frame", UIParent, nil, nil)
 
-TheEyeAddon.UI.Builder:Initialize()
+
+TheEyeAddon.Initializer = TheEyeAddon.UI.Objects.Factories.Frame:Create("Frame", UIParent, nil, nil)
+TheEyeAddon.Initializer:RegisterEvent("ADDON_LOADED")
+
+
+local function OnAddonLoaded(event, addonName)
+    if addonName == "TheEye.Addon" then
+        TheEyeAddon.UI.Builder:Initialize()
+    end
+end
+
+
+TheEyeAddon.Initializer:SetScript("OnEvent", OnAddonLoaded)
