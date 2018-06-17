5786981caef03ebe26d77441a6172bde490b3956
EdvardXYZ <eddumansky@gmail.com>
2018-06-17 14:51:30 +0300

    Define SetDimensions function
    Having the dimension setting code separate from the Create function
    will allow dimension resetting by pools.

--- UI/Objects/Factories/Frame.lua
+++ UI/Objects/Factories/Frame.lua
@@ -6,7 +6,11 @@ local CreateFrame = CreateFrame
 
 function TheEyeAddon.UI.Objects.Factories.Frame:Create(frameType, parentFrame, inheritsFrom, dimensionTemplate)
 	local instance = CreateFrame(frameType, nil, parentFrame, inheritsFrom)
+	TheEyeAddon.UI.Objects.Factories.Frame:SetDimensions(instance, dimensionTemplate)
+	return instance
+end
 
+function TheEyeAddon.UI.Objects.Factories.Frame:SetDimensions(instance, dimensionTemplate)
 	if dimensionTemplate ~= nil then
 		instance:SetWidth(dimensionTemplate.width)
 		instance:SetHeight(dimensionTemplate.height)
@@ -19,6 +23,4 @@ function TheEyeAddon.UI.Objects.Factories.Frame:Create(frameType, parentFrame, i
 	else
 		instance:SetAllPoints()
 	end
-
-	return instance
 end
