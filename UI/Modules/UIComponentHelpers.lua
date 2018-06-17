b49277191e10656c296d5bb6ec014a0a541bdbce
EdvardXYZ <eddumansky@gmail.com>
2018-06-17 12:00:15 +0300

    Move OnStateChange to Components

--- UI/Modules/UIComponentHelpers.lua
+++ UI/Modules/UIComponentHelpers.lua
@@ -18,3 +18,26 @@ end
 function TheEyeAddon.UI.Modules.Components:SetupComponent(module, component)
     SetupStateGroup(module, component, component.StateGroups.Enabled)
 end
+
+function TheEyeAddon.UI.Modules.Components:OnStateChange(stateListener, newState)
+    local stateGroup = stateListener.stateGroup
+    local previousState = stateGroup.currentState
+    
+    if newState == stateListener.validValue then
+        stateGroup.stateKey = stateGroup.stateKey + stateListener.stateValue
+    else
+        stateGroup.stateKey = stateGroup.stateKey - stateListener.stateValue
+    end
+
+    if stateGroup.validKeys[stateGroup.stateKey] ~= nil then
+        stateGroup.currentState = true
+        if previousState == false or previousState == nil then
+            stateGroup:OnValidKey()
+        end
+    else
+        stateGroup.currentState = false
+        if previousState == true or previousState == nil then
+            stateGroup:OnInvalidKey()
+        end
+    end
+end
