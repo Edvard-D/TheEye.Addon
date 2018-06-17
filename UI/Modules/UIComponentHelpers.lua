d992c7523ea55b60e6b1b66d575d55556a1e3e25
EdvardXYZ <eddumansky@gmail.com>
2018-06-17 11:43:09 +0300

    Store reference to listener's parent module

--- UI/Modules/UIComponentHelpers.lua
+++ UI/Modules/UIComponentHelpers.lua
@@ -2,19 +2,20 @@ local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Modules.UIComponentHelpers = {}
 
 
-local function SetupListener(component, stateGroup, listener, eventHandlerName)
+local function SetupListener(module, component, stateGroup, listener, eventHandlerName)
+    listener.module = module
     listener.component = component
     listener.stateGroup = enabledGroup
     TheEyeAddon.EventHandlers:RegisterListener(eventHandlerName, listener)
 end
 
-local function SetupStateGroup(component, stateGroup)
+local function SetupStateGroup(module, component, stateGroup)
     for eventHandlerName,v in pairs(enabledGroup.StateListeners) do
         local listener = enabledGroup.StateListeners[eventHandlerName]
-        SetupListener(component, stateGroup, listener, eventHandlerName)
+        SetupListener(module, component, stateGroup, listener, eventHandlerName)
     end
 end
 
 function TheEyeAddon.UI.Modules.UIComponentHelpers:SetupComponent(module, component)
-    SetupStateGroup(component, component.StateGroups.Enabled)
+    SetupStateGroup(module, component, component.StateGroups.Enabled)
 end
