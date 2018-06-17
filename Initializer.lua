33dbdda8d0f19892f4bbc96f471ed893464353e6
EdvardXYZ <eddumansky@gmail.com>
2018-06-17 20:50:19 +0300

    Correct Initializer function call and value comparison

--- Initializer.lua
+++ Initializer.lua
@@ -1,4 +1,5 @@
 local TheEyeAddon = TheEyeAddon
+TheEyeAddon.Initializer = {}
 
 
 TheEyeAddon.UI.ParentFrame = TheEyeAddon.UI.Factories.Frame:Create("Frame", UIParent, nil, nil)
@@ -6,14 +7,14 @@ TheEyeAddon.UI.ParentFrame:RegisterEvent("ADDON_LOADED")
 TheEyeAddon.UI.ParentFrame:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
 
 
-local function OnEvent(event, arg1)
+function TheEyeAddon.Initializer:OnEvent(event, arg1)
     local _, class, _ = UnitClass("player")
     local previousSpec = TheEyeAddon.CurrentSpec
     TheEyeAddon.CurrentSpec = GetSpecialization()
 
     if class == "PRIEST" then
         if event == "ACTIVE_TALENT_GROUP_CHANGED" or
-        (event == "ADDON_LOADED" and arg1 == "TheEye.Addon") then
+        (event == "ADDON_LOADED" and arg1 == "TheEyeAddon") then
             if TheEyeAddon.CurrentSpec == 3 then
                 TheEyeAddon.UI.Modules:Setup()
             elseif previousSpec == 3 then
@@ -24,4 +25,4 @@ local function OnEvent(event, arg1)
 end
 
 
-TheEyeAddon.UI.ParentFrame:SetScript("OnEvent", OnEvent)
+TheEyeAddon.UI.ParentFrame:SetScript("OnEvent", TheEyeAddon.Initializer.OnEvent)
