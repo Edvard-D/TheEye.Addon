1326087035fa844a1d407ea589332e89d6e4eb08
EdvardXYZ <eddumansky@gmail.com>
2018-06-17 21:29:05 +0300

    Correct for loop declaration

--- TableHelpers.lua
+++ TableHelpers.lua
@@ -2,7 +2,7 @@ local table = table
 
 function table.hasvalue(tab, value)
     if tab ~= nil then
-        for k,v in tab do
+        for k,v in pairs(tab) do
             if v == value then
                 return true
             end
@@ -15,7 +15,7 @@ end
 function table.removevalue(tab, value)
     if tab ~= nil then
         local count = 0
-        for k,v in tab do
+        for k,v in pairs(tab) do
             count = count + 1
             if v == value then
                 table.remove(tab, count)
