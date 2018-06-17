fa58ac4cc0a2e4956dcd761fe5a96522004b25bc
EdvardXYZ <eddumansky@gmail.com>
2018-06-17 11:40:16 +0300

    Define SetupListener function

--- UI/Modules/UIComponentHelpers.lua
+++ UI/Modules/UIComponentHelpers.lua
@@ -1,2 +1,9 @@
 local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Modules.UIComponentHelpers = {}
+
+
+local function SetupListener(component, stateGroup, listener, eventHandlerName)
+    listener.component = component
+    listener.stateGroup = enabledGroup
+    TheEyeAddon.EventHandlers:RegisterListener(eventHandlerName, listener)
+end
