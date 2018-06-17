56833eccd0ce046a5ea4c92521d3bfa081c426d3
EdvardXYZ <eddumansky@gmail.com>
2018-06-17 17:14:35 +0300

    Call Modules:Teardown

--- Initializer.lua
+++ Initializer.lua
@@ -8,15 +8,16 @@ TheEyeAddon.UI.ParentFrame:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
 
 local function OnEvent(event, arg1)
     local _, class, _ = UnitClass("player")
-    local spec = GetSpecialization()
+    local previousSpec = TheEyeAddon.CurrentSpec
+    TheEyeAddon.CurrentSpec = GetSpecialization()
 
     if class == "PRIEST" then
         if event == "ACTIVE_TALENT_GROUP_CHANGED" or
         (event == "ADDON_LOADED" and arg1 == "TheEye.Addon") then
-            if spec == 3 then
+            if TheEyeAddon.CurrentSpec == 3 then
                 TheEyeAddon.UI.Modules:Setup()
-            else
-                --TODO: disable addon
+            elseif previousSpec == 3 then
+                TheEyeAddon.UI.Modules:Teardown()
             end
         end
     end
