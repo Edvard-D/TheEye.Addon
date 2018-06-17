f7d1bb2ee9e216c4292bad03d576f1aae5b4fc31
EdvardXYZ <eddumansky@gmail.com>
2018-06-17 22:26:14 +0300

    Define Coordinator:HandleEvent method

--- Events/Coordinator.lua
+++ Events/Coordinator.lua
@@ -1,2 +1,10 @@
 local TheEyeAddon = TheEyeAddon
-TheEyeAddon.Events.Coordinator = {}
+TheEyeAddon.Events.Coordinator = { Handlers = {} }
+local Handlers = TheEyeAddon.Events.Coordinator.Handlers
+
+
+function TheEyeAddon.Events.Coordinator:HandleEvent(event)
+    for i,handler in ipairs(Handlers.event) do
+        TheEyeAddon.Events.Handlers:EvaluateState(handler, event)
+    end
+end
