7f399789776460f9a8ec83289ae4ead0472f266c
EdvardXYZ <eddumansky@gmail.com>
2018-06-17 21:02:33 +0300

    Populate Primary module values

--- UI/Modules/Primary.lua
+++ UI/Modules/Primary.lua
@@ -1,2 +1,19 @@
 local TheEyeAddon = TheEyeAddon
-TheEyeAddon.UI.Modules.Primary = {}
+
+
+TheEyeAddon.UI.Modules.Instances.Primary =
+{
+    tags = { "PRIMARY" },
+    sortType = nil, --TheEyeAddon.UI.SortType.prioritySort,
+    OnComponentVisibleChanged = function() end, --TODO
+    dimensionTemplate =
+    {
+        width = 0,
+        height = 0,
+        point = "TOP",
+        relativePoint = "CENTER",
+        offsetX = 0,
+        offsetY = -50,
+    },
+    Components = { }
+}
