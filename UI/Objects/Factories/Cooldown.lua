c92af53946d95d163b34bbaf922f3475d473037f
EdvardXYZ <eddumansky@gmail.com>
2018-06-17 16:10:15 +0300

    Implement Cooldown pool functionality

--- UI/Objects/Factories/Cooldown.lua
+++ UI/Objects/Factories/Cooldown.lua
@@ -1,10 +1,28 @@
 local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Objects.Factories.Cooldown = {}
 
+local Pool = {}
 
-function TheEyeAddon.UI.Objects.Factories.Cooldown:Create(parentFrame, dimensionTable)
-	local instance = TheEyeAddon.UI.Objects.Factories.Frame:Create(
-		"Cooldown", parentFrame, "CooldownFrameTemplate", dimensionTable)
+
+function TheEyeAddon.UI.Objects.Factories.Cooldown:Claim(parentFrame, displayData)
+	local instance = nil
+	for i,frame in ipairs(Pool) do
+		if frame.isClaimed == false then
+			instance = frame
+			break
+		end
+	end
+
+	if instance ~= nil then
+		instance:SetParent(parentFrame)
+		TheEyeAddon.UI.Objects.Factories.Frame:SetDimensions(instance, displayData.dimensionTable)
+	else
+		local instance = TheEyeAddon.UI.Objects.Factories.Frame:Create(
+			"Cooldown", parentFrame, "CooldownFrameTemplate", displayData.dimensionTable)
+	end
+
+
+	instance.isClaimed = true
 
 	instance:SetDrawBling(false)
 	instance:SetDrawEdge(false)
