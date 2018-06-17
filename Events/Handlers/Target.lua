e4a8216215173f8762adc196b3c248b3c90b8e38
EdvardXYZ <eddumansky@gmail.com>
2018-06-17 22:35:05 +0300

    Removes Target:HandleEvent assignment

--- Events/Handlers/Target.lua
+++ Events/Handlers/Target.lua
@@ -8,9 +8,5 @@ TheEyeAddon.Events.Handlers.Target_Attackable =
     registerTo = { PLAYER_REGEN_DISABLED, PLAYER_TARGET_CHANGED },
     Evaluate = function()
         UnitCanAttack("player", "target")
-    end,
-    HandleEvent = function(event)
-        print(event)
-        self:Evaluate()
     end
 }
