1d60b8f0d017964b75e92a9e3b97cfb26a60f6da
EdvardXYZ <eddumansky@gmail.com>
2018-06-17 12:02:09 +0300

    Define SetupModule function

--- UI/Modules/UIModulesHelpers.lua
+++ UI/Modules/UIModulesHelpers.lua
@@ -1,1 +1,10 @@
 local TheEyeAddon = TheEyeAddon
+
+
+local function SetupModule(module)
+    module.frame = TheEyeAddon.UI.Objects.Factories.Frame:Create("Frame", TheEyeAddon.UI.ParentFrame, nil, module.dimensionTemplate)
+
+    for k,v in pairs(module.Components) do
+        TheEyeAddon.UI.Modules.Components:SetupComponent(module, module.Components[k])
+    end
+end
