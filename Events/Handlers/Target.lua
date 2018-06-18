c07ab59df8f591616e29d0a1abfcfb1c748e796f
EdvardXYZ <eddumansky@gmail.com>
2018-06-18 22:29:12 +0300

    Remove unnecessary event registration

--- Events/Handlers/Target.lua
+++ Events/Handlers/Target.lua
@@ -5,7 +5,7 @@ local UnitCanAttack = UnitCanAttack
 
 TheEyeAddon.Events.Handlers.Target_Attackable =
 {
-    registerTo = { "PLAYER_REGEN_DISABLED", "PLAYER_TARGET_CHANGED" },
+    registerTo = { "PLAYER_TARGET_CHANGED" },
     Evaluate = function()
         return UnitCanAttack("player", "target")
     end
