8e8826e0cbb124420a99780f903b6860dccd9c5e
EdvardXYZ <eddumansky@gmail.com>
2018-06-18 22:26:51 +0300

    Correct Target_Attackable table values

--- Events/Handlers/Target.lua
+++ Events/Handlers/Target.lua
@@ -5,8 +5,8 @@ local UnitCanAttack = UnitCanAttack
 
 TheEyeAddon.Events.Handlers.Target_Attackable =
 {
-    registerTo = { PLAYER_REGEN_DISABLED, PLAYER_TARGET_CHANGED },
+    registerTo = { "PLAYER_REGEN_DISABLED", "PLAYER_TARGET_CHANGED" },
     Evaluate = function()
-        UnitCanAttack("player", "target")
+        return UnitCanAttack("player", "target")
     end
 }
