6f95aadbc6831b4c1106b56b8d141cb379835992
EdvardXYZ <eddumansky@gmail.com>
2018-06-18 21:33:55 +0300

    Correct how Handlers are stored and referenced
    The previous code was creating a table by the name of eventName, where
    the intention is for it to get the value that matched eventName.

--- Events/Coordinator.lua
+++ Events/Coordinator.lua
@@ -5,7 +5,7 @@ local Handlers = TheEyeAddon.Events.Coordinator.Handlers
 
 local frame = CreateFrame("Frame", nil, UIParent)
 local function HandleEvent(self, eventName)
-    for i,handler in ipairs(Handlers.eventName) do
+    for i,handler in ipairs(Handlers[eventName]) do
         TheEyeAddon.Events.Handlers:EvaluateState(handler, eventName)
     end
 end
@@ -14,30 +14,30 @@ frame:SetScript("OnEvent", HandleEvent)
 
 function TheEyeAddon.Events.Coordinator:RegisterHandler(handler)
     for i,eventName in ipairs(handler.registerTo) do
-        if Handlers.eventName == nil then
-            Handlers.eventName = { handler }
+        if Handlers[eventName] == nil then
+            Handlers[eventName] = { handler }
             frame:RegisterEvent(eventName)
         else
-            table.insert(Handlers.eventName, handler)
+            table.insert(Handlers[eventName], handler)
         end
 
-        if Handlers.eventName.handlerCount == nil then
-            Handlers.eventName.handlerCount = 0
+        if Handlers[eventName].handlerCount == nil then
+            Handlers[eventName].handlerCount = 0
         end
-        Handlers.eventName.handlerCount = Handlers.eventName.handlerCount + 1
+        Handlers[eventName].handlerCount = Handlers[eventName].handlerCount + 1
     end
 end
 
 function TheEyeAddon.Events.Coordinator:UnregisterHandler(handler)
     for i,eventName in ipairs(handler.registerTo) do
-        table.removevalue(Handlers.eventName, handler)
+        table.removevalue(Handlers[eventName], handler)
         
-        Handlers.eventName.handlerCount = Handlers.eventName.handlerCount - 1
-        if Handlers.eventName.handlerCount == 0 then
+        Handlers[eventName].handlerCount = Handlers[eventName].handlerCount - 1
+        if Handlers[eventName].handlerCount == 0 then
             frame:UnregisterEvent(eventName)
-        elseif Handlers.eventName.handlerCount < 0 then
+        elseif Handlers[eventName].handlerCount < 0 then
             error("Registered handlers set to " ..
-                tostring(Handlers.eventName.handlerCount) ..
+                tostring(Handlers[eventName].handlerCount) ..
                 " but should never be below 0.")
         end
     end
