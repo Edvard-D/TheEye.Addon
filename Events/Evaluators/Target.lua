c472cac1fa4391c503219fb6a216b68039e977d9
EdvardXYZ <eddumansky@gmail.com>
2018-06-23 13:33:27 +0300

    Separate registerTo event values into two groups

--- Events/Evaluators/Target.lua
+++ Events/Evaluators/Target.lua
@@ -5,7 +5,7 @@ local UnitCanAttack = UnitCanAttack
 
 TheEyeAddon.Events.Evaluators.Target_Attackable =
 {
-    registerTo = { "PLAYER_TARGET_CHANGED" },
+    gameEvents = { "PLAYER_TARGET_CHANGED" },
     Evaluate = function()
         return "default", UnitCanAttack("player", "target")
     end
