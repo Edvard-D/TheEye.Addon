2136e8c08f3a205572c5be3b2a7c08582e8fce81
EdvardXYZ <eddumansky@gmail.com>
2018-06-23 14:29:05 +0300

    Assign Unit_Spellcast_ElapsedTime evaluator

--- Events/Evaluators/UnitSpellcast.lua
+++ Events/Evaluators/UnitSpellcast.lua
@@ -17,3 +17,21 @@ TheEyeAddon.Events.Evaluators.Unit_Spellcast_Active =
         end
     end
 }
+
+TheEyeAddon.Events.Evaluators.Unit_Spellcast_ElapsedTime =
+{
+    gameEvents = { "UNIT_SPELLCAST_START", "UNIT_SPELLCAST_STOP" },
+    customEvents = { "THEEYE_UNIT_CAST_TIMER" },
+    Evaluate = function(event)
+        local unit = select(1, event)
+        local spellID = select(5, event)
+        
+        if event == "UNIT_SPELLCAST_START" then
+            return select(5, event)
+        elseif event == "UNIT_SPELLCAST_" then
+            --TODO
+        else
+            return nil
+        end
+    end
+}
