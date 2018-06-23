d7c3e763718231426fc3619e7bd004a317294bda
EdvardXYZ <eddumansky@gmail.com>
2018-06-23 14:28:43 +0300

    Assign Unit_Spellcast_Active evaluator

--- Events/Evaluators/UnitSpellcast.lua
+++ Events/Evaluators/UnitSpellcast.lua
@@ -1,1 +1,19 @@
 local TheEyeAddon = TheEyeAddon
+
+local select = select
+
+
+TheEyeAddon.Events.Evaluators.Unit_Spellcast_Active =
+{
+    gameEvents = { "UNIT_SPELLCAST_START", "UNIT_SPELLCAST_STOP" },
+    Evaluate = function(event)
+        local unit = select(1, event)
+        local spellID = select(5, event)
+        
+        if event == "UNIT_SPELLCAST_START" then
+            return table.concat({ unit, spellID }), true
+        else
+            return table.concat({ unit, spellID }), false
+        end
+    end
+}
