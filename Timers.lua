3f7252ee20ca6a330e6bd1af997118d7514a4ac8
EdvardXYZ <eddumansky@gmail.com>
2018-06-24 19:23:05 +0300

    Correct how values passed by StartEventTimer After call

--- Timers.lua
+++ Timers.lua
@@ -2,11 +2,13 @@ local TheEyeAddon = TheEyeAddon
 TheEyeAddon.Timers = {}
 
 local After = C_Timer.After
+local unpack = unpack
 
 
 function TheEyeAddon.Timers:StartEventTimer(duration, eventName, ...)
+    local args = {duration, ...}
     After(duration, 
     function(...)
-        TheEyeAddon.Events.Coordinator:SendCustomEvent(eventName, ...)
+        TheEyeAddon.Events.Coordinator:SendCustomEvent(eventName, unpack(args))
     end)
 end
