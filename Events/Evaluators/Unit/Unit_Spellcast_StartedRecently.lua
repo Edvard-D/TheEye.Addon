76656d9dd60a14cb267796d40b89c15e99a53270
EdvardXYZ <eddumansky@gmail.com>
2018-06-24 13:13:26 +0300

    Separate evaluators into individual files
    Some evaluators are complex enough that placing them in their own file
    will make it easier to work with them. The namespace wasn't changed as
    the full name (including Target/Unit) is needed to use it as a key.

--- Events/Evaluators/Unit/Unit_Spellcast_StartedRecently.lua
+++ Events/Evaluators/Unit/Unit_Spellcast_StartedRecently.lua
@@ -4,37 +4,6 @@ local select = select
 
 
 -- inputValues = { --[[unit]] "", --[[spellID]] 0 }
-TheEyeAddon.Events.Evaluators.Unit_Spellcast_Active =
-{
-    gameEvents =
-    {
-        "UNIT_SPELLCAST_CHANNEL_START",
-        "UNIT_SPELLCAST_CHANNEL_STOP",
-        "UNIT_SPELLCAST_START",
-        "UNIT_SPELLCAST_STOP"
-    },
-    Evaluate = function(self, event, ...)
-        local unit, _, spellID = ...
-        
-        if event == "UNIT_SPELLCAST_START" or "UNIT_SPELLCAST_CHANNEL_START" then
-            return table.concat({ unit, spellID }), true
-        else -- UNIT_SPELLCAST_STOP / UNIT_SPELLCAST_CHANNEL_STOP
-            return table.concat({ unit, spellID }), false
-        end
-    end,
-    SetInitialState = function(self, valueGroup, inputValues)
-        local expectedSpellID = inputValues[2]
-        local currentSpellID = select(9, UnitCastingInfo(inputValues[1]))
-
-        if currentSpellID == expectedSpellID then
-            valueGroup.currentState = true
-        else
-            valueGroup.currentState = false
-        end
-    end
-}
-
--- inputValues = { --[[unit]] "", --[[spellID]] 0 }
 TheEyeAddon.Events.Evaluators.Unit_Spellcast_StartedRecently =
 {
     gameEvents =
