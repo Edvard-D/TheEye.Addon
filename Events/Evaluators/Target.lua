9cb506f066dfc06bc657d713ee1e0d0691f7f69f
EdvardXYZ <eddumansky@gmail.com>
2018-06-24 12:58:27 +0300

    Move gameEvents table value into individual line
    This is easier to read when there are a large number of events.

--- Events/Evaluators/Target.lua
+++ Events/Evaluators/Target.lua
@@ -6,7 +6,10 @@ local UnitCanAttack = UnitCanAttack
 -- inputValues = nil
 TheEyeAddon.Events.Evaluators.Target_Attackable =
 {
-    gameEvents = { "PLAYER_TARGET_CHANGED" },
+    gameEvents =
+    {
+        "PLAYER_TARGET_CHANGED"
+    },
     Evaluate = function()
         return "default", UnitCanAttack("player", "target")
     end,
