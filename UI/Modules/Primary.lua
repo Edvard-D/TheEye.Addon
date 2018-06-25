60174d69f318e0646d85ff7baf602a1c72cd2fde
EdvardXYZ <eddumansky@gmail.com>
2018-06-25 13:11:06 +0300

    Place module dimensionTemplate inside DisplayData wrapper
    This is to mimic the structure of components.

--- UI/Modules/Primary.lua
+++ UI/Modules/Primary.lua
@@ -6,14 +6,17 @@ TheEyeAddon.UI.Modules.Primary =
     tags = { "PRIMARY" },
     sortType = nil, --TheEyeAddon.UI.SortType.prioritySort,
     OnComponentVisibleChanged = function() end, --TODO
-    dimensionTemplate =
+    DisplayData =
     {
-        width = 0,
-        height = 0,
-        point = "TOP",
-        relativePoint = "CENTER",
-        offsetX = 0,
-        offsetY = -50,
-    },
+        dimensionTemplate =
+        {
+            width = 0,
+            height = 0,
+            point = "TOP",
+            relativePoint = "CENTER",
+            offsetX = 0,
+            offsetY = -50,
+        },
+    },    
     Components = { }
 }
