a62748276aaa46d5788e29dd6fc77f45308fe53b
EdvardXYZ <eddumansky@gmail.com>
2018-06-25 14:09:05 +0300

    Define Objects:Add function
    This is to be used by UI objects when their file is initially loaded.

--- UI/UIObjects.lua
+++ UI/UIObjects.lua
@@ -1,2 +1,10 @@
 local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Objects = {}
+
+local table = table
+local unpack = unpack
+
+
+function TheEyeAddon.UI.Objects:Add(uiObject)
+    TheEyeAddon.UI.Objects[table.concat(unpack(uiObject.tags))] = uiObject
+end
