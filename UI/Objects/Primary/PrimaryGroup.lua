6cb700e46601b963f2e6b90ed116aff4cfe25149
EdvardXYZ <eddumansky@gmail.com>
2018-06-25 16:49:31 +0300

    Create StateGroups for PrimaryGroup UIObject

--- UI/Objects/Primary/PrimaryGroup.lua
+++ UI/Objects/Primary/PrimaryGroup.lua
@@ -12,7 +12,37 @@ TheEyeAddon.UI.Objects:Add(
             relativePoint = "CENTER",
             offsetX = 0,
             offsetY = -50,
+        }
+    },
+    StateGroups =
+    {
+        Enabled =
+        {
+            OnValidKey = TheEyeAddon.UI.Objects.Enable,
+            OnInvalidKey = TheEyeAddon.UI.Objects.Disable,
+            validKeys = { [2] = true },
+            StateListeners =
+            {
+                Setting_Module_Enabled =
+                {
+                    keyValue = 2,
+                    inputValues = { "GROUP_HUD_MODULE_PRIMARY" }
+                }
+            }
         },
+        Visible =
+        {
+            OnValidKey = TheEyeAddon.UI.Objects.Show,
+            OnInvalidKey = TheEyeAddon.UI.Objects.Hide,
+            validKeys = { [2] = true },
+            StateListeners =
+            {
+                Target_Attackable =
+                {
+                    keyValue = 2
+                }
+            }
+        }
     }
 }
 )
