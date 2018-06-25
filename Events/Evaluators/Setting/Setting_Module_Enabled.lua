6067f66ff54e829ae0c5bcf7b73ae90f345ddf06
EdvardXYZ <eddumansky@gmail.com>
2018-06-25 16:35:23 +0300

    Define Setting_Module_Enabled evaluator

--- Events/Evaluators/Setting/Setting_Module_Enabled.lua
+++ Events/Evaluators/Setting/Setting_Module_Enabled.lua
@@ -1,1 +1,31 @@
 local TheEyeAddon = TheEyeAddon
+
+local select = select
+local table = table
+
+
+-- inputValues = { --[[moduleKey]] "" }
+
+TheEyeAddon.Events.Evaluators.Setting_Module_Enabled =
+{
+    customEvents =
+    {
+        "THEEYE_SETTING_CHANGED"
+    }
+}
+
+function TheEyeAddon.Events.Evaluators.Setting_Module_Enabled:SetInitialState(valueGroup, inputValues)
+    if TheEyeAddon.Settings == nil or table.hasvalue(TheEyeAddon.Settings.DisabledUIModules, inputValues[1]) == false then
+        valueGroup.currentState = true
+    else
+        valueGroup.currentState = false
+    end
+end
+
+function TheEyeAddon.Events.Evaluators.Setting_Module_Enabled:GetKey(event, ...)
+    return select(1, ...) -- moduleKey
+end
+
+function TheEyeAddon.Events.Evaluators.Setting_Module_Enabled:Evaluate(event)
+    return select(2, ...) -- enabledState
+end
