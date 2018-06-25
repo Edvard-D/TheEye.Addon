cd437a5c16dd743e97b57f62f4d4a42e1967015b
EdvardXYZ <eddumansky@gmail.com>
2018-06-25 15:02:09 +0300

    Create UIObject_Enabled.lua file and setup evaluator

---
+++ Events/Evaluators/UIObject/UIObject_Enabled.lua
@@ -0,0 +1,36 @@
+local TheEyeAddon = TheEyeAddon
+
+
+-- inputValues = { --[[tags]] , }
+
+ TheEyeAddon.Events.Evaluators.UIObject_Enabled =
+{
+    customEvents =
+    {
+        "THEEYE_UIOBJECT_ENABLED",
+        "THEEYE_UIOBJECT_DISABLED"
+    }
+}
+
+function TheEyeAddon.Events.Evaluators.UIObject_Enabled:SetInitialState(valueGroup, inputValues)
+    local uiObject = TheEyeAddon.UI.Objects[table.concat(iputValues, "-")]
+
+    if uiObject == nil then
+        valueGroup.currentState = false
+    else
+        valueGroup.currentState = uiObject.StateGroups.Enabled.currentState
+    end
+end
+
+function TheEyeAddon.Events.Evaluators.UIObject_Enabled:GetKey(event, ...)
+    local uiObject = select(1, ...)
+    return uiObject.key
+end
+
+function TheEyeAddon.Events.Evaluators.UIObject_Enabled:Evaluate(event)
+    if event == "THEEYE_UIOBJECT_ENABLED" then
+        return true
+    else -- THEEYE_UIOBJECT_DISABLED
+        return false
+    end
+end
