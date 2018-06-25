3f4aad601359bacc9cde0d3e90ccf80b05e7f65d
EdvardXYZ <eddumansky@gmail.com>
2018-06-25 17:37:15 +0300

    Create Player_Spec file and evaluator

---
+++ Events/Evaluators/Player/Player_Spec.lua
@@ -0,0 +1,28 @@
+local TheEyeAddon = TheEyeAddon
+
+local GetSpecialization = GetSpecialization
+local select = select
+local table = table
+
+
+-- inputValues = { --[[specNumber]] 0 }
+
+TheEyeAddon.Events.Evaluators.Player_Spec =
+{
+    gameEvents =
+    {
+        "ACTIVE_TALENT_GROUP_CHANGED"
+    }
+}
+
+function TheEyeAddon.Events.Evaluators.Player_Spec:SetInitialState(valueGroup, inputValues)
+    return inputValues[1] == GetSpecialization()
+end
+
+function TheEyeAddon.Events.Evaluators.Player_Spec:GetKey(event, ...)
+    return table.concat({ "_", select(1, ...) })
+end
+
+function TheEyeAddon.Events.Evaluators.Player_Spec:Evaluate(event, ...)
+    return true
+end
