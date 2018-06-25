95cc537b6421fa0df5c67aad2e5ce2d84530002d
EdvardXYZ <eddumansky@gmail.com>
2018-06-25 17:18:57 +0300

    Create Addon_Loaded file and evaluator

---
+++ Events/Evaluators/General/Addon_Loaded.lua
@@ -0,0 +1,27 @@
+local TheEyeAddon = TheEyeAddon
+
+local select = select
+local table = table
+
+
+-- inputValues = { --[[addonName]] "" }
+
+TheEyeAddon.Events.Evaluators.Addon_Loaded =
+{
+    gameEvents =
+    {
+        "ADDON_LOADED"
+    }
+}
+
+function TheEyeAddon.Events.Evaluators.Addon_Loaded:SetInitialState(valueGroup, inputValues)
+    valueGroup.currentState = false
+end
+
+function TheEyeAddon.Events.Evaluators.Addon_Loaded:GetKey(event, ...)
+    return select(1, ...)
+end
+
+function TheEyeAddon.Events.Evaluators.Addon_Loaded:Evaluate(event, ...)
+    return true
+end
