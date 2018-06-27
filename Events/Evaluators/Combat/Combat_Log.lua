df4ca437797f952ade03c4b3a4e81c60c9651867
EdvardXYZ <eddumansky@gmail.com>
2018-06-27 18:22:26 +0300

    Change Combat_Log usage
    Evaluators that need combat log data should call custom
    COMBAT_LOG_REQUESTED event, which will prompt Combat_Log to format and
    send the current event data. This is to prevent unnecessary calls of
    CombatLogGetCurrentEventInfo.

--- Events/Evaluators/Combat/Combat_Log.lua
+++ Events/Evaluators/Combat/Combat_Log.lua
@@ -1,8 +1,10 @@
 local TheEyeAddon = TheEyeAddon
 
 local ipairs = ipairs
-local table = table
 
+
+-- Evaluators that need combat log data should call custom COMBAT_LOG_REQUESTED
+-- event, which will prompt Combat_Log to format and send the current event data.
 
 -- inputValues = { --[[eventName]] "" }
 TheEyeAddon.Events.Evaluators.Combat_Log =
@@ -11,12 +13,20 @@ TheEyeAddon.Events.Evaluators.Combat_Log =
     gameEvents =
     {
         "COMBAT_LOG_EVENT_UNFILTERED"
+    },
+    customEvents =
+    {
+        "COMBAT_LOG_REQUESTED"
     }
 }
 
-function TheEyeAddon.Events.Evaluators.Combat_Log:GetKey()
-    self.rawEventInfo = { CombatLogGetCurrentEventInfo() }
-    return self.rawEventInfo[2]
+function TheEyeAddon.Events.Evaluators.Combat_Log:GetKey(event)
+    if event == COMBAT_LOG_EVENT_UNFILTERED then
+        self.combatLogSent = false
+    elseif self.combatLogSent == false then -- COMBAT_LOG_REQUESTED
+        self.rawEventInfo = { CombatLogGetCurrentEventInfo() }
+        return self.rawEventInfo[2]
+    end
 end
 
 function TheEyeAddon.Events.Evaluators.Combat_Log:Evaluate()
@@ -27,4 +37,5 @@ function TheEyeAddon.Events.Evaluators.Combat_Log:Evaluate()
     end
 
     TheEyeAddon.Events.Coordinator:SendCustomEvent(self.formattedEventInfo["event"], self.formattedEventInfo)
+    self.combatLogSent = true
 end
