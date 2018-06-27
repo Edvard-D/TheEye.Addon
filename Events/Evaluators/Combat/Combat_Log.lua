9d6666251f11ba37d034c8a8fa923d67df04441e
EdvardXYZ <eddumansky@gmail.com>
2018-06-27 20:59:07 +0300

    Correct where CombatLogGetCurrentEventInfo is called
    This function can only be called in the same frame that
    COMBAT_LOG_EVENT_UNFILTERED is called. By waiting until
    COMBAT_LOG_REQUESTED was called, CombatLogGetCurrentEventInfo returned
    nil. As such, other evaluators should no longer call
    COMBAT_LOG_REQUESTED; Combat_Log will evaluate whether there's a valid
    key with every COMBAT_LOG_EVENT_UNFILTERED event.

--- Events/Evaluators/Combat/Combat_Log.lua
+++ Events/Evaluators/Combat/Combat_Log.lua
@@ -1,13 +1,11 @@
 local TheEyeAddon = TheEyeAddon
 
+local CombatLogGetCurrentEventInfo = CombatLogGetCurrentEventInfo
 local ipairs = ipairs
 local pairs = pairs
 local table = table
 local UnitGUID = UnitGUID
 
-
--- Evaluators that need combat log data should call custom COMBAT_LOG_REQUESTED
--- event, which will prompt Combat_Log to format and send the current event data.
 
 -- inputValues = { --[[eventName]] "", --[[sourceUnit]] "", --[[destUnit]] "" }
 TheEyeAddon.Events.Evaluators.Combat_Log =
@@ -16,37 +14,31 @@ TheEyeAddon.Events.Evaluators.Combat_Log =
     gameEvents =
     {
         "COMBAT_LOG_EVENT_UNFILTERED"
-    },
-    customEvents =
-    {
-        "COMBAT_LOG_REQUESTED"
     }
 }
 
 function TheEyeAddon.Events.Evaluators.Combat_Log:GetKey(event)
-    if event == "COMBAT_LOG_EVENT_UNFILTERED" then
-        self.combatLogSent = false
-    elseif self.combatLogSent == false then -- COMBAT_LOG_REQUESTED
-        self.rawEventInfo = { CombatLogGetCurrentEventInfo() }
-        local sourceGUID = self.rawEventInfo[4]
-        local destGUID = self.rawEventInfo[8]
-        local unitGUIDs = {}
+    self.combatLogSent = false
+    self.rawEventInfo = { CombatLogGetCurrentEventInfo() }
+    
+    local sourceGUID = self.rawEventInfo[4]
+    local destGUID = self.rawEventInfo[8]
+    local unitGUIDs = {}
 
-        for k,valueGroup in pairs(self.ValueGroups) do
-            local sourceUnit = valueGroup.inputValues[2]
-            local destUnit = valueGroup.inputValues[3]
+    for k,valueGroup in pairs(self.ValueGroups) do
+        local sourceUnit = valueGroup.inputValues[2]
+        local destUnit = valueGroup.inputValues[3]
 
-            if sourceUnit ~= "" and unitGUIDs[sourceUnit] == nil then
-                unitGUIDs[sourceUnit] = UnitGUID(sourceUnit)
-            end
-            if destUnit ~= "" and unitGUIDs[destUnit] == nil then
-                unitGUIDs[destUnit] = UnitGUID(destUnit)
-            end
+        if sourceUnit ~= "" and unitGUIDs[sourceUnit] == nil then
+            unitGUIDs[sourceUnit] = UnitGUID(sourceUnit)
+        end
+        if destUnit ~= "" and unitGUIDs[destUnit] == nil then
+            unitGUIDs[destUnit] = UnitGUID(destUnit)
+        end
 
-            if (sourceUnit == "" or sourceGUID == unitGUIDs[sourceUnit]) and
-            (destUnit == "" or destGUID == unitGUIDs[destUnit]) then
-                return table.concat({ self.rawEventInfo[2], sourceUnit, destUnit })
-            end
+        if (sourceUnit == "" or sourceGUID == unitGUIDs[sourceUnit]) and
+        (destUnit == "" or destGUID == unitGUIDs[destUnit]) then
+            return table.concat({ self.rawEventInfo[2], sourceUnit, destUnit })
         end
     end
 end
