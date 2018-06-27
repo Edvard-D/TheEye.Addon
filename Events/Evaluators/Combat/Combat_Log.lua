1884906e76e507edf87fce6de5988d2844eedc1f
EdvardXYZ <eddumansky@gmail.com>
2018-06-27 18:52:23 +0300

    Add sourceUnit and destUnit as required Combat_Log inputValues
    The majority of game functions and events work off of the idea of
    "units," with combat logs being a noteable exception. Deducing the
    unitID of the source and destination unit in the Combat_Log evaluator
    will ensure that other evaluators aren't duplicating work.

--- Events/Evaluators/Combat/Combat_Log.lua
+++ Events/Evaluators/Combat/Combat_Log.lua
@@ -1,12 +1,15 @@
 local TheEyeAddon = TheEyeAddon
 
 local ipairs = ipairs
+local pairs = pairs
+local table = table
+local UnitGUID = UnitGUID
 
 
 -- Evaluators that need combat log data should call custom COMBAT_LOG_REQUESTED
 -- event, which will prompt Combat_Log to format and send the current event data.
 
--- inputValues = { --[[eventName]] "" }
+-- inputValues = { --[[eventName]] "", --[[sourceUnit]] "", --[[destUnit]] "" }
 TheEyeAddon.Events.Evaluators.Combat_Log =
 {
     type = "EVENT",
@@ -25,7 +28,28 @@ function TheEyeAddon.Events.Evaluators.Combat_Log:GetKey(event)
         self.combatLogSent = false
     elseif self.combatLogSent == false then -- COMBAT_LOG_REQUESTED
         self.rawEventInfo = { CombatLogGetCurrentEventInfo() }
-        return self.rawEventInfo[2]
+        local sourceGUID = self.rawEventInfo[4]
+        local destGUID = self.rawEventInfo[8]
+        local unitGUIDs = {}
+
+        for k,valueGroup in pairs(self.ValueGroups) do
+            local sourceUnit = valueGroup.inputValues[2]
+            local destUnit = valueGroup.inputValues[3]
+
+            if sourceUnit ~= "" and unitGUIDs[sourceUnit] == nil then
+                unitGUIDs[sourceUnit] = UnitGUID(sourceUnit)
+            end
+            if destUnit ~= "" and unitGUIDs[destUnit] == nil then
+                unitGUIDs[destUnit] = UnitGUID(destUnit)
+            end
+
+            if (sourceUnit == "" or sourceGUID == unitGUIDs[sourceUnit]) and
+            (destUnit == "" or destGUID == unitGUIDs[destUnit]) then
+                self.sourceUnit = sourceUnit
+                self.destUnit = destUnit
+                return table.concat({ self.rawEventInfo[2], sourceUnit, destUnit })
+            end
+        end
     end
 end
 
@@ -35,6 +59,9 @@ function TheEyeAddon.Events.Evaluators.Combat_Log:Evaluate()
     for i,valueName in ipairs(TheEyeAddon.Events.Evaluators.Combat_Log.EventValueNames[self.rawEventInfo[2]]) do
         self.formattedEventInfo[valueName] = self.rawEventInfo[i]
     end
+
+    self.formattedEventInfo["sourceUnit"] = self.sourceUnit
+    self.formattedEventInfo["destUnit"] = self.destUnit
 
     TheEyeAddon.Events.Coordinator:SendCustomEvent(self.formattedEventInfo["event"], self.formattedEventInfo)
     self.combatLogSent = true
