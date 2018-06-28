60edd36dcf0f438894ad8671afadfb2090ebfa37
EdvardXYZ <eddumansky@gmail.com>
2018-06-28 10:35:41 +0300

    Define AuraFiltersGet function

--- Auras.lua
+++ Auras.lua
@@ -1,2 +1,21 @@
 local TheEyeAddon = TheEyeAddon
-TheEyeAddon.AuraFilters = { }
+TheEyeAddon.Auras = { }
+
+local ipairs = ipairs
+local table = table
+
+
+local function AuraFiltersGet(subTableKey, filtersKey, sourceUnit)
+    local filters = {}
+
+    if sourceUnit == "player" then
+        table.insert(filters, "PLAYER ")
+    end
+
+    local retrievedFilters = TheEyeAddon.Auras.Filters[subTableKey][filtersKey]
+    if retrievedFilters ~= nil then
+        for i,v in ipairs(retrievedFilters) do
+            table.insert(filters, v)
+        end
+    end
+end
