8e7a38123b92d340047f0d2e9a24c9f8cd168282
EdvardXYZ <eddumansky@gmail.com>
2018-06-28 10:14:21 +0300

    Add clarifying comments

--- AuraFilters.lua
+++ AuraFilters.lua
@@ -1,10 +1,12 @@
+-- Allowed filters: HELPFUL, HARMFUL, CANCELABLE, NOT_CANCELABLE
+
 local TheEyeAddon = TheEyeAddon
 TheEyeAddon.Auras.Filters = {}
 
 
 TheEyeAddon.Auras.Filters.SpellID =
 {
-    [589] =
+    [589] = -- Shadow Word: Pain
     {
         "HARMFUL "
     }
