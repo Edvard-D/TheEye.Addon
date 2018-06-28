fb76fb25d80f006364a832a394dbf11ed0bb78bd
EdvardXYZ <eddumansky@gmail.com>
2018-06-28 17:06:26 +0300

    Define RegisterChild function
    This is to be used by EventGroups as an OnEvent function.

--- UI/Objects/Helpers/EventGroups.lua
+++ UI/Objects/Helpers/EventGroups.lua
@@ -1,1 +1,10 @@
 local TheEyeAddon = TheEyeAddon
+
+
+function TheEyeAddon.UI.Objects:RegisterChild(event, uiObject)
+    if self.uiObject.Children == nil then
+        self.uiObject.Children = { uiObject }
+    else
+        table.insert(self.uiObject.Children, uiObject)
+    end
+end
