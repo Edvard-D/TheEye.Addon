9e11c6817c0ff09a24fd2c571b59749f4c6d5860
EdvardXYZ <eddumansky@gmail.com>
2018-06-28 16:10:23 +0300

    Create UIOBJECT_WITHTAGS_INITIALIZED evaluator

---
+++ Events/Evaluators/UIObject/UIOBJECT_WITHTAGS_INITIALIZED.lua
@@ -0,0 +1,30 @@
+local TheEyeAddon = TheEyeAddon
+
+local ipairs = ipairs
+local select = select
+local table = table
+
+
+-- inputValues = { --[[tags]] "_", "_" }
+--      tags: must be in alphabetical order
+TheEyeAddon.Events.Evaluators.UIOBJECT_WITHTAGS_INITIALIZED =
+{
+    type = "EVENT",
+    reevaluateEvents =
+    {
+        UIOBJECT_HIDDEN = true,
+        UIOBJECT_SHOWN = true,
+    },
+    customEvents =
+    {
+        "UIOBJECT_HIDDEN",
+        "UIOBJECT_SHOWN",
+    }
+}
+
+function TheEyeAddon.Events.Evaluators.UIOBJECT_WITHTAGS_INITIALIZED:Evaluate(valueGroup, event, ...)
+    local uiObject = ...
+    local hasTags = TheEyeAddon.UI.Objects.Tags:UIObjectHasTags(uiObject, valueGroup.inputValues, valueGroup.key)
+
+    return hasTags, "UIOBJECT_WITHTAGS_INITIALIZED", uiObject
+end
