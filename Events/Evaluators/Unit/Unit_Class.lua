4cb469ef2f6c1db08fc360f8114ebce7782c5f18
EdvardXYZ <eddumansky@gmail.com>
2018-06-28 13:12:00 +0300

    Change Player_Class to generic Unit_Class

---
+++ Events/Evaluators/Unit/Unit_Class.lua
@@ -0,0 +1,30 @@
+local TheEyeAddon = TheEyeAddon
+
+local select = select
+local UnitClass = UnitClass
+
+
+-- inputValues = { --[[unit]] "_", --[[classIndex]] 0 }
+--      unit: required
+--      classIndex: required
+TheEyeAddon.Events.Evaluators.Unit_Class =
+{
+    type = "STATE",
+    reevaluateEvents =
+    {
+        PLAYER_TARGET_CHANGED = true
+    },
+    gameEvents =
+    {
+        "PLAYER_TARGET_CHANGED"
+    }
+}
+
+function TheEyeAddon.Events.Evaluators.Player_Class:CalculateCurrentState(inputValues)
+    local classIndex = select(3, UnitClass(inputValues[1]))
+    return classIndex == inputValues[2]
+end
+
+function TheEyeAddon.Events.Evaluators.Player_Class:Evaluate(valueGroup)
+    return TheEyeAddon.Events.Evaluators.Player_Class:CalculateCurrentState(valueGroup.inputValues)
+end
