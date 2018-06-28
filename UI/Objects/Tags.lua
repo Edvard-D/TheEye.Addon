1722f0008b5b4ac6e0f05abeb80d9437c88e8b04
EdvardXYZ <eddumansky@gmail.com>
2018-06-28 16:05:08 +0300

    Create KeyHasTags local function

--- UI/Objects/Tags.lua
+++ UI/Objects/Tags.lua
@@ -1,2 +1,15 @@
 local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Objects.Tags = {}
+
+local ipairs = ipairs
+
+
+local function KeyHasTags(key, tags)
+    for i,tag in ipairs(tags) do
+        if key:find(tag) == nil then
+            return false
+        end
+    end
+
+    return true
+end
