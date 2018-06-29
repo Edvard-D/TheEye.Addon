5271a68462d0e7c7c24d7af28bd52cda46c68f22
EdvardXYZ <eddumansky@gmail.com>
2018-06-29 11:32:47 +0300

    Define SetupListener function

--- UI/Objects/Helpers/ListenerGroups.lua
+++ UI/Objects/Helpers/ListenerGroups.lua
@@ -2,7 +2,16 @@ local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Objects.ListenerGroups = {}
 
 
---EVENT
+-- Setup
+function SetupListener(uiObject, listenerGroup, listener, evaluatorName, OnEvaluate)
+    listener.uiObject = uiObject
+    listener.listenerGroup = listenerGroup
+    listener.OnEvaluate = OnEvaluate
+    TheEyeAddon.Events.Evaluators:RegisterListener(evaluatorName, listener)
+end
+
+
+-- OnEvaluate: EVENT
 function TheEyeAddon.UI.Objects.ListenerGroups:RegisterChild(event, uiObject)
     if self.uiObject.Children == nil then
         self.uiObject.Children = { uiObject }
@@ -17,7 +26,7 @@ function TheEyeAddon.UI.Objects.ListenerGroups:SortChildrenByPriority(state, eve
 end
 
 
--- STATE
+-- OnEvaluate: STATE
 function TheEyeAddon.UI.Objects.ListenerGroups:ChangeValueByState(state)
     if state == true then
         self.uiObject.ValueHandlers[self.listenerGroup.valueHandlerKey]:ChangeValue(self.value)
