d3aa37a94d85ce04b1b5ef67f10c82ca54ac12a5
EdvardXYZ <eddumansky@gmail.com>
2018-06-29 11:21:00 +0300

    Define ChangeValueByState function

--- UI/Objects/Helpers/ListenerGroups.lua
+++ UI/Objects/Helpers/ListenerGroups.lua
@@ -1,2 +1,11 @@
 local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Objects.ListenerGroups = {}
+
+
+function TheEyeAddon.UI.Objects.ListenerGroups:ChangeValueByState(state)
+    if state == true then
+        self.uiObject.ValueHandlers[self.listenerGroup.valueHandlerKey]:ChangeValue(self.value)
+    else
+        self.uiObject.ValueHandlers[self.listenerGroup.valueHandlerKey]:ChangeValue(self.value * -1)
+    end
+end
