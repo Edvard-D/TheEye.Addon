a5cdc7a8b856b4dd1332924be77446c8d29d33db
EdvardXYZ <eddumansky@gmail.com>
2018-06-29 17:23:09 +0300

    Correct function name

--- UI/Objects/Helpers/ListenerGroups.lua
+++ UI/Objects/Helpers/ListenerGroups.lua
@@ -62,7 +62,7 @@ function TheEyeAddon.UI.Objects.ListenerGroups:RegisterChild(event, uiObject)
     end
 end
 
-function TheEyeAddon.UI.Objects.ListenerGroups:SortChildrenDescending(state, event, uiObject)
+function TheEyeAddon.UI.Objects.ListenerGroups:ChildrenSortDescending(state, event, uiObject)
     table.sort(uiObject.Children, function(a,b)
         return a.ValueGroups.SortRank.value > b.ValueGroups.SortRank.combinedKeyValue end) 
 end
