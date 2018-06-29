1de4de4863185b6b5f0b515b35a1948b8b318934
EdvardXYZ <eddumansky@gmail.com>
2018-06-29 11:24:40 +0300

    Move EventGroups functions to ListenerGroups

--- UI/Objects/Helpers/ListenerGroups.lua
+++ UI/Objects/Helpers/ListenerGroups.lua
@@ -2,6 +2,22 @@ local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Objects.ListenerGroups = {}
 
 
+--EVENT
+function TheEyeAddon.UI.Objects.ListenerGroups:RegisterChild(event, uiObject)
+    if self.uiObject.Children == nil then
+        self.uiObject.Children = { uiObject }
+    else
+        table.insert(self.uiObject.Children, uiObject)
+    end
+end
+
+function TheEyeAddon.UI.Objects.ListenerGroups:SortChildrenByPriority(state, event, uiObject)
+    table.sort(uiObject.Children, function(a,b)
+        return a.ListenerGroups.Priority.combinedKeyValue > b.ListenerGroups.Priority.combinedKeyValue end) 
+end
+
+
+-- STATE
 function TheEyeAddon.UI.Objects.ListenerGroups:ChangeValueByState(state)
     if state == true then
         self.uiObject.ValueHandlers[self.listenerGroup.valueHandlerKey]:ChangeValue(self.value)
