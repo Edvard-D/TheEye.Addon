f3e26bf88da706a45bc9fc71a2ebd69709651ed7
EdvardXYZ <eddumansky@gmail.com>
2018-06-29 15:11:59 +0300

    Recheck if modules are enabled when the addon loads
    Returning true by default is not the intended functionality of this.
    UIObjects register as listeners to Evaluators before saved settings
    are loaded, so TheEyeAddon.Settings would always be nil. Settings are
    avaialble at the ADDON_LOADED event.

--- Initializer.lua
+++ Initializer.lua
@@ -1,1 +1,10 @@
 TheEyeAddon.UI.Objects:Initialize()
+
+
+if TheEyeAddon.Settings == nil then
+    TheEyeAddon.Settings = {}
+end
+
+if TheEyeAddon.Settings.DisabledUIModules == nil then
+    TheEyeAddon.Settings.DisabledUIModules = {}
+end
