8f17c3bf538adf5b1de7269a3a571f22c5659137
EdvardXYZ <eddumansky@gmail.com>
2018-06-29 11:25:52 +0300

    Change function namespace

--- UI/Objects/Helpers/ValueHandlers.lua
+++ UI/Objects/Helpers/ValueHandlers.lua
@@ -2,7 +2,7 @@ local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Objects.ValueHandlers = {}
 
 
-function TheEyeAddon.UI.Objects:OnStateKeyChange(valueChange)
+function TheEyeAddon.UI.Objects.ValueHandlers:OnStateKeyChange(valueChange)
     self.value = self.value + valueChange
 
     if self.validValues[self.value] == true then
