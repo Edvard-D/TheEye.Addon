f27fca169768b30b547a5893835e4ba31d86d478
EdvardXYZ <eddumansky@gmail.com>
2018-06-29 11:16:39 +0300

    Define OnStateKeyChange function

--- UI/Objects/Helpers/ValueHandlers.lua
+++ UI/Objects/Helpers/ValueHandlers.lua
@@ -1,2 +1,13 @@
 local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Objects.ValueHandlers = {}
+
+
+function TheEyeAddon.UI.Objects:OnStateKeyChange(valueChange)
+    self.value = self.value + valueChange
+
+    if self.validValues[self.value] == true then
+        self:OnValidValue(self.uiObject)
+    else
+        self:OnInvalidValue(self.uiObject)
+    end
+end
