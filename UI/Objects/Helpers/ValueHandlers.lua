a325f502f46e1f48a328c2d57c8bfdd0288d1fb5
EdvardXYZ <eddumansky@gmail.com>
2018-06-29 11:30:14 +0300

    Define SetupStateValue function

--- UI/Objects/Helpers/ValueHandlers.lua
+++ UI/Objects/Helpers/ValueHandlers.lua
@@ -2,6 +2,18 @@ local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Objects.ValueHandlers = {}
 
 
+-- Setup
+function TheEyeAddon.UI.Objects.ValueHandlers:SetupStateValue()
+    self.value = 0
+    self.state = false
+
+    if self.validValues[0] == true then
+        self.OnValidValue(self.uiObject)
+    end
+end
+
+
+-- ChangeValue
 function TheEyeAddon.UI.Objects.ValueHandlers:OnStateKeyChange(valueChange)
     self.value = self.value + valueChange
 
