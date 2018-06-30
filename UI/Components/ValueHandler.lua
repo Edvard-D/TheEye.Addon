d67617a9772a6d643b01e279410d881a3dc33450
EdvardXYZ <eddumansky@gmail.com>
2018-06-30 15:03:55 +0300

    Define ValueHandler:Create function

--- UI/Components/ValueHandler.lua
+++ UI/Components/ValueHandler.lua
@@ -1,2 +1,19 @@
 local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Objects.Components.ValueHandler = {}
+
+
+function TheEyeAddon.UI.Components.ValueHandler:Create(setup, changeValue, valueDefault)
+    local instance = {}
+    
+    -- Base
+    instance.Reset = function() self.value = self.valueDefault end
+
+    -- Passed
+    instance.Setup = setup -- function
+    instance.ChangeValue = changeValue -- function
+    
+    instance.valueDefault = valueDefault -- value
+    instance.value = valueDefault -- value
+
+    return instance 
+end
