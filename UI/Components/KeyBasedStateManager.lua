e63988ca0bb5226aeb30c7fdf668d736f9593f8f
EdvardXYZ <eddumansky@gmail.com>
2018-06-30 19:29:04 +0300

    Define KeyBasedStateManager:Setup function

--- UI/Components/KeyBasedStateManager.lua
+++ UI/Components/KeyBasedStateManager.lua
@@ -1,3 +1,30 @@
 local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Components.KeyBasedStateManager = {}
 local this = TheEyeAddon.UI.Components.KeyBasedStateManager
+
+-- DEFINED IN TEMPLATE
+--      ValueHandler            ValueHandler: must be of type KeyState
+--      ListenerGroup           ListenerGroup: must be of type StateBasedValueChanger
+
+
+-- SETUP
+--      instance
+--      OnValidKey              function(...)
+--      OnInvalidKey            function(...)
+function this:Setup(
+    instance,
+    OnValidKey,
+    OnInvalidKey
+)
+    
+    TheEyeAddon.UI.Objects.Components.ValueHandlers.KeyState:Setup(
+        instance.ValueHandler,
+        OnValidKey,
+        OnInvalidKey
+    )
+
+    TheEyeAddon.UI.Objects.Components.ListenerGroups.StateBasedValueChanger:Setup(
+        instance.ListenerGroup,
+        instance.ValueHandler
+    )
+end
