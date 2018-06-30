37012f2a9464e393183d2e6566929747b8cd4b12
EdvardXYZ <eddumansky@gmail.com>
2018-06-30 19:09:37 +0300

    Create StateBasedValueChanger.lua file

---
+++ UI/Components/ListenerGroups.lua/StateBasedValueChanger.lua
@@ -0,0 +1,3 @@
+local TheEyeAddon = TheEyeAddon
+TheEyeAddon.UI.Objects.Components.ListenerGroups.StateBasedValueChanger = {}
+local this = TheEyeAddon.UI.Objects.Components.ListenerGroups.StateBasedValueChanger
