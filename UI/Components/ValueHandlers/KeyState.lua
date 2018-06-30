c1fe0a461cf44238fdd23936def8a9035b2f4c50
EdvardXYZ <eddumansky@gmail.com>
2018-06-30 15:35:59 +0300

    Define KeyState:Create function

--- UI/Components/ValueHandlers/KeyState.lua
+++ UI/Components/ValueHandlers/KeyState.lua
@@ -1,3 +1,21 @@
 local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Objects.Components.ValueHandlers.KeyState = {}
 local this = TheEyeAddon.UI.Objects.Components.ValueHandlers.KeyState
+
+
+function this:Create(
+    ValidKeys, -- table with integer keys
+    OnValidKey, -- function
+    OnInvalidKey -- function
+)
+    instance = TheEyeAddon.UI.Objects.Components.ValueHandlers.Base:Create(
+        this.ChangeValue,
+        0
+    )
+
+    instance.ValidKeys = ValidKeys
+    instance.OnValidKey = OnValidKey
+    instance.OnInvalidKey = OnInvalidKey
+
+    return instance
+end
