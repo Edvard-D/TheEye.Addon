630efef2b67df3d4fb94639dea4f1b8a8bea40a1
EdvardXYZ <eddumansky@gmail.com>
2018-06-30 16:30:55 +0300

    Define ValueChanger:Create function

--- UI/Components/Listeners/ValueChanger.lua
+++ UI/Components/Listeners/ValueChanger.lua
@@ -1,3 +1,22 @@
 local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Objects.Components.Listeners.ValueChanger = {}
 local this = TheEyeAddon.UI.Objects.Components.Listeners.ValueChanger
+
+
+function this:Create(
+    evaluatorKey,   -- string: must match the name of an Evaluator
+    inputValues,    -- table: structure defined by Evaluators
+    value,          -- integer: value passed to ValueGroup
+    ValueGroup,     -- ValueGroup
+    OnEvaluate      -- function: should do something with value to ValueGroup
+)
+    local instance = TheEyeAddon.UI.Objects.Components.Listeners.ValueChanger.Base:Create(
+    evaluatorKey,
+    inputValues,
+    OnEvaluate
+)
+
+    instance.value = value
+
+    return instance
+end
