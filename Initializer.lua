a1ea4ba61c32a21a1905d8b42531c4be6c369fd0
EdvardXYZ <eddumansky@gmail.com>
2018-06-30 14:16:59 +0300

    Save screen dimensions on initialization

--- Initializer.lua
+++ Initializer.lua
@@ -1,6 +1,13 @@
 TheEyeAddon.UI.Objects:Initialize()
 
 
+TheEyeAddon.Screen =
+{
+    width = GetScreenWidth(),
+    height = GetScreenHeight(),
+}
+
+
 if TheEyeAddon.Settings == nil then
     TheEyeAddon.Settings = {}
 end
