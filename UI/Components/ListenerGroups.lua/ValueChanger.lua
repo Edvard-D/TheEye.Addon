f4747b83fc9ac59d4667b68fc76b1170e7e01c8e
EdvardXYZ <eddumansky@gmail.com>
2018-06-30 18:55:44 +0300

    Define ListenerGroups\ValueChanger:Setup function

--- UI/Components/ListenerGroups.lua/ValueChanger.lua
+++ UI/Components/ListenerGroups.lua/ValueChanger.lua
@@ -1,3 +1,19 @@
 local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Objects.Components.ListenerGroups.ValueChanger = {}
 local this = TheEyeAddon.UI.Objects.Components.ListenerGroups.ValueChanger
+
+
+-- SETUP
+--      instance
+function this:Setup(
+    instance
+)
+
+    TheEyeAddon.UI.Objects.Components.ListenerGroups.Base:Setup(
+        instance,
+        TheEyeAddon.UI.Objects.Components.Listeners.ValueChanger.Setup,
+        OnSetup, -- @TODO
+        OnTeardown, -- @TODO
+        OnEvaluate -- @TODO
+    )
+end
