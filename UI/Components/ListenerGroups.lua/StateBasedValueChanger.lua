ffcd9a6e37d79572650d6a6291cf3bd3bc71a0cd
EdvardXYZ <eddumansky@gmail.com>
2018-06-30 19:10:07 +0300

    Define Setup function

--- UI/Components/ListenerGroups.lua/StateBasedValueChanger.lua
+++ UI/Components/ListenerGroups.lua/StateBasedValueChanger.lua
@@ -1,3 +1,19 @@
 local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Objects.Components.ListenerGroups.StateBasedValueChanger = {}
 local this = TheEyeAddon.UI.Objects.Components.ListenerGroups.StateBasedValueChanger
+
+
+-- SETUP
+--      instance
+--      ValueHandler
+function this:Setup(
+    instance,
+    ValueHandler
+)
+
+    TheEyeAddon.UI.Objects.Components.ListenerGroups.ValueChanger:Setup(
+        instance,
+        ValueHandler,
+        OnEvaluate -- @TODO
+    )
+end
