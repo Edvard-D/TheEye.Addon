dd34abea367ef50638408ab4edb53f6415f26496
EdvardXYZ <eddumansky@gmail.com>
2018-06-30 11:14:57 +0300

    Define GroupChildren:ClearAll function

--- UI/Objects/Helpers/GroupChildren.lua
+++ UI/Objects/Helpers/GroupChildren.lua
@@ -1,2 +1,12 @@
 local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Objects.GroupChildren = {}
+
+
+function TheEyeAddon.UI.Objects.GroupChildren:ClearAll()
+    local children = uiObject.children
+    if children ~= nil then
+        for i=#children, -1 do
+            children[i] = nil
+        end
+    end
+end
