3af777b8787d22f0bbe6d98704a42118d13ed1fd
EdvardXYZ <eddumansky@gmail.com>
2018-07-01 18:01:07 +0300

    Define ListenerValueHandlers.Base:Setup function

--- UI/Components/ListenerValueHandlers/Base.lua
+++ UI/Components/ListenerValueHandlers/Base.lua
@@ -1,3 +1,40 @@
 local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Components.ListenerValueHandlers.Base = {}
 local this = TheEyeAddon.UI.Components.ListenerValueHandlers.Base
+
+
+--[[ #this#TEMPLATE#
+{
+    ValueHandler = #TheEyeAddon.UI.Objects.Components.ValueHandlers#NAME#TEMPLATE#
+    ListenerGroup = #TheEyeAddon.UI.Objects.Components.ListenerGroups#NAME#TEMPLATE#
+}
+]]
+
+
+--[[ SETUP
+    instance
+    UIObject                    UIObject
+    OnValidKey                  function()
+    OnInvalidKey                function()
+]]
+function this:Setup(
+    instance,
+    UIObject,
+    OnValidKey,
+    OnInvalidKey
+)
+    
+    instance.UIObject = UIObject
+
+    instance.ValueHandler = IntegerKeyStateSetup:Setup(
+        instance.ValueHandler or {},
+        UIObject,
+        this.OnStateChange -- @TODO
+    )
+
+    StateBasedValueChangerSetup:Setup(
+        instance.ListenerGroup or {},
+        UIObject,
+        instance.ValueHandler
+    )
+end
