dd93109d4bef4aea734c5ededb7a095d9a0f6c54
EdvardXYZ <eddumansky@gmail.com>
2018-07-01 18:08:05 +0300

    Refactor ListenerValueHandlers.Base to be more generic

--- UI/Components/ListenerValueHandlers/Base.lua
+++ UI/Components/ListenerValueHandlers/Base.lua
@@ -5,8 +5,7 @@ local this = TheEyeAddon.UI.Components.ListenerValueHandlers.Base
 
 --[[ #this#TEMPLATE#
 {
-    ValueHandler = #TheEyeAddon.UI.Objects.Components.ValueHandlers#NAME#TEMPLATE#
-    ListenerGroup = #TheEyeAddon.UI.Objects.Components.ListenerGroups#NAME#TEMPLATE#
+    nil
 }
 ]]
 
@@ -14,35 +13,17 @@ local this = TheEyeAddon.UI.Components.ListenerValueHandlers.Base
 --[[ SETUP
     instance
     UIObject                    UIObject
-    OnValidKey                  function()
-    OnInvalidKey                function()
+    ValueHandler                ValueHandler
+    ListenerGroup               ListenerGroup
 ]]
 function this:Setup(
     instance,
     UIObject,
-    OnValidKey,
-    OnInvalidKey
+    ValueHandler,
+    ListenerGroup
 )
     
     instance.UIObject = UIObject
-
-    instance.ValueHandler = IntegerKeyStateSetup:Setup(
-        instance.ValueHandler or {},
-        UIObject,
-        this.OnStateChange
-    )
-
-    instance.ListenerGroup = StateBasedValueChangerSetup:Setup(
-        instance.ListenerGroup or {},
-        UIObject,
-        instance.ValueHandler
-    )
-end
-
-function this:OnStateChange(state)
-    if state == true then
-        self:OnValidKey()
-    else
-        self:OnInvalidKey()
-    end
+    instance.ValueHandler = ValueHandler
+    instance.ListenerGroup = ListenerGroup
 end
