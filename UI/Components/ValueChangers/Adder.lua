b0bdf0c827552db1dee56a2c8c003a680854a96a
EdvardXYZ <eddumansky@gmail.com>
2018-07-01 09:00:31 +0300

    Define ValueChangers\Adder:Setup function

--- UI/Components/ValueChangers/Adder.lua
+++ UI/Components/ValueChangers/Adder.lua
@@ -1,3 +1,24 @@
 local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Objects.Components.ValueChangers.Adder = {}
 local this = TheEyeAddon.UI.Objects.Components.ValueChangers.Adder
+
+--[[ TEMPLATE
+nil
+]]
+
+
+-- SETUP
+--      instance
+--      UIObject                    UIObject
+function this:Setup(
+    instance,
+    UIObject
+)
+
+    TheEyeAddon.UI.Objects.Components.ValueChangers.Base:Setup(
+        instance,
+        UIObject,
+        this.Add, -- @TODO
+        0
+    )
+end
