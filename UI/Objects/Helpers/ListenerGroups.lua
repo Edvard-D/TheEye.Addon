aa10663ba47a989eedf2048c914213ac82efc238
EdvardXYZ <eddumansky@gmail.com>
2018-07-01 07:34:46 +0300

    Remove old setup/teardown functions
    Activation/deactivation is handled by components themselves.

--- UI/Objects/Helpers/ListenerGroups.lua
+++ UI/Objects/Helpers/ListenerGroups.lua
@@ -6,32 +6,7 @@ local select = select
 local table = table
 
 
--- Setup
-function SetupListener(uiObject, listenerGroup, listener, evaluatorName)
-    listener.UIObject = uiObject
-    listener.listenerGroup = listenerGroup
-    listener.OnEvaluate = listenerGroup.OnEvaluate
-    TheEyeAddon.Events.Evaluators:RegisterListener(evaluatorName, listener)
-end
-
-local function SetupListeningTo(uiObject, listenerGroup)
-    local listeningTo = listenerGroup.ListeningTo
-    for evaluatorName,v in pairs(listeningTo) do
-        local listener = listeningTo[evaluatorName]
-        SetupListener(uiObject, listenerGroup, listener, evaluatorName)
-    end
-end
-
-function TheEyeAddon.UI.Objects.ListenerGroups:SetupGroup(uiObject, listenerGroup)
-    listenerGroup.UIObject = uiObject
-
-    if listenerGroup.OnSetup ~= nil then -- Must be called before setting up listeners
-        listenerGroup:OnSetup()
-    end
-
-    SetupListeningTo(uiObject, listenerGroup)
-end
-
+-- @TODO implement multiple component setup/teardown, likley in managers
 function TheEyeAddon.UI.Objects.ListenerGroups:SetupGroupsOfType(uiObject, groupType)
     local listenerGroups = uiObject.ListenerGroups
     for i = 1, #listenerGroups do
@@ -39,19 +14,6 @@ function TheEyeAddon.UI.Objects.ListenerGroups:SetupGroupsOfType(uiObject, group
         if listenerGroup.type == groupType then
             TheEyeAddon.UI.Objects.ListenerGroups:SetupGroup(uiObject, listenerGroup)
         end
-    end
-end
-
-
--- Teardown
-function TheEyeAddon.UI.Objects.ListenerGroups:TeardownGroup(listenerGroup)
-    if listenerGroup.OnTeardown ~= nil then
-        listenerGroup:OnTeardown()
-    end
-
-    for evaluatorName,v in pairs(listenerGroup.ListeningTo) do
-        local listener = listenerGroup.ListeningTo[evaluatorName]
-        TheEyeAddon.Events.Evaluators:UnregisterListener(evaluatorName, listener)
     end
 end
 
