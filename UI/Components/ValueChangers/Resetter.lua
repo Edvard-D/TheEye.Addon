4bab3852fdb0342df565e6abc8b9a0e17d748292
EdvardXYZ <eddumansky@gmail.com>
2018-07-01 09:06:45 +0300

    Define Resetter:Setup function

--- UI/Components/ValueChangers/Resetter.lua
+++ UI/Components/ValueChangers/Resetter.lua
@@ -1,3 +1,24 @@
 local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Objects.Components.ValueChangers.Resetter = {}
 local this = TheEyeAddon.UI.Objects.Components.ValueChangers.Resetter
+
+--[[ TEMPLATE
+nil
+]]
+
+
+-- SETUP
+--      instance
+--      UIObject                    UIObject
+function this:Setup(
+    instance,
+    UIObject
+)
+
+    TheEyeAddon.UI.Objects.Components.ValueChangers.Base:Setup(
+        instance,
+        UIObject,
+        this.Reset, -- @TODO
+        0
+    )
+end
