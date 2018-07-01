6f35cc1dbe42e1bc64dc0a0c2ce7bc1c0279b074
EdvardXYZ <eddumansky@gmail.com>
2018-07-01 19:55:25 +0300

    Create Listeners\EventListener.lua file

---
+++ UI/Components/Listeners/EventListener.lua
@@ -0,0 +1,13 @@
+local TheEyeAddon = TheEyeAddon
+TheEyeAddon.UI.Objects.Components.Listeners.EventListener = {}
+local this = TheEyeAddon.UI.Objects.Components.Listeners.EventListener
+local inherited = TheEyeAddon.UI.Objects.Components.Listeners.Base
+
+
+--[[ #this#TEMPLATE#
+{
+    #inherited#TEMPLATE#
+    eventName = #EVENT#NAME#
+    inputValues = { #EVENT#TEMPLATE#inputValues# }
+}
+]]
