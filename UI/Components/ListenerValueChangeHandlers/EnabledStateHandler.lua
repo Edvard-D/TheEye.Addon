40c26945db339b2f93a3ce0b2747382f66cf536a
EdvardXYZ <eddumansky@gmail.com>
2018-07-01 18:43:18 +0300

    Create ListenerValueChangeHandlers\EnabledStateHandler.lua file

---
+++ UI/Components/ListenerValueChangeHandlers/EnabledStateHandler.lua
@@ -0,0 +1,11 @@
+local TheEyeAddon = TheEyeAddon
+TheEyeAddon.UI.Components.ListenerValueChangeHandlers.EnabledStateHandler = {}
+local this = TheEyeAddon.UI.Components.ListenerValueChangeHandlers.EnabledStateHandler
+local inherited = TheEyeAddon.UI.Components.ListenerValueChangeHandlers.KeyStateFunctionManager
+
+
+--[[ #this#TEMPLATE#
+{
+    #inherited#TEMPLATE#
+}
+]]
