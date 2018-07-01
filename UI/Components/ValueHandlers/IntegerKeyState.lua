95ac1824ec1575a9d2a2ccd7f59a25fb49b71b60
EdvardXYZ <eddumansky@gmail.com>
2018-07-01 11:25:58 +0300

    Define IntegerKeyState:Setup function

--- UI/Components/ValueHandlers/IntegerKeyState.lua
+++ UI/Components/ValueHandlers/IntegerKeyState.lua
@@ -1,3 +1,34 @@
 local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Objects.Components.ValueHandlers.IntegerKeyState = {}
 local this = TheEyeAddon.UI.Objects.Components.ValueHandlers.IntegerKeyState
+
+--[[ TEMPLATE
+ValidKeys =
+{
+    [0] = true    
+}
+]]
+
+
+-- SETUP
+--      instance
+--      UIObject                    UIObject
+--      OnValidKey                  function()
+--      OnInvalidKey                function()
+function this:Setup(
+    instance,
+    UIObject,
+    OnValidKey,
+    OnInvalidKey
+)
+
+    TheEyeAddon.UI.Objects.Components.ValueHandlers.KeyState:Setup(
+        instance,
+        UIObject,
+        TheEyeAddon.UI.Objects.Components.ValueHandlers.Base.Add,
+        0,
+        instance.ValidKeys,
+        OnValidKey,
+        OnInvalidKey
+    )
+end
