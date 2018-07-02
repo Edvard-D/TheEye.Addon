edf1b8597fe381baceb9402e633109963e5455ab
EdvardXYZ <eddumansky@gmail.com>
2018-07-03 00:07:19 +0300

    Add ability to activate/deactivate ListenerValueChangeHandlers

--- UI/Components/ListenerValueChangeHandlers/Base.lua
+++ UI/Components/ListenerValueChangeHandlers/Base.lua
@@ -26,4 +26,15 @@ function this:Setup(
     instance.UIObject = UIObject
     instance.ValueHandler = ValueHandler
     instance.ListenerGroup = ListenerGroup
+
+    instance.Activate = this.Activate
+    instance.Deactivate = this.Deactivate
+end
+
+function this:Activate()
+    self.ListenerGroup:Activate()
+end
+
+function this:Deactivate()
+    self.ListenerGroup:Deactivate()
 end
