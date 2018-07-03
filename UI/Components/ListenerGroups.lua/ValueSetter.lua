6274666d433b1bb3e02b842377d9f6922b42fb0c
EdvardXYZ <eddumansky@gmail.com>
2018-07-03 22:52:25 +0300

    Define ValueSetter:Setup function

--- UI/Components/ListenerGroups.lua/ValueSetter.lua
+++ UI/Components/ListenerGroups.lua/ValueSetter.lua
@@ -3,9 +3,39 @@ TheEyeAddon.UI.Objects.Components.ListenerGroups.ValueSetter = {}
 local this = TheEyeAddon.UI.Objects.Components.ListenerGroups.ValueSetter
 local inherited = TheEyeAddon.UI.Objects.Components.ListenerGroups.Base
 
+local ListenerSetup = TheEyeAddon.UI.Objects.Components.Listeners.EvaluatorListener.Setup
+
 
 --[[ #this#TEMPLATE#
 {
     #inherited#TEMPLATE#
 }
 ]]
+
+
+--[[ #SETUP#
+    instance
+    UIObject                    UIObject
+    ValueHandler                ValueHandler
+]]
+function this:Setup(
+    instance,
+    UIObject,
+    ValueHandler
+)
+
+    inherited:Setup(
+        instance,
+        UIObject,
+        ListenerSetup,
+        this.SetValue,
+        ValueHandler.Reset,
+        ValueHandler.Reset
+    )
+
+    instance.ValueHandler = ValueHandler
+end
+
+function this:SetValue(...)
+    self.ValueHandler:Change(select(1, ...))
+end
