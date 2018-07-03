960de38df5d84cfdc3284d2f960bdd87b97531c3
EdvardXYZ <eddumansky@gmail.com>
2018-07-03 22:52:00 +0300

    Create ListenerGroups\ValueSetter.lua file

---
+++ UI/Components/ListenerGroups.lua/ValueSetter.lua
@@ -0,0 +1,11 @@
+local TheEyeAddon = TheEyeAddon
+TheEyeAddon.UI.Objects.Components.ListenerGroups.ValueSetter = {}
+local this = TheEyeAddon.UI.Objects.Components.ListenerGroups.ValueSetter
+local inherited = TheEyeAddon.UI.Objects.Components.ListenerGroups.Base
+
+
+--[[ #this#TEMPLATE#
+{
+    #inherited#TEMPLATE#
+}
+]]
