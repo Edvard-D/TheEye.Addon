5a3f93c5de51080473023a0967ed423e02012695
EdvardXYZ <eddumansky@gmail.com>
2018-07-05 19:22:48 +0300

    Create ValueHandlers\SimpleState.lua file

---
+++ UI/Components/ValueHandlers/SimpleState.lua
@@ -0,0 +1,11 @@
+local TheEyeAddon = TheEyeAddon
+TheEyeAddon.UI.Objects.Components.ValueHandlers.SimpleState = {}
+local this = TheEyeAddon.UI.Objects.Components.ValueHandlers.SimpleState
+local inherited = TheEyeAddon.UI.Objects.Components.ValueHandlers.Base
+
+
+--[[ #this#TEMPLATE#
+{
+    #inherited#TEMPLATE#
+}
+]]
