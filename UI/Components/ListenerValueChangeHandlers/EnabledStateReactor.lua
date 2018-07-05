08660c93918893e3c4d35bf8d3def988b41d3f58
EdvardXYZ <eddumansky@gmail.com>
2018-07-05 20:16:19 +0300

    Define EnabledStateReactor:Setup function

--- UI/Components/ListenerValueChangeHandlers/EnabledStateReactor.lua
+++ UI/Components/ListenerValueChangeHandlers/EnabledStateReactor.lua
@@ -3,9 +3,57 @@ TheEyeAddon.UI.Components.ListenerValueChangeHandlers.EnabledStateReactor = {}
 local this = TheEyeAddon.UI.Components.ListenerValueChangeHandlers.EnabledStateReactor
 local inherited = TheEyeAddon.UI.Components.ListenerValueChangeHandlers.Base
 
+local SimpleStateSetup = TheEyeAddon.UI.Objects.Components.ValueHandlers.SimpleState
+local ValueSetterSetup = TheEyeAddon.UI.Objects.Components.ListenerGroups.ValueSetter.Setup
+
 
 --[[ #this#TEMPLATE#
 {
     #inherited#TEMPLATE#
 }
 ]]
+
+
+--[[ SETUP
+    instance
+    UIObject                    UIObject
+    OnEnable                    function()
+    OnDisable                   function()
+]]
+function this:Setup(
+    instance,
+    UIObject,
+    OnEnable,
+    OnDisable
+)
+
+    instance.ValueHandler = {}
+    SimpleStateSetup(
+        instance.ValueHandler,
+        UIObject,
+        this.OnEnabledChanged -- @TODO
+    )
+
+    instance.ListenerGroup =
+    {
+        Listeners =
+        {
+            evaluatorName = "UIObject_Visible",
+            inputValues = { UIObject.key }
+        }
+    }
+    ValueSetterSetup(
+        ListenerGroup,
+        UIObject,
+        instance.ValueHandler
+    )
+
+    inherited:Setup(
+        instance,
+        UIObject,
+        instance.ListenerGroup
+    )
+
+    instance.OnEnable = OnEnable
+    instance.OnDisable = OnDisable
+end
