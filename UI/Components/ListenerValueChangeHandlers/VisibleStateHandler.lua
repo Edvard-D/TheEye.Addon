946623029ab1e612e0fde3ee1c04553d4e367ae5
EdvardXYZ <eddumansky@gmail.com>
2018-07-05 20:37:24 +0300

    Define VisibleStateHandler:Setup function

--- UI/Components/ListenerValueChangeHandlers/VisibleStateHandler.lua
+++ UI/Components/ListenerValueChangeHandlers/VisibleStateHandler.lua
@@ -3,9 +3,41 @@ TheEyeAddon.UI.Components.ListenerValueChangeHandlers.VisibleStateHandler = {}
 local this = TheEyeAddon.UI.Components.ListenerValueChangeHandlers.VisibleStateHandler
 local inherited = TheEyeAddon.UI.Components.ListenerValueChangeHandlers.KeyStateFunctionCaller
 
+local EnabledStateReactorSetup = TheEyeAddon.UI.Components.ListenerValueChangeHandlers.EnabledStateReactor.Setup
+
 
 --[[ #this#TEMPLATE#
 {
     #inherited#TEMPLATE#
+    -- @TODO add DisplayData Template
 }
 ]]
+
+
+--[[ SETUP
+    instance
+    UIObject                    UIObject
+]]
+function this:Setup(
+    instance,
+    UIObject
+)
+    
+    inherited:Setup(
+        instance,
+        UIObject,
+        this.Show, -- @TODO
+        this.Hide -- @TODO
+    )
+    
+    instance.OnEnable = this.OnEnable -- @TODO
+    instance.OnDisable = this.OnDisable -- @TODO
+
+    instance.EnabledStateReactor = {}
+    EnabledStateReactorSetup(
+        instance.EnabledStateReactor,
+        UIObject,
+        instance.OnEnable,
+        instance.OnDisable
+    )
+end
