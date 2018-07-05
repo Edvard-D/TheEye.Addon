89838e1624bcab0eebb3e9651206f3bae0142e5d
EdvardXYZ <eddumansky@gmail.com>
2018-07-05 20:14:10 +0300

    Create EnabledStateReactor.lua file

---
+++ UI/Components/ListenerValueChangeHandlers/EnabledStateReactor.lua
@@ -0,0 +1,11 @@
+local TheEyeAddon = TheEyeAddon
+TheEyeAddon.UI.Components.ListenerValueChangeHandlers.EnabledStateReactor = {}
+local this = TheEyeAddon.UI.Components.ListenerValueChangeHandlers.EnabledStateReactor
+local inherited = TheEyeAddon.UI.Components.ListenerValueChangeHandlers.Base
+
+
+--[[ #this#TEMPLATE#
+{
+    #inherited#TEMPLATE#
+}
+]]
