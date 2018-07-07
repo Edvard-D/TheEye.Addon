bf69c9bcaa07dee180589a3ae1240b9c5fe00ea6
EdvardXYZ <eddumansky@gmail.com>
2018-07-07 21:59:34 +0300

    Define NotifyBasedFunctionCaller:Setup function

--- UI/Components/ListenerGroups.lua/NotifyBasedFunctionCaller.lua
+++ UI/Components/ListenerGroups.lua/NotifyBasedFunctionCaller.lua
@@ -3,9 +3,33 @@ TheEyeAddon.UI.Objects.Components.ListenerGroups.NotifyBasedFunctionCaller = {}
 local this = TheEyeAddon.UI.Objects.Components.ListenerGroups.NotifyBasedFunctionCaller
 local inherited = TheEyeAddon.UI.Objects.Components.ListenerGroups.Base
 
+local EventListenerSetup = TheEyeAddon.UI.Objects.Components.Listeners.EventListener.Setup
+
 
 --[[ #this#TEMPLATE#
 {
     #inherited#TEMPLATE#
 }
 ]]
+
+
+--[[ #SETUP#
+    instance
+    UIObject                    UIObject
+    OnNotify                    function(...)
+]]
+function this:Setup(
+    instance,
+    UIObject,
+    OnNotify
+)
+
+    inherited:Setup(
+        instance,
+        UIObject,
+        EventListenerSetup,
+        OnNotify,
+        nil,
+        nil
+    )
+end
