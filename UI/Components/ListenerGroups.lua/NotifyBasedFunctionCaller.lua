cad9514b997b9d0328f66299d0c9430423841f4e
EdvardXYZ <eddumansky@gmail.com>
2018-07-07 21:58:32 +0300

    Create ListenerGroups\NotifyBasedFunctionCaller.lua file

---
+++ UI/Components/ListenerGroups.lua/NotifyBasedFunctionCaller.lua
@@ -0,0 +1,11 @@
+local TheEyeAddon = TheEyeAddon
+TheEyeAddon.UI.Objects.Components.ListenerGroups.NotifyBasedFunctionCaller = {}
+local this = TheEyeAddon.UI.Objects.Components.ListenerGroups.NotifyBasedFunctionCaller
+local inherited = TheEyeAddon.UI.Objects.Components.ListenerGroups.Base
+
+
+--[[ #this#TEMPLATE#
+{
+    #inherited#TEMPLATE#
+}
+]]
