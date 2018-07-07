dc19118ac63361cda17380f90f5ca680aac4fa61
EdvardXYZ <eddumansky@gmail.com>
2018-07-07 21:03:05 +0300

    Delete Enabled/Visible functions from ValueHandlers.lua
    This is now handled by EnabledStateHandler and VisibleStateHandler.

--- UI/Objects/Helpers/ValueHandlers.lua
+++ UI/Objects/Helpers/ValueHandlers.lua
@@ -23,34 +23,3 @@ function TheEyeAddon.UI.Objects.ValueHandlers:OnSortRankChanged(valueChange)
 
     TheEyeAddon.Events.Coordinator:SendCustomEvent("UIOBJECT_SORTRANK_CHANGED", uiObject)
 end
-
-
--- OnValidValue
-function TheEyeAddon.UI.Objects.ValueHandlers:Enable(uiObject)
-    print ("ENABLE    " .. uiObject.key) -- DEBUG
-    TheEyeAddon.UI.Objects.ListenerGroups:SetupGroup(uiObject, uiObject.ListenerGroups.Visible)
-    TheEyeAddon.UI.Objects.ListenerGroups:SetupGroupsOfType(uiObject, "EVENT")
-    TheEyeAddon.Events.Coordinator:SendCustomEvent("UIOBJECT_ENABLED", uiObject)
-end
-
-function TheEyeAddon.UI.Objects.ValueHandlers:Show(uiObject)
-    print ("SHOW    " .. uiObject.key) -- DEBUG
-    uiObject.frame = uiObject.DisplayData.factory:Claim(uiObject, uiObject.DisplayData)
-    TheEyeAddon.Events.Coordinator:SendCustomEvent("UIOBJECT_SHOWN", uiObject)
-end
-
-
--- OnInvalidValue
-function TheEyeAddon.UI.Objects.ValueHandlers:Disable(uiObject)
-    print ("DISABLE    " .. uiObject.key) -- DEBUG
-    TheEyeAddon.UI.Objects.ListenerGroups:TeardownGroup(uiObject.ListenerGroups.Visible)
-    TheEyeAddon.UI.Objects.ListenerGroups:TeardownGroupsOfType(uiObject, "EVENT")
-    TheEyeAddon.Events.Coordinator:SendCustomEvent("UIOBJECT_DISABLED", uiObject)
-end
-
-function TheEyeAddon.UI.Objects.ValueHandlers:Hide(uiObject)
-    print ("HIDE    " .. uiObject.key) -- DEBUG
-    TheEyeAddon.UI.Pools:Release(uiObject.frame)
-    uiObject.frame = nil
-    TheEyeAddon.Events.Coordinator:SendCustomEvent("UIOBJECT_HIDDEN", uiObject)
-end
