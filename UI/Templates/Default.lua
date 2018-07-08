e42d1892b6ec3130327f58cf52bc8096f0bb256b
EdvardXYZ <eddumansky@gmail.com>
2018-07-08 19:06:37 +0300

    Define Default:Setup function

--- UI/Templates/Default.lua
+++ UI/Templates/Default.lua
@@ -3,3 +3,14 @@ TheEyeAddon.UI.Templates.Default = {}
 local this = TheEyeAddon.UI.Templates.Default
 
 this.tags = {}
+
+local EnabledStateSetup = TheEyeAddon.UI.Components.EnabledState.Setup
+
+
+function this:Setup(UIObject)
+    UIObject.EnabledState = {}
+    EnabledStateSetup(
+        UIObject.EnabledState,
+        UIObject
+    )
+end
