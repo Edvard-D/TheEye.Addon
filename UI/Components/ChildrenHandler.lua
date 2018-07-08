bd8c58a241e72d046c89d76dd07e8aafbe78cca7
EdvardXYZ <eddumansky@gmail.com>
2018-07-08 13:37:53 +0300

    Define ChildrenHandler:Arrange function

--- UI/Components/ChildrenHandler.lua
+++ UI/Components/ChildrenHandler.lua
@@ -4,7 +4,9 @@ local this = TheEyeAddon.UI.Components.ChildrenHandler
 
 local EnabledStateReactorSetup = TheEyeAddon.UI.Components.ListenerValueChangeHandlers.EnabledStateReactor.Setup
 local NotifyBasedFunctionCallerSetup = TheEyeAddon.UI.Objects.Components.ListenerGroups.NotifyBasedFunctionCaller.Setup
+local select = select
 local SortedTableSetup = TheEyeAddon.UI.Objects.Components.ValueHandlers.SortedTable.Setup
+local table = table
 
 
 --[[ #this#TEMPLATE#
@@ -80,7 +82,7 @@ function this:Setup(
     NotifyBasedFunctionCallerSetup(
         instance.ListenerGroups.Arrange,
         UIObject,
-        this.Arrange -- @TODO
+        this.Arrange
     )
 
     NotifyBasedFunctionCallerSetup(
@@ -121,6 +123,36 @@ function this:OnDisable()
     for i=1, #listenerGroups do
         listenerGroups[i]:Deactivate()
     end
+end
+
+
+-- Arrange
+function this:Arrange()
+    local children = self.ValueHandler.value
+    local frame = self.UIObject.frame
+    local groupArranger = self.GroupArranger
+    local combinedOffsetX = 0
+    local combinedOffsetY = 0
+    local childRects = {}
+
+    for i = 1, #children do
+        local childFrame = children[i].frame
+		local currentOffsetX, currentOffsetY = select(4, childFrame:GetPoint(1))
+		
+		if currentOffsetX ~= combinedOffsetX or currentOffsetY ~= combinedOffsetY then
+			childFrame:ClearAllPoints()
+			childFrame:SetPoint(groupArranger.point, frame, groupArranger.relativePoint, combinedOffsetX, combinedOffsetY)
+		end
+
+		table.insert(childRects, { childFrame:GetRect() })
+		combinedOffsetX, combinedOffsetY = groupArranger.UpdateOffset(combinedOffsetX, combinedOffsetY, childFrame)
+	end
+	
+	if #childRects > 0 then
+		frame:SetSizeWithEvent(GetSizeFromRects(childRects)) -- @TODO
+	else
+		frame:SetSizeWithEvent(0, 0)
+	end
 end
 
 
