2b6ef22198f25347aaaa54927702aec465436b8c
EdvardXYZ <eddumansky@gmail.com>
2018-07-08 15:11:32 +0300

    Create ListenerValueChangeHandlers\SortRanker.lua file

---
+++ UI/Components/Elements/ListenerValueChangeHandlers/SortRanker.lua
@@ -0,0 +1,11 @@
+local TheEyeAddon = TheEyeAddon
+TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.SortRanker = {}
+local this = TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.SortRanker
+local inherited = TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.Base
+
+
+--[[ #this#TEMPLATE#
+{
+    #inherited#TEMPLATE#
+}
+]]
