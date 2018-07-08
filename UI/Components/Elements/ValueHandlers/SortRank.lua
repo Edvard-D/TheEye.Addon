036f80dea986b37c335a309096d1605803346895
EdvardXYZ <eddumansky@gmail.com>
2018-07-08 14:36:14 +0300

    Define SortRank:Setup function

--- UI/Components/Elements/ValueHandlers/SortRank.lua
+++ UI/Components/Elements/ValueHandlers/SortRank.lua
@@ -7,5 +7,28 @@ local inherited = TheEyeAddon.UI.Components.Elements.ValueHandlers.Base
 --[[ #this#TEMPLATE#
 {
     #inherited#TEMPLATE#
+    defaultValue = #INT#
 }
 ]]
+
+
+--[[ #SETUP#
+    instance
+    UIObject                    UIObject
+    OnValueChange               function(#VALUE#)
+]]
+function this:Setup(
+    instance,
+    UIObject,
+    OnValueChange
+)
+
+    inherited:Setup(
+        instance,
+        UIObject,
+        nil,
+        nil,
+        OnValueChange,
+        instance.defaultValue
+    )
+end
