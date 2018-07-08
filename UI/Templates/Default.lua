7d21dfde019310d3b891f42dcaefa4f85bc599d2
EdvardXYZ <eddumansky@gmail.com>
2018-07-08 19:09:16 +0300

    Change structure of Template files
    Templates don't need to be looked up using key values, so it's more
    efficient to save the data in an array.

--- UI/Templates/Default.lua
+++ UI/Templates/Default.lua
@@ -1,16 +1,12 @@
 local TheEyeAddon = TheEyeAddon
-TheEyeAddon.UI.Templates.Default = {}
-local this = TheEyeAddon.UI.Templates.Default
-
-this.tags = {}
-
-local EnabledStateSetup = TheEyeAddon.UI.Components.EnabledState.Setup
-
-
-function this:Setup(UIObject)
-    UIObject.EnabledState = {}
-    EnabledStateSetup(
-        UIObject.EnabledState,
-        UIObject
-    )
-end
+table.insert(TheEyeAddon.UI.Templates, 
+{
+    tags = {},
+    Setup = function(UIObject)
+        UIObject.EnabledState = {}
+        EnabledStateSetup(
+            UIObject.EnabledState,
+            UIObject
+        )
+    end
+})
