aef311aa7884b51d2efa5541f69c93c03fd7e968
EdvardXYZ <eddumansky@gmail.com>
2018-07-08 15:12:07 +0300

    Define SortRanker:Setup function

--- UI/Components/Elements/ListenerValueChangeHandlers/SortRanker.lua
+++ UI/Components/Elements/ListenerValueChangeHandlers/SortRanker.lua
@@ -3,9 +3,38 @@ TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.SortRanker = {}
 local this = TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.SortRanker
 local inherited = TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.Base
 
+local SortRankSetup = TheEyeAddon.UI.Components.Elements.ValueHandlers.SortRank.Setup
+
 
 --[[ #this#TEMPLATE#
 {
     #inherited#TEMPLATE#
+    ListenerGroup = #TheEyeAddon.UI.Components.Elements.ListenerGroups#NAME#TEMPLATE#
 }
 ]]
+
+
+--[[ SETUP
+    instance
+    UIObject                    UIObject
+]]
+function this:Setup(
+    instance,
+    UIObject
+)
+
+    instance.OnSortRankChange = this.OnEnabledChanged -- @TODO
+
+    instance.ValueHandler = {}
+    SortRankSetup(
+        instance.ValueHandler,
+        UIObject,
+        instance.OnSortRankChange
+    )
+
+    inherited:Setup(
+        instance,
+        UIObject,
+        instance.ListenerGroup
+    )
+end
