963dab3e33547db883a9ca9d0096b0df21cda06a
EdvardXYZ <eddumansky@gmail.com>
2018-07-08 10:52:07 +0300

    Create ValueHandlers\TableBase.lua file

---
+++ UI/Components/ValueHandlers/TableBase.lua
@@ -0,0 +1,11 @@
+local TheEyeAddon = TheEyeAddon
+TheEyeAddon.UI.Objects.Components.ValueHandlers.TableBase = {}
+local this = TheEyeAddon.UI.Objects.Components.ValueHandlers.TableBase
+local inherited = TheEyeAddon.UI.Objects.Components.ValueHandlers.Base
+
+
+--[[ #this#TEMPLATE#
+{
+    #inherited#TEMPLATE#
+}
+]]
