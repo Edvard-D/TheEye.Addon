6a25b0a72cde786ae81b37d8668240d77cec6c8e
EdvardXYZ <eddumansky@gmail.com>
2018-07-08 16:03:16 +0300

    Define PriorityRank:Setup function
    The #isDynamic#TRUE/FALSE# declaration in the template means that the
    template to use depends on what the isDynamic value is set to.

--- UI/Components/PriorityRank.lua
+++ UI/Components/PriorityRank.lua
@@ -2,9 +2,52 @@ local TheEyeAddon = TheEyeAddon
 TheEyeAddon.UI.Components.PriorityRank = {}
 local this = TheEyeAddon.UI.Components.PriorityRank
 
+local DynamicSortRankerSetup = TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.DynamicSortRanker.Setup
+local SortRankSetup = TheEyeAddon.UI.Components.Elements.ValueHandlers.SortRank.Setup
+
 
 --[[ #this#TEMPLATE#
 {
-    nil
+    isDynamic = #BOOL#
+}
+
+#isDynamic#TRUE#
+{
+    #this#TEMPLATE#
+    {
+        #TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.DynamicSortRanker#TEMPLATE#
+    }
+}
+
+#isDynamic#FALSE#
+{
+    #this#TEMPLATE#
+    {
+        #TheEyeAddon.UI.Components.Elements.ValueHandlers.SortRank#TEMPLATE#
+    }
 }
 ]]
+
+
+--[[ SETUP
+    instance
+    UIObject                    UIObject
+]]
+function this:Setup(
+    instance,
+    UIObject
+)
+
+    if instance.isDynamic == true then
+        DynamicSortRankerSetup:Setup(
+            instance,
+            UIObject
+        )
+    else
+        SortRankSetup:Setup(
+            instance,
+            UIObject,
+            nil
+        )
+    end
+end
