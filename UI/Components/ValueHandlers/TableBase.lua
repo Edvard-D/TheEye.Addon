f5d68247d2a584c06a1dfb86ee1d9e6210df64c1
EdvardXYZ <eddumansky@gmail.com>
2018-07-08 10:52:45 +0300

    Define TableBase:Setup function

--- UI/Components/ValueHandlers/TableBase.lua
+++ UI/Components/ValueHandlers/TableBase.lua
@@ -9,3 +9,31 @@ local inherited = TheEyeAddon.UI.Objects.Components.ValueHandlers.Base
     #inherited#TEMPLATE#
 }
 ]]
+
+
+--[[ #SETUP#
+    instance
+    UIObject                    UIObject
+    OnTableValuesChange         function()
+]]
+function this:Setup(
+    instance,
+    UIObject,
+    OnTableValuesChange
+)
+
+    instance.OnTableChange = this.OnTableChange -- @TODO
+    inherited:Setup(
+        instance,
+        UIObject,
+        nil,
+        nil,
+        instance.OnTableChange,
+        {}
+    )
+
+    instance.OnTableValuesChange = OnTableValuesChange
+
+    instance.Insert = this.Insert -- @TODO
+    instance.Remove = this.Remove -- @TODO
+end
