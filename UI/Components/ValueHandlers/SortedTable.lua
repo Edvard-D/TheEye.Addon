747b8555401df867a417116aac6c14e87fdeaf94
EdvardXYZ <eddumansky@gmail.com>
2018-07-08 12:54:15 +0300

    Create ValueHandlers\SortedTablue.lua file

---
+++ UI/Components/ValueHandlers/SortedTable.lua
@@ -0,0 +1,11 @@
+local TheEyeAddon = TheEyeAddon
+TheEyeAddon.UI.Objects.Components.ValueHandlers.SortedTable = {}
+local this = TheEyeAddon.UI.Objects.Components.ValueHandlers.SortedTable
+local inherited = TheEyeAddon.UI.Objects.Components.ValueHandlers.TableBase
+
+
+--[[ #this#TEMPLATE#
+{
+    #inherited#TEMPLATE#
+}
+]]
