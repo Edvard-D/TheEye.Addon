d41f5acadaa89596d38e3b19db8af59d7e6e06c1
EdvardXYZ <eddumansky@gmail.com>
2018-07-08 12:54:44 +0300

    Define ValueHandlers:Setup function

--- UI/Components/ValueHandlers/SortedTable.lua
+++ UI/Components/ValueHandlers/SortedTable.lua
@@ -9,3 +9,28 @@ local inherited = TheEyeAddon.UI.Objects.Components.ValueHandlers.TableBase
     #inherited#TEMPLATE#
 }
 ]]
+
+
+--[[ #SETUP#
+    instance
+    UIObject                    UIObject
+    sortActionnName             string
+    sortValueComponentName      string
+]]
+function this:Setup(
+    instance,
+    UIObject,
+    sortActionName,
+    sortValueComponentName
+)
+
+    instance.Sort = this.Sort -- @TODO
+    inherited:Setup(
+        instance,
+        UIObject,
+        instance.Sort
+    )
+
+    instance.SortAction = this[sortActionName]
+    instance.sortValueComponentName = sortValueComponentName
+end
