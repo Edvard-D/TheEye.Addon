98af664f84f7865631299d941c524029e5a2408a
EdvardXYZ <eddumansky@gmail.com>
2018-07-08 14:34:40 +0300

    Create ValueHandlers\SortRank.lua file

---
+++ UI/Components/Elements/ValueHandlers/SortRank.lua
@@ -0,0 +1,11 @@
+local TheEyeAddon = TheEyeAddon
+TheEyeAddon.UI.Components.Elements.ValueHandlers.SortRank = {}
+local this = TheEyeAddon.UI.Components.Elements.ValueHandlers.SortRank
+local inherited = TheEyeAddon.UI.Components.Elements.ValueHandlers.Base
+
+
+--[[ #this#TEMPLATE#
+{
+    #inherited#TEMPLATE#
+}
+]]
