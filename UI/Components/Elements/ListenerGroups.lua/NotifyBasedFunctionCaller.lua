f0ab303cf45867c30c648a5921c9d4539ec02e48
EdvardXYZ <eddumansky@gmail.com>
2018-07-11 10:10:25 +0300

    Replace use of EventListener with EvaluatorListener

--- UI/Components/Elements/ListenerGroups.lua/NotifyBasedFunctionCaller.lua
+++ UI/Components/Elements/ListenerGroups.lua/NotifyBasedFunctionCaller.lua
@@ -3,7 +3,7 @@ TheEyeAddon.UI.Components.Elements.ListenerGroups.NotifyBasedFunctionCaller = {}
 local this = TheEyeAddon.UI.Components.Elements.ListenerGroups.NotifyBasedFunctionCaller
 local inherited = TheEyeAddon.UI.Components.Elements.ListenerGroups.Base
 
-local EventListenerSetup = TheEyeAddon.UI.Components.Elements.Listeners.EventListener.Setup
+local EvaluatorListenerSetup = TheEyeAddon.UI.Components.Elements.Listeners.EvaluatorListener.Setup
 
 
 --[[ #this#TEMPLATE#
@@ -27,7 +27,7 @@ function this:Setup(
     inherited:Setup(
         instance,
         UIObject,
-        EventListenerSetup,
+        EvaluatorListenerSetup,
         OnNotify,
         nil,
         nil
