acf39751901212c5c1818688809d1389c7b655b6
EdvardXYZ <eddumansky@gmail.com>
2018-07-11 15:07:38 +0300

    Add file to correct name capitalization

---
+++ Namespaces.lua
@@ -0,0 +1,18 @@
+-- Used to create tables for folders in the file structure as they
+-- are part of the program hierarchy but don't have a dedicated file.
+TheEyeAddon = {}
+    TheEyeAddon.Events = {}
+        TheEyeAddon.Events.Evaluators = {}
+    TheEyeAddon.UI = {}
+        TheEyeAddon.UI.Components = {}
+            TheEyeAddon.UI.Components.Elements = {}
+                TheEyeAddon.UI.Components.Elements.ListenerGroups = {}
+                TheEyeAddon.UI.Components.Elements.Listeners = {}
+                TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers = {}
+                TheEyeAddon.UI.Components.Elements.ValueEvaluators = {}
+                TheEyeAddon.UI.Components.Elements.ValueHandlers = {}
+        TheEyeAddon.UI.DimensionTemplates = {}
+        TheEyeAddon.UI.Factories = {}
+        TheEyeAddon.UI.Fonts = {}
+        TheEyeAddon.UI.Objects = {}
+            TheEyeAddon.UI.Objects.Instances = {}
