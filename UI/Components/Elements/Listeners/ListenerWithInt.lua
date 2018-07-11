aaa79f8170294d7ec0f66a4c7ab38fcea2f5101c
EdvardXYZ <eddumansky@gmail.com>
2018-07-11 12:21:52 +0300

    Create Listeners\ListenerWithInt.lua file

---
+++ UI/Components/Elements/Listeners/ListenerWithInt.lua
@@ -0,0 +1,12 @@
+local TheEyeAddon = TheEyeAddon
+TheEyeAddon.UI.Components.Elements.Listeners.ListenerWithInt = {}
+local this = TheEyeAddon.UI.Components.Elements.Listeners.ListenerWithInt
+local inherited = TheEyeAddon.UI.Components.Elements.Listeners.Base
+
+
+--[[ #this#TEMPLATE#
+{
+    #inherited#TEMPLATE#
+    value = #INT#
+}
+]]
