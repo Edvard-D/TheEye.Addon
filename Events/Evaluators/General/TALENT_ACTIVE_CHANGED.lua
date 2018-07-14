c7ca63b865a7f4f9c0dd86b1079333a280111a1b
EdvardXYZ <eddumansky@gmail.com>
2018-07-14 16:38:58 +0300

    Implement TALENT_ACTIVE_CHANGED

--- Events/Evaluators/General/TALENT_ACTIVE_CHANGED.lua
+++ Events/Evaluators/General/TALENT_ACTIVE_CHANGED.lua
@@ -2,3 +2,39 @@ local TheEyeAddon = TheEyeAddon
 TheEyeAddon.Events.Evaluators.TALENT_ACTIVE_CHANGED = {}
 local this = TheEyeAddon.Events.Evaluators.TALENT_ACTIVE_CHANGED
 this.name = "TALENT_ACTIVE_CHANGED"
+
+local GetActiveSpecGroup = GetActiveSpecGroup
+local GetTalentInfo = GetTalentInfo
+local select = select
+
+
+--[[ #this#TEMPLATE#
+{
+    inputValues =
+    {
+        #LABEL#Tier# #INT#,
+        #LABEL#Column# #INT#
+    }
+}
+]]
+
+
+this.reevaluateEvents =
+{
+    PLAYER_TALENT_UPDATE = true
+}
+this.gameEvents = 
+{
+    "PLAYER_TALENT_UPDATE"
+}
+
+
+function this:CalculateCurrentState(inputValues)
+    local isKnown = select(11, GetTalentInfo(inputValues[1], inputValues[2], GetActiveSpecGroup()))
+    return isKnown
+end
+
+function this:Evaluate(valueGroup)
+    local isKnown = self:CalculateCurrentState(valueGroup.inputValues)
+    return true, this.name, isKnown
+end
