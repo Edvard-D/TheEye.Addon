8e2a7c5c22bd320125ae941d521159cfaca9ab3c
EdvardXYZ <eddumansky@gmail.com>
2018-07-14 16:32:15 +0300

    Create Evaluators\TALENT_ACTIVE_CHANGED.lua file

---
+++ Events/Evaluators/General/TALENT_ACTIVE_CHANGED.lua
@@ -0,0 +1,4 @@
+local TheEyeAddon = TheEyeAddon
+TheEyeAddon.Events.Evaluators.TALENT_ACTIVE_CHANGED = {}
+local this = TheEyeAddon.Events.Evaluators.TALENT_ACTIVE_CHANGED
+this.name = "TALENT_ACTIVE_CHANGED"
