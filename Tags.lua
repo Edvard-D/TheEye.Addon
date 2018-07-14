3b0ba6b53a114ed836b1b568d7cccb409be81727
EdvardXYZ <eddumansky@gmail.com>
2018-07-14 08:09:08 +0300

    Correct how has tag value is assigned
    string:find returns the place within the string that matches the
    substring being searched for rather than a true/false value.

--- Tags.lua
+++ Tags.lua
@@ -16,9 +16,9 @@ end
 function this.UIObjectHasTag(uiObject, tag)
     local tagGroup = GetTagGroup(tag)
     local uiObjectKey = uiObject.key
-    
+
     if tagGroup[uiObjectKey] == nil then
-        tagGroup[uiObjectKey] = uiObjectKey:find(tag)
+        tagGroup[uiObjectKey] = uiObjectKey:find(tag) ~= nil
     end
 
     return tagGroup[uiObjectKey]
