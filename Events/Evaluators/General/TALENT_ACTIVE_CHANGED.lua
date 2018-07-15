dcd4e49a5af962e0f03db7f81c1225168d2e7d24
EdvardXYZ <eddumansky@gmail.com>
2018-07-15 13:21:11 +0300

    Correct CalculateCurrentValue return
    isKnown doesn't work when a talent is selected by the player, only in
    cases like the class rings in Legion that give you the effect of a
    talent without having to talent it yourself.

--- Events/Evaluators/General/TALENT_ACTIVE_CHANGED.lua
+++ Events/Evaluators/General/TALENT_ACTIVE_CHANGED.lua
@@ -21,22 +21,25 @@ local select = select
 
 this.reevaluateEvents =
 {
-    PLAYER_TALENT_UPDATE = true
+    PLAYER_TALENT_UPDATE = true,
+    UNIT_INVENTORY_CHANGED = true,
 }
 this.gameEvents = 
 {
-    "PLAYER_TALENT_UPDATE"
+    "PLAYER_TALENT_UPDATE",
+    "UNIT_INVENTORY_CHANGED",
 }
 
 
 function this:CalculateCurrentValue(inputValues)
-    local isKnown = select(11, GetTalentInfo(inputValues[1], inputValues[2], GetActiveSpecGroup()))
-    return isKnown
+    local isSelected, _, _, _, _, _, _, isKnown = select(4, GetTalentInfo(inputValues[1], inputValues[2], GetActiveSpecGroup()))
+    return isSelected or isKnown
 end
 
 function this:Evaluate(inputGroup)
     local isKnown = self:CalculateCurrentValue(inputGroup.inputValues)
 
+    print("isKnown: " .. tostring(isKnown))
     if self.currentValue ~= isKnown then
         self.currentValue = isKnown
         return true, this.name, isKnown
