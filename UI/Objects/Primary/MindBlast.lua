24c40e30b8f22caaf36f25eb5c934f98d4a0f6f4
EdvardXYZ <eddumansky@gmail.com>
2018-07-17 12:43:11 +0300

    Create Objects\Primary\MindBlast file

---
+++ UI/Objects/Primary/MindBlast.lua
@@ -0,0 +1,79 @@
+local spellID = 8092
+
+TheEyeAddon.UI.Objects:FormatData(
+{
+    tags = { "HUD", "ICON", "PRIMARY", "SPELL-8092", },
+    DisplayData =
+    {
+        factory = TheEyeAddon.UI.Factories.Icon,
+        DimensionTemplate = TheEyeAddon.UI.DimensionTemplates.Icon_Large,
+        iconObjectType = "SPELL",
+        iconObjectID = spellID,
+    },
+    -- @TODO cooldown
+    EnabledState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [2] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UIOBJECT_VISIBLE_CHANGED",
+                    inputValues = { --[[uiObjectKey]] "GROUP_HUD_MODULE_PRIMARY", },
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "TALENT_ACTIVE_CHANGED",
+                    inputValues = { --[[tier]] 1, --[[column]] 3, },
+                    value = 4,
+                },
+            },
+        },
+    },
+    PriorityRank =
+    {
+        isDynamic = false,
+        ValueHandler =
+        {
+            defaultValue = 3,
+        },
+    },
+    VisibleState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [4] = true, [6] = true, [8] = true, [10] = true,  [14] = true,  },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UNIT_SPELLCAST_START_RECENTLY_CHANGED",
+                    inputValues = { --[[unit]] "player", --[[spellID]] spellID, },
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "SPELL_COOLDOWN_DURATION_CHANGED",
+                    inputValues = { --[[spellID]] spellID },
+                    comparisonValues =
+                    {
+                        value = 0.75,
+                        type = "LessThan"
+                    },
+                    value = 4,
+                },
+                {
+                    eventEvaluatorKey = "UNIT_SPELLCAST_ACTIVE_CHANGED",
+                    inputValues = { --[[unit]] "player", --[[spellID]] spellID, },
+                    value = 8,
+                },
+            },
+        },
+    },
+}
+)
