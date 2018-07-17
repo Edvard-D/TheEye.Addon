1e68345a282d11abaa55830a9189ea47c67ed2a1
EdvardXYZ <eddumansky@gmail.com>
2018-07-17 12:35:48 +0300

    Create ShadowFiend file

---
+++ UI/Objects/Primary/ShadowFiend.lua
@@ -0,0 +1,74 @@
+local spellID = 34433
+
+TheEyeAddon.UI.Objects:FormatData(
+{
+    tags = { "HUD", "ICON", "PRIMARY", "SPELL-34433", },
+    DisplayData =
+    {
+        factory = TheEyeAddon.UI.Factories.Icon,
+        DimensionTemplate = TheEyeAddon.UI.DimensionTemplates.Icon_Large,
+        iconObjectType = "SPELL",
+        iconObjectID = spellID,
+    },
+    -- @TODO cooldown
+    EnabledState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [2] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UIOBJECT_VISIBLE_CHANGED",
+                    inputValues = { --[[uiObjectKey]] "GROUP_HUD_MODULE_PRIMARY", },
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "TALENT_ACTIVE_CHANGED",
+                    inputValues = { --[[tier]] 6, --[[column]] 2, },
+                    value = 4,
+                },
+            },
+        },
+    },
+    PriorityRank =
+    {
+        isDynamic = false,
+        ValueHandler =
+        {
+            defaultValue = 5,
+        },
+    },
+    VisibleState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [2] = true, [4] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UNIT_SPELLCAST_START_RECENTLY_CHANGED",
+                    inputValues = { --[[unit]] "player", --[[spellID]] spellID, },
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "SPELL_COOLDOWN_DURATION_CHANGED",
+                    inputValues = { --[[spellID]] spellID, },
+                    value = 4,
+                    comparisonValues =
+                    {
+                        value = 0,
+                        type = "EqualTo"
+                    },
+                },
+            },
+        },
+    },
+}
+)
