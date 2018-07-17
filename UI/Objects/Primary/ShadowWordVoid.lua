c8e629fe0bb23b923b0b08d5df178aa1bdaa9e56
EdvardXYZ <eddumansky@gmail.com>
2018-07-17 23:36:56 +0300

    Create ShadowWordVoid file

---
+++ UI/Objects/Primary/ShadowWordVoid.lua
@@ -0,0 +1,79 @@
+local spellID = 205351
+
+TheEyeAddon.UI.Objects:FormatData(
+{
+    tags = { "HUD", "ICON", "PRIMARY", "SPELL-205351", },
+    DisplayData =
+    {
+        factory = TheEyeAddon.UI.Factories.Icon,
+        DimensionTemplate = TheEyeAddon.UI.DimensionTemplates.Icon_Large,
+        iconObjectType = "SPELL",
+        iconObjectID = spellID,
+    },
+    -- @TODO cooldown
+    EnabledState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [6] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UIOBJECT_VISIBLE_CHANGED",
+                    inputValues = { --[[uiObjectKey]] "GROUP_HUD_MODULE_PRIMARY", },
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "TALENT_ACTIVE_CHANGED",
+                    inputValues = { --[[tier]] 1, --[[column]] 3, },
+                    value = 4,
+                },
+            },
+        },
+    },
+    PriorityRank =
+    {
+        isDynamic = false,
+        ValueHandler =
+        {
+            defaultValue = 3,
+        },
+    },
+    VisibleState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [2] = true, [6] = true, [8] = true, [10] = true, [12] = true, [14] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UNIT_SPELLCAST_START_RECENTLY_CHANGED",
+                    inputValues = { --[[unit]] "player", --[[spellID]] spellID, },
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "UNIT_SPELLCAST_ACTIVE_CHANGED",
+                    inputValues = { --[[unit]] "player", --[[spellID]] spellID, },
+                    value = 4,
+                },
+                {
+                    eventEvaluatorKey = "SPELL_CHARGE_COOLDOWN_DURATION_CHANGED",
+                    inputValues = { --[[spellID]] spellID },
+                    comparisonValues =
+                    {
+                        value = TheEyeAddon.Values.cooldownEndAlertLength,
+                        type = "LessThan"
+                    },
+                    value = 8,
+                },
+            },
+        },
+    },
+}
+)
