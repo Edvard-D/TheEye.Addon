dd3fba5fcf264a4f29e3f13a8a05511e6815e9fb
EdvardXYZ <eddumansky@gmail.com>
2018-07-17 23:58:08 +0300

    Add Mindbender file

---
+++ UI/Objects/Primary/Mindbender.lua
@@ -0,0 +1,84 @@
+local spellID = 200174
+
+TheEyeAddon.UI.Objects:FormatData(
+{
+    tags = { "HUD", "ICON", "PRIMARY", "SPELL-200174", },
+    DisplayData =
+    {
+        factory = TheEyeAddon.UI.Factories.Icon,
+        DimensionTemplate = TheEyeAddon.UI.DimensionTemplates.Icon_Large,
+        iconObjectType = "SPELL",
+        iconObjectID = spellID,
+    },
+    -- @TODO cooldown
+    EnabledState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [6] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UIOBJECT_VISIBLE_CHANGED",
+                    inputValues = { --[[uiObjectKey]] "GROUP_HUD_MODULE_PRIMARY", },
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "TALENT_ACTIVE_CHANGED",
+                    inputValues = { --[[tier]] 6, --[[column]] 2, },
+                    value = 4,
+                },
+            },
+        },
+    },
+    PriorityRank =
+    {
+        isDynamic = false,
+        ValueHandler =
+        {
+            defaultValue = 8,
+        },
+    },
+    VisibleState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [2] = true, [12] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UNIT_SPELLCAST_START_RECENTLY_CHANGED",
+                    inputValues = { --[[unit]] "player", --[[spellID]] spellID, },
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "SPELL_COOLDOWN_DURATION_CHANGED",
+                    inputValues = { --[[spellID]] spellID, },
+                    comparisonValues =
+                    {
+                        value = TheEyeAddon.Values.cooldownEndAlertLength,
+                        type = "LessThan"
+                    },
+                    value = 4,
+                },
+                {
+                    eventEvaluatorKey = "AURA_STACK_CHANGED",
+                    inputValues = { --[[spellID]] 194249, },
+                    comparisonValues =
+                    {
+                        value = 9,
+                        type = "GreaterThan"
+                    },
+                    value = 8,
+                },
+            },
+        },
+    },
+}
+)
