178c73b83ad0d22411f0c92cfd6832a3f9ae2c75
EdvardXYZ <eddumansky@gmail.com>
2018-07-19 23:45:07 +0300

    Remove unnecessary value assignments

--- UI/Components/Children.lua
+++ UI/Components/Children.lua
@@ -172,8 +172,6 @@ function this:Arrange()
         for i = 1, #children do
             local childUIObject = children[i]
             local childFrame = childUIObject.Frame
-                local currentOffsetX, currentOffsetY = select(4, childFrame:GetPoint(1))
-                
                 local childPointSettings = childFrame.UIObject.DisplayData.DimensionTemplate.PointSettings
                 childFrame:ClearAllPoints()
                 groupArranger.SetPoint(frame, childFrame, childPointSettings, combinedOffsetX, combinedOffsetY)
