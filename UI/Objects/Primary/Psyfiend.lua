f3c00fa72fc5ff96326f06d00c0408b545bee007
EdvardXYZ <eddumansky@gmail.com>
2018-07-19 20:34:52 +0300

    Add Psyfiend file

---
+++ UI/Objects/Primary/Psyfiend.lua
@@ -0,0 +1,79 @@
+local spellID = 211522
+
+TheEyeAddon.UI.Objects:FormatData(
+{
+    tags = { "HUD", "ICON", "PRIMARY", "SPELL-211522", },
+    DisplayData =
+    {
+        factory = TheEyeAddon.UI.Factories.Icon,
+        DimensionTemplate = TheEyeAddon.UI.DimensionTemplates.Icon_Large,
+        iconObjectType = "SPELL",
+        iconObjectID = spellID,
+    },
+    -- @TODO cooldown
+    EnabledState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [14] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UIOBJECT_VISIBLE_CHANGED",
+                    inputValues = { --[[uiObjectKey]] "GROUP_HUD_MODULE_PRIMARY", },
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "PLAYER_TALENT_KNOWN_CHANGED",
+                    inputValues = { --[[talentID]] 763, },
+                    value = 4,
+                },
+                {
+                    eventEvaluatorKey = "UNIT_PVP_FLAGGED_CHANGED",
+                    inputValues = { --[[unit]] "player" },
+                    value = 8,
+                },
+            },
+        },
+    },
+    PriorityRank =
+    {
+        isDynamic = false,
+        ValueHandler =
+        {
+            defaultValue = 8,
+        },
+    },
+    VisibleState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [2] = true, [4] = true, [6] = true },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UNIT_SPELLCAST_START_RECENTLY_CHANGED",
+                    inputValues = { --[[unit]] "player", --[[spellID]] spellID, },
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "PLAYER_SPELL_COOLDOWN_DURATION_CHANGED",
+                    inputValues = { --[[spellID]] spellID, },
+                    comparisonValues =
+                    {
+                        value = TheEyeAddon.Values.cooldownEndAlertLength,
+                        type = "LessThan"
+                    },
+                    value = 4,
+                },
+            },
+        },
+    },
+}
+)
