80cad4ebc4728d54d47d97d34d83cf27f8785396
EdvardXYZ <eddumansky@gmail.com>
2018-07-21 00:20:09 +0300

    Change Voidform priority

--- UI/Objects/HUD/Active/Voidform.lua
+++ UI/Objects/HUD/Active/Voidform.lua
@@ -34,7 +34,7 @@ TheEyeAddon.UI.Objects:FormatData(
         isDynamic = false,
         ValueHandler =
         {
-            defaultValue = 1,
+            defaultValue = 6,
         },
     },
     VisibleState =
