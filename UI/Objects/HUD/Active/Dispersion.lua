f29b1b94eea8b5e350931507f6d5ae383f7878f9
EdvardXYZ <eddumansky@gmail.com>
2018-07-21 00:46:03 +0300

    Add HUD\Active\Dispersion.lua file

---
+++ UI/Objects/HUD/Active/Dispersion.lua
@@ -0,0 +1,58 @@
+local spellID = 47585
+
+TheEyeAddon.UI.Objects:FormatData(
+{
+    tags = { "HUD", "ICON", "ACTIVE", "SPELL-47585", },
+    DisplayData =
+    {
+        factory = TheEyeAddon.UI.Factories.Icon,
+        DimensionTemplate = TheEyeAddon.UI.DimensionTemplates.Icon.Medium,
+        iconObjectType = "SPELL",
+        iconObjectID = spellID,
+    },
+    EnabledState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [2] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UIOBJECT_VISIBLE_CHANGED",
+                    inputValues = { --[[uiObjectKey]] "HUD_MODULE_ACTIVE", },
+                    value = 2,
+                },
+            },
+        },
+    },
+    PriorityRank =
+    {
+        isDynamic = false,
+        ValueHandler =
+        {
+            defaultValue = 6,
+        },
+    },
+    VisibleState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [2] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UNIT_AURA_ACTIVE_CHANGED",
+                    inputValues = { --[[sourceUnit]] "player", --[[destUnit]] "player", --[[spellID]] 47585, },
+                    value = 2,
+                },
+            },
+        },
+    },
+}
+)
