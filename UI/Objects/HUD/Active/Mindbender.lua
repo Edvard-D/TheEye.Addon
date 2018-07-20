7639cb9e30d0d800c5e2122895022de500752017
EdvardXYZ <eddumansky@gmail.com>
2018-07-21 00:19:32 +0300

    Create HUD\Active\Mindbender.lua file

---
+++ UI/Objects/HUD/Active/Mindbender.lua
@@ -0,0 +1,63 @@
+local spellID = 200174
+
+TheEyeAddon.UI.Objects:FormatData(
+{
+    tags = { "HUD", "ICON", "ACTIVE", "SPELL-200174", },
+    DisplayData =
+    {
+        factory = TheEyeAddon.UI.Factories.Icon,
+        DimensionTemplate = TheEyeAddon.UI.DimensionTemplates.Icon.Medium,
+        iconObjectType = "SPELL",
+        iconObjectID = spellID,
+    },
+    EnabledState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [6] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UIOBJECT_VISIBLE_CHANGED",
+                    inputValues = { --[[uiObjectKey]] "HUD_MODULE_PRIMARY", },
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "PLAYER_TALENT_KNOWN_CHANGED",
+                    inputValues = { --[[talentID]] 21719, },
+                    value = 4,
+                },
+            },
+        },
+    },
+    PriorityRank =
+    {
+        isDynamic = false,
+        ValueHandler =
+        {
+            defaultValue = 3,
+        },
+    },
+    VisibleState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [2] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "PLAYER_TOTEM_ACTIVE_CHAGNED",
+                    inputValues = { --[[totemName]] "Mindbender", },
+                    value = 2,
+                },
+            },
+        },
+    },
+}
+)
