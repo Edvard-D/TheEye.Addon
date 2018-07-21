51ad6cf75964fcada4a206cf4f3dc932543bda1e
EdvardXYZ <eddumansky@gmail.com>
2018-07-21 22:17:37 +0300

    Add HUD\Active\Sephuz file

---
+++ UI/Objects/HUD/Active/Sephuz.lua
@@ -0,0 +1,63 @@
+local spellID = 208052
+
+TheEyeAddon.UI.Objects:FormatData(
+{
+    tags = { "HUD", "ICON", "ACTIVE", "SPELL-208052", },
+    DisplayData =
+    {
+        factory = TheEyeAddon.UI.Factories.Icon,
+        DimensionTemplate = TheEyeAddon.UI.DimensionTemplates.Icon.Medium,
+        iconObjectType = "SPELL",
+        iconObjectID = spellID,
+    },
+    EnabledState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [6] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UIOBJECT_VISIBLE_CHANGED",
+                    inputValues = { --[[uiObjectKey]] "HUD_MODULE_PRIMARY", },
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "PLAYER_ITEM_EQUIPPED_CHANGED",
+                    inputValues = { --[[itemID]] 132452, },
+                    value = 4,
+                },
+            },
+        },
+    },
+    PriorityRank =
+    {
+        isDynamic = false,
+        ValueHandler =
+        {
+            defaultValue = 4,
+        },
+    },
+    VisibleState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [2] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UNIT_AURA_ACTIVE_CHANGED",
+                    inputValues = { --[[sourceUnit]] "player", --[[destUnit]] "player", --[[spellID]] spellID, },
+                    value = 2,
+                },
+            },
+        },
+    },
+}
+)
