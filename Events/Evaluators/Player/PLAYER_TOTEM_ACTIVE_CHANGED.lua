e48968f1f2d6cc34fe15f4e2bfbf98f211eea94f
EdvardXYZ <eddumansky@gmail.com>
2018-07-21 21:13:05 +0300

    Update totem status when duration ends

--- Events/Evaluators/Player/PLAYER_TOTEM_ACTIVE_CHANGED.lua
+++ Events/Evaluators/Player/PLAYER_TOTEM_ACTIVE_CHANGED.lua
@@ -3,7 +3,9 @@ TheEyeAddon.Events.Evaluators.PLAYER_TOTEM_ACTIVE_CHANGED = {}
 local this = TheEyeAddon.Events.Evaluators.PLAYER_TOTEM_ACTIVE_CHANGED
 this.name = "PLAYER_TOTEM_ACTIVE_CHANGED"
 
+local InputGroupCooldownTimerStart = TheEyeAddon.Timers.InputGroupCooldownTimerStart
 local GetTotemInfo = GetTotemInfo
+local select = select
 
 
 --[[ #this#TEMPLATE#
@@ -19,6 +21,10 @@ local GetTotemInfo = GetTotemInfo
 this.gameEvents = 
 {
     "PLAYER_TOTEM_UPDATE",
+}
+this.customEvents =
+{
+    "PLAYER_TOTEM_TIMER_END",
 }
 
 
@@ -37,13 +43,30 @@ function this:InputGroupSetup(inputGroup)
     inputGroup.currentValue = CalculateCurrentValue(inputGroup.inputValues)
 end
 
-function this:GetKey(event, totemSlot)
-    local _, totemName = GetTotemInfo(totemSlot)
+function this:GetKey(event, ...)
+    local totemName
+
+    if event == "PLAYER_TOTEM_UPDATE" then
+        local totemSlot = ...
+        totemName = select(2, GetTotemInfo(totemSlot))
+    else
+        totemName = select(2, ...)
+    end
+
     return totemName
 end
 
-function this:Evaluate(inputGroup, event, totemSlot)
-    local isActive = GetTotemInfo(totemSlot)
+function this:Evaluate(inputGroup, event, ...)
+    local isActive
+    local totemSlot
+
+    if event == "PLAYER_TOTEM_UPDATE" then
+        totemSlot = ...
+        isActive, totemName, _, remainingTime = GetTotemInfo(totemSlot)
+        InputGroupCooldownTimerStart(inputGroup, remainingTime, "PLAYER_TOTEM_TIMER_END", totemName)
+    else -- PLAYER_TOTEM_TIMER_END
+        isActive = false
+    end
 
     if inputGroup.currentValue ~= isActive then
         inputGroup.currentValue = isActive
