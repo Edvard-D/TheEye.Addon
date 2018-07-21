35a412127023fd5aee1307d2b4e00c85ab7b49d1
EdvardXYZ <eddumansky@gmail.com>
2018-07-21 20:32:33 +0300

    Add HUD\Active\Psyfiend file

---
+++ UI/Objects/HUD/Active/Psyfiend.lua
@@ -0,0 +1,73 @@
+local spellID = 211522
+
+TheEyeAddon.UI.Objects:FormatData(
+{
+    tags = { "HUD", "ICON", "ACTIVE", "SPELL-211522", },
+    DisplayData =
+    {
+        factory = TheEyeAddon.UI.Factories.Icon,
+        DimensionTemplate = TheEyeAddon.UI.DimensionTemplates.Icon.Medium,
+        iconObjectType = "SPELL",
+        iconObjectID = spellID,
+    },
+    EnabledState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [14] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UIOBJECT_VISIBLE_CHANGED",
+                    inputValues = { --[[uiObjectKey]] "HUD_MODULE_PRIMARY", },
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "PLAYER_TALENT_KNOWN_CHANGED",
+                    inputValues = { --[[talentID]] 763, },
+                    value = 4,
+                },
+                {
+                    eventEvaluatorKey = "UNIT_PVP_FLAGGED_CHANGED",
+                    inputValues = { --[[unit]] "player" },
+                    value = 8,
+                },
+            },
+        },
+    },
+    PriorityRank =
+    {
+        isDynamic = false,
+        ValueHandler =
+        {
+            defaultValue = 3,
+        },
+    },
+    VisibleState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [2] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UNIT_SPELLCAST_SUCCEEDED_ELAPSED_TIME_CHANGED",
+                    inputValues = { --[[unit]] "player", --[[spellID]] spellID },
+                    comparisonValues =
+                    {
+                        value = 12,
+                        type = "LessThan"
+                    },
+                    value = 2,
+                },
+            },
+        },
+    },
+}
+)
