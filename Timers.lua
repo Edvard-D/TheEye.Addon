e868a03574cc9b00e5a7b29d2e138dbf99950611
EdvardXYZ <eddumansky@gmail.com>
2018-07-21 17:40:22 +0300

    Define NewElapsedTimerGetLength function

--- Timers.lua
+++ Timers.lua
@@ -38,8 +38,28 @@ function this.InputGroupCooldownTimerStart(inputGroup, remainingTime, eventName,
     end
 end
 
+local function NewElapsedTimerGetLength(inputGroup, elapsedTime)
+    local nextUpdatePoint = math.huge
+    local listeners = inputGroup.listeners
+    for i=1, #listeners do
+        listener = listeners[i]
+        if listener.comparisonValues ~= nil then
+            local value = listener.comparisonValues.value
+            if value < nextUpdatePoint and value > elapsedTime then
+                nextUpdatePoint = value
+            end
+        end
+    end
+
+    if nextUpdatePoint == math.huge then
+        return nil
+    end
+
+    return nextUpdatePoint - elapsedTime
+end
+
 function this.InputGroupElapsedTimerStart(inputGroup, elapsedTime, eventName, ...)
-    local timerLength = NewElapsedTimerGetLength(inputGroup, elapsedTime) -- @TODO
+    local timerLength = NewElapsedTimerGetLength(inputGroup, elapsedTime)
     if timerLength ~= nil then
         this.StartEventTimer(timerLength, eventName, ...)
     end
