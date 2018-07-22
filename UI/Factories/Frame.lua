733a251ef134a8b635a3abfdbee7e5947d6efcb3
EdvardXYZ <eddumansky@gmail.com>
2018-07-22 14:47:18 +0300

    Remove unnecessary value bounds check
    Since group sizes are now correctly being set instead of being left at
    0, there's no need to ensure the value is above 0.

--- UI/Factories/Frame.lua
+++ UI/Factories/Frame.lua
@@ -33,9 +33,6 @@ function this.SetDimensions(frame, parentFrame, dimensionTemplate)
 end
 
 function this:SetSizeWithEvent(width, height)
-	if width < 0.0001 then width = 0.0001 end
-	if height < 0.0001 then height = 0.0001 end
-
 	if width ~= self:GetWidth() or height ~= self:GetHeight() then
 		self:SetSize(width, height)
 		SendCustomEvent("UIOBJECT_RESIZED" , self.UIObject)
