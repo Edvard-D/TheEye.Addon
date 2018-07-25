3c3bf35061837a52c463c0b00b0ff69d7f7bc2b0
EdvardXYZ <eddumansky@gmail.com>
2018-07-25 18:38:44 +0300

    Define StateFunctionCaller.Setup function

--- UI/Components/Elements/ListenerValueChangeHandlers/StateFunctionCaller.lua
+++ UI/Components/Elements/ListenerValueChangeHandlers/StateFunctionCaller.lua
@@ -3,9 +3,56 @@ TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.StateFunctionCall
 local this = TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.StateFunctionCaller
 local inherited = TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.Base
 
+local SimpleStateSetup = TheEyeAddon.UI.Components.Elements.ValueHandlers.SimpleState.Setup
+local ValueSetterSetup = TheEyeAddon.UI.Components.Elements.ListenerGroups.ValueSetter.Setup
+
 
 --[[ #this#TEMPLATE#
 {
     #inherited#TEMPLATE#
 }
 ]]
+
+
+--[[ SETUP
+    instance
+    uiObject                    UIObject
+    listener                    #LISTENER#
+    OnStateChange               fucntion(#BOOL#)
+]]
+function this.Setup(
+    instance,
+    uiObject,
+    listener,
+    OnStateChange
+)
+
+    instance.ValueHandler = {}
+    SimpleStateSetup(
+        instance.ValueHandler,
+        uiObject,
+        instance
+    )
+
+    instance.ListenerGroup =
+    {
+        Listeners =
+        {
+            listener
+        }
+    }
+    ValueSetterSetup(
+        instance.ListenerGroup,
+        uiObject,
+        instance.ValueHandler
+    )
+
+    inherited.Setup(
+        instance,
+        uiObject,
+        instance.ValueHandler,
+        instance.ListenerGroup
+    )
+
+    instance.OnStateChange = OnStateChange
+end
