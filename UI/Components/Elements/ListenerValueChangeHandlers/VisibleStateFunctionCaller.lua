b7c9b3af97a9e6dc401b38711bb069976d8f8b10
EdvardXYZ <eddumansky@gmail.com>
2018-07-25 19:05:06 +0300

    Add VisibleStateFunctionCaller.lua file

---
+++ UI/Components/Elements/ListenerValueChangeHandlers/VisibleStateFunctionCaller.lua
@@ -0,0 +1,11 @@
+local TheEyeAddon = TheEyeAddon
+TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.VisibleStateFunctionCaller = {}
+local this = TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.VisibleStateFunctionCaller
+local inherited = TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.StateFunctionCaller
+
+
+--[[ #this#TEMPLATE#
+{
+    #inherited#TEMPLATE#
+}
+]]
