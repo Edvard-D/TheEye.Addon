d6802e223ccfd2042de5f6c7618ab96c4f12e350
EdvardXYZ <eddumansky@gmail.com>
2018-07-25 23:06:02 +0300

    Create DefaultSettings file

---
+++ DefaultSettings.lua
@@ -0,0 +1,4 @@
+TheEyeAddon.Settings =
+{
+    DisabledUIModules = {}
+}
--- Initializer.lua
+++ Initializer.lua
@@ -1,12 +1,2 @@
 TheEyeAddon.GarbageCollector:Initialize()
 TheEyeAddon.UI.Objects:Initialize()
-
-
--- Settings
-if TheEyeAddon.Settings == nil then
-    TheEyeAddon.Settings = {}
-end
-
-if TheEyeAddon.Settings.DisabledUIModules == nil then
-    TheEyeAddon.Settings.DisabledUIModules = {}
-end
