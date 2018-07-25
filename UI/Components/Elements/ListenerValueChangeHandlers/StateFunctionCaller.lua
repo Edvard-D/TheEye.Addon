b03569633a19574687e4808c22de22a8671a7dbe
EdvardXYZ <eddumansky@gmail.com>
2018-07-25 18:38:09 +0300

    Add StateFunctionCaller.lua file

---
+++ UI/Components/Elements/ListenerValueChangeHandlers/StateFunctionCaller.lua
@@ -0,0 +1,11 @@
+local TheEyeAddon = TheEyeAddon
+TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.StateFunctionCaller = {}
+local this = TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.StateFunctionCaller
+local inherited = TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.Base
+
+
+--[[ #this#TEMPLATE#
+{
+    #inherited#TEMPLATE#
+}
+]]
