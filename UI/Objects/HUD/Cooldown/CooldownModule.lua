526d42ca3570a4789d52b42a057dabb35f061a29
EdvardXYZ <eddumansky@gmail.com>
2018-07-25 23:47:59 +0300

    Add CooldownModule.lua file

---
+++ UI/Objects/HUD/Cooldown/CooldownModule.lua
@@ -0,0 +1,59 @@
+local parentKey = "GROUP_HUD_LEFT"
+
+TheEyeAddon.UI.Objects:FormatData(
+{
+    tags = { "HUD", "MODULE", "COOLDOWN" },
+    Child =
+    {
+        parentKey = parentKey,
+    },
+    DisplayData =
+    {
+        factory = TheEyeAddon.UI.Factories.Group,
+    },
+    EnabledState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [6] = true },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UIOBJECT_MODULE_SETTING_CHANGED",
+                    inputValues = { --[[uiObjectKey]] "HUD_MODULE_COOLDOWN" }, -- @TODO have Setup auto populate fields with some special character, like "#thisKey"
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "UIOBJECT_VISIBLE_CHANGED",
+                    inputValues = { --[[uiObjectKey]] parentKey },
+                    value = 4,
+                },
+            },
+        },
+    },
+    Parent =
+    {
+        ChildArranger = TheEyeAddon.UI.ChildArrangers.TopToBottom,
+        sortActionName = "SortAscending",
+        sortValueComponentName = "Cooldown", -- @TOOD
+    },
+    PriorityRank =
+    {
+        isDynamic = false,
+        ValueHandler =
+        {
+            defaultValue = 1,
+        },
+    },
+    VisibleState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [0] = true },
+        },
+    },
+}
+)
