faebd43c88308183eb1b9ce192434af9d9baf95c
EdvardXYZ <eddumansky@gmail.com>
2018-07-26 07:41:10 +0300

    Add ValueChangeNotifiers\Number.lua file

---
+++ UI/Components/Elements/ValueHandlers/ValueChangeNotifiers/Number.lua
@@ -0,0 +1,10 @@
+TheEyeAddon.UI.Components.Elements.ValueHandlers.ValueChangeNotifiers.Number = {}
+local this = TheEyeAddon.UI.Components.Elements.ValueHandlers.ValueChangeNotifiers.Number
+local inherited = TheEyeAddon.UI.Components.Elements.ValueHandlers.ValueChangeNotifiers.Base
+
+
+--[[ #this#TEMPLATE#
+{
+    #inherited#TEMPLATE#
+}
+]]
