d7cc7b5a32813b420d07b187f8ffff7205b6dc6c
EdvardXYZ <eddumansky@gmail.com>
2018-07-26 07:59:17 +0300

    Add ValueHandlers\StaticValue.lua file

---
+++ UI/Components/Elements/ValueHandlers/StaticValue.lua
@@ -0,0 +1,10 @@
+TheEyeAddon.UI.Components.Elements.ValueHandlers.StaticValue = {}
+local this = TheEyeAddon.UI.Components.Elements.ValueHandlers.StaticValue
+local inherited = TheEyeAddon.UI.Components.Elements.ValueHandlers.Base
+
+
+--[[ #this#TEMPLATE#
+{
+    #inherited#TEMPLATE#
+}
+]]
