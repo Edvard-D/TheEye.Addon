bac6ce4afa729e616d3970b5098676d8c321eef6
EdvardXYZ <eddumansky@gmail.com>
2018-07-26 07:59:31 +0300

    Define StaticValue.Setup function

--- UI/Components/Elements/ValueHandlers/StaticValue.lua
+++ UI/Components/Elements/ValueHandlers/StaticValue.lua
@@ -6,5 +6,27 @@ local inherited = TheEyeAddon.UI.Components.Elements.ValueHandlers.Base
 --[[ #this#TEMPLATE#
 {
     #inherited#TEMPLATE#
+    value = #VALUE#
 }
 ]]
+
+
+--[[ #SETUP#
+    instance
+    uiObject                    UIObject
+]]
+function this.Setup(
+    instance,
+    uiObject
+)
+
+    inherited.Setup(
+        instance,
+        uiObject,
+        nil,
+        nil,
+        nil,
+        nil,
+        instance.value
+    )
+end
