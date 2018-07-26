fcd3c04344f403399f889940901ee4e30953e041
EdvardXYZ <eddumansky@gmail.com>
2018-07-26 07:24:11 +0300

    Create ValueHandlers\ValueChangeNotifiers\Base.lua file

---
+++ UI/Components/Elements/ValueHandlers/ValueChangeNotifiers/Base.lua
@@ -0,0 +1,10 @@
+TheEyeAddon.UI.Components.Elements.ValueHandlers.ValueChangeNotifiers.Base = {}
+local this = TheEyeAddon.UI.Components.Elements.ValueHandlers.ValueChangeNotifiers.Base
+local inherited = TheEyeAddon.UI.Components.Elements.ValueHandlers.Base
+
+
+--[[ #this#TEMPLATE#
+{
+    #inherited#TEMPLATE#
+}
+]]
