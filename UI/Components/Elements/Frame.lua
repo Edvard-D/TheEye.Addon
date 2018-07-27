25f9e50684bc4f9d15386e0e5ceccad56e30f2d3
EdvardXYZ <eddumansky@gmail.com>
2018-07-27 15:07:33 +0300

    Define Elements.Frame:OnUserRegisteredChanged function

--- UI/Components/Elements/Frame.lua
+++ UI/Components/Elements/Frame.lua
@@ -39,7 +39,7 @@ function this.Setup(
     instance.UIObject = uiObject
     instance.Factory = factory
 
-    instance.OnUserRegisteredChanged = this.OnUserRegisteredChanged -- @TODO
+    instance.OnUserRegisteredChanged = this.OnUserRegisteredChanged
 
     -- NotifyBasedFunctionCaller
     instance.NotifyBasedFunctionCaller =
@@ -60,4 +60,33 @@ function this.Setup(
         "OnUserRegisteredChanged"
     )
     instance.NotifyBasedFunctionCaller:Activate()
+end
+
+local function UserRegister(self, userName)
+    if self.Users == nil then
+        self.Users = { userCount = 0 }
+    end
+
+    table.insert(self.Users, user)
+    self.Users.userCount = self.Users.userCount + 1
+    if self.Users.userCount == 1 then
+        self.UIObject.frame = self.Factory.Claim(self.UIObject, nil, self.DisplayData)
+    end
+end
+
+local function UserDeregister(self, userName)
+    table.removevalue(self.Users, user)
+    self.Users.userCount = self.Users.userCount - 1
+    if self.Users.userCount == 0 then
+        self.UIObject.frame:Release()
+        self.UIObject.frame = nil
+    end
+end
+
+function this:OnUserRegisteredChanged(isRegistered, userName)
+    if isRegistered == true then
+        UserRegister(self, userName)
+    else
+        UserDeregister(self, userName)
+    end
 end
