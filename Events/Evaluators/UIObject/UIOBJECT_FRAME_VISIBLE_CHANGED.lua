40f35b63a2d539a8da222ba0d06920450c2e60db
EdvardXYZ <eddumansky@gmail.com>
2018-07-27 21:33:16 +0300

    Create UIOBJECT_FRAME_VISIBLE_CHANGED event evaluator

---
+++ Events/Evaluators/UIObject/UIOBJECT_FRAME_VISIBLE_CHANGED.lua
@@ -0,0 +1,41 @@
+TheEyeAddon.Events.Evaluators.UIOBJECT_FRAME_VISIBLE_CHANGED = {}
+local this = TheEyeAddon.Events.Evaluators.UIOBJECT_FRAME_VISIBLE_CHANGED
+this.name = "UIOBJECT_FRAME_VISIBLE_CHANGED"
+
+
+--[[ #this#TEMPLATE#
+{
+    inputValues = { #LABEL#UIObject Key# #UIOBJECT#KEY# }
+}
+]]
+
+
+this.customEvents =
+{
+    "UIOBJECT_HIDDEN",
+    "UIOBJECT_SHOWN",
+}
+
+
+local function CalculateCurrentValue(uiObject)
+    return uiObject.frame ~= nil
+end
+
+function this:InputGroupSetup(inputGroup)
+    local uiObject = TheEyeAddon.UI.Objects.Instances[inputGroup.inputValues[1]]
+    inputGroup.currentValue = CalculateCurrentValue(uiObject)
+end
+
+function this:GetKey(event, uiObject)
+    return uiObject.key
+end
+
+function this:Evaluate(inputGroup, event, uiObject)
+    local isVisible = CalculateCurrentValue(uiObject)
+
+    print("isVisible: " .. tostring(isVisible))
+    if inputGroup.currentValue ~= isVisible then
+        inputGroup.currentValue = isVisible
+        return true, this.name, isVisible
+    end
+end
