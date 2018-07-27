b6a0a2142ee6ed5032bea2ee3fe8a26e446fb162
EdvardXYZ <eddumansky@gmail.com>
2018-07-27 15:28:09 +0300

    Create Components.Group file

---
+++ UI/Components/Group.lua
@@ -0,0 +1,10 @@
+TheEyeAddon.UI.Components.Group = {}
+local this = TheEyeAddon.UI.Components.Group
+local inherited = TheEyeAddon.UI.Components.Frame
+
+
+--[[ #this#TEMPLATE#
+{
+    #inherited#TEMPLATE#
+}
+]]
