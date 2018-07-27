596473403827fad8373fd9a711f295bc933ae7f5
EdvardXYZ <eddumansky@gmail.com>
2018-07-27 15:20:59 +0300

    Create Component\Icon file

---
+++ UI/Components/Icon.lua
@@ -0,0 +1,10 @@
+TheEyeAddon.UI.Components.Icon = {}
+local this = TheEyeAddon.UI.Components.Icon
+local inherited = TheEyeAddon.UI.Components.Frame
+
+
+--[[ #this#TEMPLATE#
+{
+    #inherited#TEMPLATE#
+}
+]]
