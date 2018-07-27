72e815cd305d89ce6d78c89e385cc52374c71bdf
EdvardXYZ <eddumansky@gmail.com>
2018-07-27 15:06:59 +0300

    Define Elements.Frame.Setup function and template

--- UI/Components/Elements/Frame.lua
+++ UI/Components/Elements/Frame.lua
@@ -4,6 +4,60 @@ local this = TheEyeAddon.UI.Components.Elements.Frame
 
 --[[ #this#TEMPLATE#
 {
-    nil
+    DisplayData =
+    {
+        DimensionTemplate =
+        {
+            #OPTIONAL#PointSettings =
+            {
+                #OPTIONAL#point = #POINT#
+                #OPTIONAL#relativePoint = #RELATIVEPOINT#
+                #OPTIONAL#offsetX = #NUMBER#
+                #OPTIONAL#offsetY = #NUMBER#
+            }
+            #OPTIONAL#width = #NUMBER#
+            #OPTIONAL#height = #NUMBER#
+        }
+        iconObjectType = 
+        iconObjectID = 
+    }
 }
 ]]
+
+
+--[[ SETUP
+    instance
+    uiObject                    UIObject
+    factory                     UI.Factories
+]]
+function this.Setup(
+    instance,
+    uiObject,
+    factory
+)
+
+    instance.UIObject = uiObject
+    instance.Factory = factory
+
+    instance.OnUserRegisteredChanged = this.OnUserRegisteredChanged -- @TODO
+
+    -- NotifyBasedFunctionCaller
+    instance.NotifyBasedFunctionCaller =
+    {
+        Listeners =
+        {
+            {
+                eventEvaluatorKey = "UIOBJECT_FRAME_USER_REGISTERED_CHANGED",
+                inputValues = { --[[uiObjectKey]] uiObject.key },
+                isInternal = true
+            }
+        }
+    }
+    NotifyBasedFunctionCallerSetup(
+        instance.NotifyBasedFunctionCaller,
+        uiObject,
+        instance,
+        "OnUserRegisteredChanged"
+    )
+    instance.NotifyBasedFunctionCaller:Activate()
+end
