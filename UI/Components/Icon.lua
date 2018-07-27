79e493fc0892cad30d9146d51448ee5e3440be95
EdvardXYZ <eddumansky@gmail.com>
2018-07-27 15:21:44 +0300

    Define Components.Icon.Setup function

--- UI/Components/Icon.lua
+++ UI/Components/Icon.lua
@@ -2,9 +2,33 @@ TheEyeAddon.UI.Components.Icon = {}
 local this = TheEyeAddon.UI.Components.Icon
 local inherited = TheEyeAddon.UI.Components.Frame
 
+local IconFactory = TheEyeAddon.UI.Factories.Icon
+
 
 --[[ #this#TEMPLATE#
 {
     #inherited#TEMPLATE#
+    DisplayData =
+    {
+        iconObjectType = #ICON#TYPE#
+        iconObjectID = #ICON#ID#
+    }
 }
 ]]
+
+
+--[[ SETUP
+    instance
+    uiObject                    UIObject
+]]
+function this.Setup(
+    instance,
+    uiObject
+)
+
+    inherited.Setup(
+        instance,
+        uiObject,
+        IconFactory
+    )
+end
