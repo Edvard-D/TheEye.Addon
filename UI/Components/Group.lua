12283cf5bba573212efeccc536767466ab61f921
EdvardXYZ <eddumansky@gmail.com>
2018-07-27 15:28:26 +0300

    Define Components.Group.Setup function

--- UI/Components/Group.lua
+++ UI/Components/Group.lua
@@ -2,9 +2,28 @@ TheEyeAddon.UI.Components.Group = {}
 local this = TheEyeAddon.UI.Components.Group
 local inherited = TheEyeAddon.UI.Components.Frame
 
+local GroupFactory = TheEyeAddon.UI.Factories.Group
+
 
 --[[ #this#TEMPLATE#
 {
     #inherited#TEMPLATE#
 }
 ]]
+
+
+--[[ SETUP
+    instance
+    uiObject                    UIObject
+]]
+function this.Setup(
+    instance,
+    uiObject
+)
+
+    inherited.Setup(
+        instance,
+        uiObject,
+        GroupFactory
+    )
+end
