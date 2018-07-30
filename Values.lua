000e8fa5ec1a82bf2e002bb781785747db11d4e5
EdvardXYZ <eddumansky@gmail.com>
2018-07-30 09:40:36 +0300

    Define Values.CooldownAlertLengthGet function

--- Values.lua
+++ Values.lua
@@ -5,6 +5,15 @@ local this = TheEyeAddon.Values
 this.castStartHideDelay = 0.5
 this.cooldownEndAlertLength = 0.75
 
+function this.CooldownAlertLengthGet()
+    local cooldownEndAlertLength = select(2, GetSpellCooldown(61304)) -- GCD
+    if cooldownEndAlertLength == 0 then
+        cooldownEndAlertLength = 0.75
+    end
+
+    return cooldownEndAlertLength
+end
+
 this.screenSize =
 {
     width = GetScreenWidth(),
