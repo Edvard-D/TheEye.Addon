4ff9e10dc7635da6da40e914597a2ee694f1c2d1
EdvardXYZ <eddumansky@gmail.com>
2018-07-31 08:58:29 +0300

    Create Components\ReadySoonAlert.lua file

---
+++ UI/Components/ReadySoonAlert.lua
@@ -0,0 +1,10 @@
+TheEyeAddon.UI.Components.ReadySoonAlert = {}
+local this = TheEyeAddon.UI.Components.ReadySoonAlert
+local inherited = TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.KeyStateFunctionCaller
+
+
+--[[ #this#TEMPLATE#
+{
+    #inherited#TEMPLATE#
+}
+]]
