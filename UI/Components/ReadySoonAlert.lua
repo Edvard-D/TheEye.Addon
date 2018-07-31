679daca4a98001d79632b6e82d1dfdb0754f761f
EdvardXYZ <eddumansky@gmail.com>
2018-07-31 08:59:49 +0300

    Define ReadySoonAlert.Setup function

--- UI/Components/ReadySoonAlert.lua
+++ UI/Components/ReadySoonAlert.lua
@@ -2,9 +2,66 @@ TheEyeAddon.UI.Components.ReadySoonAlert = {}
 local this = TheEyeAddon.UI.Components.ReadySoonAlert
 local inherited = TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.KeyStateFunctionCaller
 
+local auraFilters = TheEyeAddon.Values.auraFilters
+local EnabledStateFunctionCallerSetup = TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.EnabledStateFunctionCaller.Setup
+
 
 --[[ #this#TEMPLATE#
 {
     #inherited#TEMPLATE#
 }
 ]]
+
+
+--[[ SETUP
+    instance
+    uiObject                    UIObject
+]]
+function this.Setup(
+    instance,
+    uiObject
+)
+
+    instance.ValueHandler = { validKeys = { [2] = true } }
+
+    instance.ListenerGroup =
+    {
+        Listeners =
+        {
+            {
+                comparisonValues =
+                {
+                    floor = 0,
+                    ceiling = TheEyeAddon.Values.ReadySoonAlertLengthGet,
+                    type = "Between",
+                },
+                value = 2,
+            },
+        },
+    }
+    if auraFilters[instance.spellID] == nil then
+        instance.ListenerGroup.Listeners[1].eventEvaluatorKey = "PLAYER_SPELL_COOLDOWN_DURATION_CHANGED"
+        instance.ListenerGroup.Listeners[1].inputValues = { --[[spellID]] instance.spellID }
+    else
+        instance.ListenerGroup.Listeners[1].eventEvaluatorKey = "UNIT_AURA_DURATION_CHANGED"
+        instance.ListenerGroup.Listeners[1].inputValues = { --[[sourceUnit]] "player", --[[destUnit]] "target", --[[spellID]] instance.spellID }
+    end
+
+    inherited.Setup(
+        instance,
+        uiObject,
+        this.OnValidKey, -- @TODO
+        this.OnInvalidKey -- @TODO
+    )
+    
+    -- EnabledStateFunctionCaller
+    instance.OnEnable = this.OnEnable -- @TODO
+    instance.OnDisable = this.OnDisable -- @TODO
+
+    instance.EnabledStateFunctionCaller = {}
+    EnabledStateFunctionCallerSetup(
+        instance.EnabledStateFunctionCaller,
+        uiObject,
+        instance
+    )
+end
