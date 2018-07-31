e15f10534cead64fa2e52e66de3567cf6d2b9a3a
EdvardXYZ <eddumansky@gmail.com>
2018-07-31 09:10:18 +0300

    Refactor Comparisons to allow values to be function calls

--- Helpers/Comparisons.lua
+++ Helpers/Comparisons.lua
@@ -1,23 +1,33 @@
 TheEyeAddon.Helpers.Comparisons = {}
 this = TheEyeAddon.Helpers.Comparisons
 
+local type = type
+
+
+local function ValueGet(value)
+    if type(value) == "function" then
+        value = value()
+    end
+
+    return value
+end
 
 function this.EqualTo(value, comparisonValues)
-    return value == comparisonValues.value
+    return value == ValueGet(comparisonValues.value)
 end
 
 function this.GreaterThan(value, comparisonValues)
-    return value > comparisonValues.value
+    return value > ValueGet(comparisonValues.value)
 end
 
 function this.LessThan(value, comparisonValues)
-    return value < comparisonValues.value
+    return value < ValueGet(comparisonValues.value)
 end
 
 function this.GreaterThanEqualTo(value, comparisonValues)
-    return value >= comparisonValues.value
+    return value >= ValueGet(comparisonValues.value)
 end
 
 function this.LessThanEqualTo(value, comparisonValues)
-    return value <= comparisonValues.value
+    return value <= ValueGet(comparisonValues.value)
 end
