b49ee305f00d2b6bb1d750f9c635879a9e1e9fed
EdvardXYZ <eddumansky@gmail.com>
2018-08-12 19:09:30 +0300

    Add missing space for readability

--- UI/Components/Elements/ValueHandlers/SortedTable.lua
+++ UI/Components/Elements/ValueHandlers/SortedTable.lua
@@ -47,7 +47,7 @@ end
 -- Sort Actions
 function this:SortAscending()
     table.sort(self.value, function(a,b)
-        return a[self.sortValueComponentName].ValueHandler.value< b[self.sortValueComponentName].ValueHandler.value end)
+        return a[self.sortValueComponentName].ValueHandler.value < b[self.sortValueComponentName].ValueHandler.value end)
 end
 
 function this:SortDescending()
