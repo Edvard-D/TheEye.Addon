123e7845395207ffef2228a1e4fa86372ea3bc7f
EdvardXYZ <eddumansky@gmail.com>
2018-08-12 15:48:58 +0300

    Add FrameClaimedStateFunctionCaller component element file

---
+++ UI/Components/Elements/ListenerValueChangeHandlers/FrameClaimedStateFunctionCaller.lua
@@ -0,0 +1,60 @@
+TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.FrameClaimedStateFunctionCaller = {}
+local this = TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.FrameClaimedStateFunctionCaller
+local inherited = TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.StateFunctionCaller
+
+local EnabledStateFunctionCallerSetup = TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.EnabledStateFunctionCaller.Setup
+
+
+--[[ #this#TEMPLATE#
+{
+    #inherited#TEMPLATE#
+}
+]]
+
+
+--[[ SETUP
+    instance
+    uiObject                    UIObject
+    stateListener               { function OnClaim(), function OnRelease() }
+]]
+function this.Setup(
+    instance,
+    uiObject,
+    stateListener
+)
+
+    listener =
+    {
+        eventEvaluatorKey = "UIOBJECT_VISIBLE_CHANGED",
+        inputValues = { uiObject.key },
+        isInternal = true,
+    }
+
+    inherited.Setup(
+        instance,
+        uiObject,
+        listener,
+        stateListener,
+        "OnClaim",
+        "OnRelease"
+    )
+
+    -- EnabledStateFunctionCaller
+    instance.OnEnable = this.OnEnable
+    instance.OnDisable = this.OnDisable
+
+    instance.EnabledStateFunctionCaller = {}
+    EnabledStateFunctionCallerSetup(
+        instance.EnabledStateFunctionCaller,
+        uiObject,
+        instance
+    )
+end
+
+function this:OnEnable()
+    self:Activate()
+end
+
+function this:OnDisable()
+    self:Deactivate()
+end
