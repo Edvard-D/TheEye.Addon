5fdbe72a58c4e269a951a72724395fdf5907c12d
EdvardXYZ <eddumansky@gmail.com>
2018-08-12 17:02:51 +0300

    Add KeyStateFunctionCaller element handling
    This will allow FrameModifiers to determine their state. Note that
    derived components must define a "name" variable.

--- UI/Components/Elements/ListenerValueChangeHandlers/FrameModifier.lua
+++ UI/Components/Elements/ListenerValueChangeHandlers/FrameModifier.lua
@@ -2,6 +2,7 @@ TheEyeAddon.UI.Components.FrameModifier = {}
 local this = TheEyeAddon.UI.Components.FrameModifier
 
 local FrameStateFunctionCallerSetup = TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.FrameStateFunctionCaller.Setup
+local KeyStateFunctionCallerSetup = TheEyeAddon.UI.Components.Elements.ListenerValueChangeHandlers.KeyStateFunctionCaller.Setup
 
 
 --[[ #this#TEMPLATE#
@@ -12,7 +13,7 @@ local FrameStateFunctionCallerSetup = TheEyeAddon.UI.Components.Elements.Listene
 
 
 --[[ SETUP
-    instance                    { function Modify(frame), function Demodify(frame) }
+    instance                    { #name#STRING#, KeyStateFunctionHandler StateHandler, function Modify(frame), function Demodify(frame) }
     uiObject                    UIObject
 ]]
 function this.Setup(
@@ -32,12 +33,34 @@ function this.Setup(
         uiObject,
         instance
     )
+
+    -- StateHandler
+    KeyStateFunctionCallerSetup(
+        instance.StateHandler,
+        uiObject,
+        this.OnValidKey,
+        this.OnInvalidKey
+    )
 end
 
 function this:OnClaim()
-    self:Modify(self.UIObject.frame)
+    if self.StateHandler.state == true then
+        self:Modify(self.UIObject.frame)
+    end
 end
 
 function this:OnRelease()
+    self:Demodify(self.UIObject.frame)
+end
+
+function this:OnValidKey()
+    SendCustomEvent("UIOBJECT_COMPONENT_ENABLED", self.UIObject, self.name)
+    if self.UIObject.frame ~= nil then
+        self:Modify(self.UIObject.frame)
+    end
+end
+
+function this:OnInvalidKey()
+    SendCustomEvent("UIOBJECT_COMPONENT_DISABLED", self.UIObject, self.name)
     self:Demodify(self.UIObject.frame)
 end
