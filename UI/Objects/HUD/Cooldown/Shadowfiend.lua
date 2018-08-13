69d4354ced39749a0e23bac558677f5b6f74bbe7
EdvardXYZ <eddumansky@gmail.com>
2018-08-13 13:00:53 +0300

    Add Shadowfiend Cooldown module UIObject

---
+++ UI/Objects/HUD/Cooldown/Shadowfiend.lua
@@ -0,0 +1,71 @@
+local parentKey = "HUD_MODULE_COOLDOWN"
+local spellID = 34433
+
+TheEyeAddon.UI.Objects:FormatData(
+{
+    tags = { "HUD", "ICON", "COOLDOWN", "SPELL-34433", },
+    Child =
+    {
+        parentKey = parentKey,
+    },
+    Cooldown =
+    {
+        spellID = spellID
+    },
+    EnabledState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [2] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UIOBJECT_COMPONENT_STATE_CHANGED",
+                    inputValues = { --[[uiObjectKey]] parentKey, --[[componentName]] "VisibleState" },
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "PLAYER_TALENT_KNOWN_CHANGED",
+                    inputValues = { --[[talentID]] 21719, },
+                    value = 4,
+                },
+            },
+        },
+    },
+    Icon =
+    {
+        DisplayData =
+        {
+            DimensionTemplate = TheEyeAddon.UI.DimensionTemplates.Icon.Small,
+            iconObjectType = "SPELL",
+            iconObjectID = spellID,
+        },
+    },
+    VisibleState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [2] = true },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UIOBJECT_COMPONENT_STATE_CHANGED",
+                    inputValues = { --[[uiObject]] "#SELF#UIOBJECT#KEY#", --[[componentName]] "Cooldown" },
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "PLAYER_TOTEM_ACTIVE_CHANGED",
+                    inputValues = { --[[totemName]] "Shadowfiend", },
+                    value = 4,
+                },
+            },
+        },
+    },
+}
+)
