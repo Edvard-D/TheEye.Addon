ccce9d7a75a95ae449510e39815364138366ee0f
EdvardXYZ <eddumansky@gmail.com>
2018-08-13 15:18:03 +0300

    Add SecondaryModule UIObject

---
+++ UI/Objects/HUD/Secondary/SecondaryModule.lua
@@ -0,0 +1,60 @@
+local parentKey = "GROUP_HUD"
+
+TheEyeAddon.UI.Objects:FormatData(
+{
+    tags = { "HUD", "MODULE", "SECONDARY" },
+    Child =
+    {
+        parentKey = parentKey,
+    },
+    EnabledState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [6] = true },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UIOBJECT_MODULE_SETTING_CHANGED",
+                    inputValues = { --[[uiObjectKey]] "#SELF#UIOBJECT#KEY#" },
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "UIOBJECT_COMPONENT_STATE_CHANGED",
+                    inputValues = { --[[uiObjectKey]] parentKey, --[[componentName]] "VisibleState" },
+                    value = 4,
+                },
+            },
+        },
+    },
+    Group =
+    {
+        DisplayData =
+        {
+            DimensionTemplate =
+            {
+                PointSettings =
+                {
+                    point = "TOPLEFT",
+                    relativePoint = "TOP",
+                    offsetX = 32.5,
+                    offsetY = -5,
+                },
+            },
+        },
+        childArranger = TheEyeAddon.UI.ChildArrangers.TopToBottom,
+        sortActionName = "SortDescending",
+        sortValueComponentName = "PriorityRank",
+    },
+    VisibleState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [0] = true },
+        },
+    },
+}
+)
