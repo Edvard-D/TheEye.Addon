b2d5e330ae5e273f4e41e30fb0cd90e08fa10750
EdvardXYZ <eddumansky@gmail.com>
2018-08-13 13:45:36 +0300

    Add GladiatorsMedallion Cooldown module UIObject

---
+++ UI/Objects/HUD/Cooldown/GladiatorsMedallion.lua
@@ -0,0 +1,61 @@
+local parentKey = "HUD_MODULE_COOLDOWN"
+local spellID = 208683
+
+TheEyeAddon.UI.Objects:FormatData(
+{
+    tags = { "HUD", "ICON", "COOLDOWN", "SPELL-208683", },
+    Child =
+    {
+        parentKey = parentKey,
+    },
+    Cooldown =
+    {
+        spellID = spellID
+    },
+    EnabledState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [2] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UIOBJECT_COMPONENT_STATE_CHANGED",
+                    inputValues = { --[[uiObjectKey]] parentKey, --[[componentName]] "VisibleState" },
+                    value = 2,
+                },
+            },
+        },
+    },
+    Icon =
+    {
+        DisplayData =
+        {
+            DimensionTemplate = TheEyeAddon.UI.DimensionTemplates.Icon.Small,
+            iconObjectType = "SPELL",
+            iconObjectID = spellID,
+        },
+    },
+    VisibleState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [2] = true },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UIOBJECT_COMPONENT_STATE_CHANGED",
+                    inputValues = { --[[uiObject]] "#SELF#UIOBJECT#KEY#", --[[componentName]] "Cooldown" },
+                    value = 2,
+                },
+            },
+        },
+    },
+}
+)
