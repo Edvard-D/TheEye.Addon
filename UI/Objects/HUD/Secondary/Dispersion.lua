fa2fa5f6084cc2cad5a49cfe32586ee4a4ad757e
EdvardXYZ <eddumansky@gmail.com>
2018-08-13 15:43:59 +0300

    Add Dispersion Secondary module UIObject

---
+++ UI/Objects/HUD/Secondary/Dispersion.lua
@@ -0,0 +1,70 @@
+local parentKey = "HUD_MODULE_SECONDARY"
+local spellID = 47585
+
+TheEyeAddon.UI.Objects:FormatData(
+{
+    tags = { "HUD", "ICON", "SECONDARY", "SPELL-47585", },
+    Child =
+    {
+        parentKey = parentKey,
+    },
+    EnabledState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [2] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UIOBJECT_COMPONENT_STATE_CHANGED",
+                    inputValues = { --[[uiObjectKey]] parentKey, --[[componentName]] "VisibleState" },
+                    value = 2,
+                },
+            },
+        },
+    },
+    Icon =
+    {
+        DisplayData =
+        {
+            DimensionTemplate = TheEyeAddon.UI.DimensionTemplates.Icon.Medium,
+            iconObjectType = "SPELL",
+            iconObjectID = spellID,
+        },
+    },
+    PriorityRank =
+    {
+        isDynamic = false,
+        ValueHandler =
+        {
+            value = nil, -- @TODO
+        },
+    },
+    VisibleState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [2] = true },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "PLAYER_SPELL_COOLDOWN_DURATION_CHANGED",
+                    inputValues = { --[[spellID]] spellID, },
+                    comparisonValues =
+                    {
+                        value = 0,
+                        type = "EqualTo",
+                    },
+                    value = 2,
+                },
+            },
+        },
+    },
+}
+)
