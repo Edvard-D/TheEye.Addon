ac4b2b6d6c5cf79790dcb8d74a30d6b68433e789
EdvardXYZ <eddumansky@gmail.com>
2018-08-13 15:34:40 +0300

    Add PsychicScream Secondary module UIObject

---
+++ UI/Objects/HUD/Secondary/PsychicScream.lua
@@ -0,0 +1,80 @@
+local parentKey = "HUD_MODULE_SECONDARY"
+local spellID = 8122
+
+TheEyeAddon.UI.Objects:FormatData(
+{
+    tags = { "HUD", "ICON", "SECONDARY", "SPELL-8122", },
+    Child =
+    {
+        parentKey = parentKey,
+    },
+    EnabledState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [2] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UIOBJECT_COMPONENT_STATE_CHANGED",
+                    inputValues = { --[[uiObjectKey]] parentKey, --[[componentName]] "VisibleState" },
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "PLAYER_TALENT_KNOWN_CHANGED",
+                    inputValues = { --[[talentID]] 23375, },
+                    value = 4,
+                },
+            },
+        },
+    },
+    Icon =
+    {
+        DisplayData =
+        {
+            DimensionTemplate = TheEyeAddon.UI.DimensionTemplates.Icon.Medium,
+            iconObjectType = "SPELL",
+            iconObjectID = spellID,
+        },
+    },
+    PriorityRank =
+    {
+        isDynamic = false,
+        ValueHandler =
+        {
+            value = nil, -- @TODO
+        },
+    },
+    VisibleState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [2] = true },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "PLAYER_SPELL_COOLDOWN_DURATION_CHANGED",
+                    inputValues = { --[[spellID]] spellID, },
+                    comparisonValues =
+                    {
+                        value = 0,
+                        type = "EqualTo",
+                    },
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "UIOBJECT_COMPONENT_STATE_CHANGED",
+                    inputValues = { --[[uiObject]] "HUD_ICON_PRIMARY_SPELL-8122", --[[componentName]] "VisibleState" },
+                    value = 4,
+                },
+            },
+        },
+    },
+}
+)
