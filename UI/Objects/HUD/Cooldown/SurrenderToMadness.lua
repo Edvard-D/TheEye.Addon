7c980699d30b075c162ea51cddd033eccda3555c
EdvardXYZ <eddumansky@gmail.com>
2018-08-13 13:35:39 +0300

    Add SurrenderToMadness Cooldown module UIObject

---
+++ UI/Objects/HUD/Cooldown/SurrenderToMadness.lua
@@ -0,0 +1,71 @@
+local parentKey = "HUD_MODULE_COOLDOWN"
+local spellID = 193223
+
+TheEyeAddon.UI.Objects:FormatData(
+{
+    tags = { "HUD", "ICON", "COOLDOWN", "SPELL-193223", },
+    Child =
+    {
+        parentKey = parentKey,
+    },
+    Cooldown =
+    {
+        spellID = spellID
+    },
+    EnabledState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [6] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UIOBJECT_COMPONENT_STATE_CHANGED",
+                    inputValues = { --[[uiObjectKey]] parentKey, --[[componentName]] "VisibleState" },
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "PLAYER_TALENT_KNOWN_CHANGED",
+                    inputValues = { --[[talentID]] 21979, },
+                    value = 4,
+                },
+            },
+        },
+    },
+    Icon =
+    {
+        DisplayData =
+        {
+            DimensionTemplate = TheEyeAddon.UI.DimensionTemplates.Icon.Small,
+            iconObjectType = "SPELL",
+            iconObjectID = spellID,
+        },
+    },
+    VisibleState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [2] = true },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UIOBJECT_COMPONENT_STATE_CHANGED",
+                    inputValues = { --[[uiObject]] "#SELF#UIOBJECT#KEY#", --[[componentName]] "Cooldown" },
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "UIOBJECT_COMPONENT_STATE_CHANGED",
+                    inputValues = { --[[uiObject]] "HUD_ICON_PRIMARY_SPELL-193223", --[[componentName]] "VisibleState" },
+                    value = 4,
+                },
+            },
+        },
+    },
+}
+)
