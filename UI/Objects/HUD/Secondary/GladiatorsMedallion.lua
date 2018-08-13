b832132b3c9e1c23cd68e66c85477d358ca34721
EdvardXYZ <eddumansky@gmail.com>
2018-08-13 15:55:20 +0300

    Add GladiatorsMedallion Secondary module UIObject

---
+++ UI/Objects/HUD/Secondary/GladiatorsMedallion.lua
@@ -0,0 +1,76 @@
+local parentKey = "HUD_MODULE_SECONDARY"
+local spellID = 208683
+
+TheEyeAddon.UI.Objects:FormatData(
+{
+    tags = { "HUD", "ICON", "SECONDARY", "SPELL-208683", },
+    Child =
+    {
+        parentKey = parentKey,
+    },
+    EnabledState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [6] = true, },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "UIOBJECT_COMPONENT_STATE_CHANGED",
+                    inputValues = { --[[uiObjectKey]] parentKey, --[[componentName]] "VisibleState" },
+                    value = 2,
+                },
+                {
+                    eventEvaluatorKey = "PLAYER_TALENT_KNOWN_CHANGED",
+                    inputValues = { --[[talentID]] 3476, },
+                    value = 4,
+                },
+            },
+        },
+    },
+    Icon =
+    {
+        DisplayData =
+        {
+            DimensionTemplate = TheEyeAddon.UI.DimensionTemplates.Icon.Medium,
+            iconObjectType = "SPELL",
+            iconObjectID = spellID,
+        },
+    },
+    PriorityRank =
+    {
+        isDynamic = false,
+        ValueHandler =
+        {
+            value = nil, -- @TODO
+        },
+    },
+    VisibleState =
+    {
+        ValueHandler =
+        {
+            validKeys = { [2] = true },
+        },
+        ListenerGroup =
+        {
+            Listeners =
+            {
+                {
+                    eventEvaluatorKey = "PLAYER_SPELL_COOLDOWN_DURATION_CHANGED",
+                    inputValues = { --[[spellID]] spellID, },
+                    comparisonValues =
+                    {
+                        value = 0,
+                        type = "EqualTo",
+                    },
+                    value = 2,
+                },
+                -- @TODO Don't show unless doing PvP content
+            },
+        },
+    },
+}
+)
