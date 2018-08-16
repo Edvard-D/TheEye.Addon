c63e772ab00554838c467b0681d672f17dbc03bf
EdvardXYZ <eddumansky@gmail.com>
2018-08-16 12:03:40 +0300

    Correct events that trigger evaluation of UNIT_IN_COMBAT_CHANGED
    "UNIT_THREAT_SITUATION_UPDATE" does not update when the player is
    attacking a target dummy or when the player starts attacking a mob that
    another player already agroed.

--- Events/Evaluators/Unit/UNIT_IN_COMBAT_CHANGED.lua
+++ Events/Evaluators/Unit/UNIT_IN_COMBAT_CHANGED.lua
@@ -2,7 +2,7 @@ TheEyeAddon.Events.Evaluators.UNIT_IN_COMBAT_CHANGED = {}
 local this = TheEyeAddon.Events.Evaluators.UNIT_IN_COMBAT_CHANGED
 this.name = "UNIT_IN_COMBAT_CHANGED"
 
-local UnitThreatSituation = UnitThreatSituation
+local UnitAffectingCombat = UnitAffectingCombat
 
 
 --[[ #this#TEMPLATE#
@@ -18,26 +18,35 @@ this.reevaluateEvents =
 }
 this.gameEvents =
 {
+    "PLAYER_REGEN_ENABLED",
+    "PLAYER_REGEN_DISABLED",
     "PLAYER_TARGET_CHANGED",
-    "UNIT_THREAT_SITUATION_UPDATE",
+    "UNIT_COMBAT",
 }
 
 
 local function CalculateCurrentValue(inputValues)
-    return UnitThreatSituation(inputValues[1]) ~= nil
+    return UnitAffectingCombat(inputValues[1])
 end
 
 function this:InputGroupSetup(inputGroup)
     inputGroup.currentValue = CalculateCurrentValue(inputGroup.inputValues)
 end
 
-function this:GetKey(event, unit) -- Only called on UNIT_THREAT_SITUATION_UPDATE
+function this:GetKey(event, ...)
+    local unit
+    if event == "UNIT_COMBAT" then
+        unit = select(1, ...)
+    elseif event == "PLAYER_REGEN_ENABLED" or event == "PLAYER_REGEN_DISABLED" then
+        unit = "player"
+    end
+    
     return unit
 end
 
 function this:Evaluate(inputGroup, event)
     local inCombat = CalculateCurrentValue(inputGroup.inputValues)
-
+    
     if inputGroup.currentValue ~= inCombat then
         inputGroup.currentValue = inCombat
         return true, this.name, inCombat
