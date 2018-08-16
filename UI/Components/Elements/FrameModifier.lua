75587cd6f4cd1b691163079fe0f3b43521a547a9
EdvardXYZ <eddumansky@gmail.com>
2018-08-16 13:28:16 +0300

    Correct when FrameModifiers change their state
    It's necessary for FrameModifiers to change their state before calling
    Modify/Demodify since their state is part of determining the
    VisibleState of many Primary icons. This should be refactored.

--- UI/Components/Elements/FrameModifier.lua
+++ UI/Components/Elements/FrameModifier.lua
@@ -47,27 +47,37 @@ function this.Setup(
     instance:Activate()
 end
 
+--[[ @REFACTOR The state shouldn't change until Modify/Demodify is called, but right now it's
+    necessary since frames are created by VisibleState, but the VisibleState of many Primary icons
+    depends on the FrameModifier's state to be true. Right now the order of events is:
+    * FrameModifier.state = true
+    * VisibleState has valid key
+    * Frame claimed calling OnClaim
+    * FrameModifier calls Modify
+]]
 local function CallModify(self)
-    if self.state ~= true
-        and self.ValueHandler.state == true
-        and self.UIObject.Frame ~= nil
-        and self.UIObject.Frame.instance ~= nil
-        then
-        self:Modify(self.UIObject.Frame.instance)
-        self.state = true
-        SendCustomEvent("UIOBJECT_COMPONENT_STATE_CHANGED", self.UIObject, self.name)
+    if self.ValueHandler.state == true then
+        if self.UIObject.Frame ~= nil and self.UIObject.Frame.instance ~= nil then
+         self:Modify(self.UIObject.Frame.instance)
+        end
+
+        if self.state ~= true then
+            self.state = true
+            SendCustomEvent("UIOBJECT_COMPONENT_STATE_CHANGED", self.UIObject, self.name)
+        end
     end
 end
 
 local function CallDemodify(self)
-    if self.state ~= false
-        and self.ValueHandler.state == false
-        and self.UIObject.Frame ~= nil
-        and self.UIObject.Frame.instance ~= nil
-        then
-        self:Demodify(self.UIObject.Frame.instance)
-        self.state = false
-        SendCustomEvent("UIOBJECT_COMPONENT_STATE_CHANGED", self.UIObject, self.name)
+    if self.ValueHandler.state == false then
+        if self.UIObject.Frame ~= nil and self.UIObject.Frame.instance ~= nil then
+         self:Demodify(self.UIObject.Frame.instance)
+        end
+        
+        if self.state ~= false then
+            self.state = false
+            SendCustomEvent("UIOBJECT_COMPONENT_STATE_CHANGED", self.UIObject, self.name)
+        end
     end
 end
 
