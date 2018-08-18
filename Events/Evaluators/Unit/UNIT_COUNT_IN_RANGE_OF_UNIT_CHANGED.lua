2fae2832fd56972f20f6494eaddb6db42e91a043
EdvardXYZ <eddumansky@gmail.com>
2018-08-18 09:16:25 +0300

    Define template

--- Events/Evaluators/Unit/UNIT_COUNT_IN_RANGE_OF_UNIT_CHANGED.lua
+++ Events/Evaluators/Unit/UNIT_COUNT_IN_RANGE_OF_UNIT_CHANGED.lua
@@ -1,3 +1,14 @@
 TheEyeAddon.Events.Evaluators.UNIT_COUNT_IN_RANGE_OF_UNIT_CHANGED = {}
 local this = TheEyeAddon.Events.Evaluators.UNIT_COUNT_IN_RANGE_OF_UNIT_CHANGED
 this.name = "UNIT_COUNT_IN_RANGE_OF_UNIT_CHANGED"
+
+
+--[[ #this#TEMPLATE#
+{
+    inputValues =
+    {
+        #LABEL#Unit# #UNIT#
+        #LABEL#Range# #INT#
+    }
+}
+]]
