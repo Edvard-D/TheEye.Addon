7a7d25e2e40a0cd1b126aca04c8db97cc24813df
EdvardXYZ <eddumansky@gmail.com>
2018-08-18 09:15:49 +0300

    Add UNIT_COUNT_IN_RANGE_OF_UNIT_CHANGED event evaluator

---
+++ Events/Evaluators/Unit/UNIT_COUNT_IN_RANGE_OF_UNIT_CHANGED.lua
@@ -0,0 +1,3 @@
+TheEyeAddon.Events.Evaluators.UNIT_COUNT_IN_RANGE_OF_UNIT_CHANGED = {}
+local this = TheEyeAddon.Events.Evaluators.UNIT_COUNT_IN_RANGE_OF_UNIT_CHANGED
+this.name = "UNIT_COUNT_IN_RANGE_OF_UNIT_CHANGED"
