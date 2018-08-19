cd7fe116d7adbf054dd83b2200334e6d436dcff7
EdvardXYZ <eddumansky@gmail.com>
2018-08-19 14:19:12 +0300

    Add debug logging manager
    Instead of using "print", functions should now call Debug.LogEntryAdd
    to register a log entry. The Debug manager is currently changed by the
    user using slash commands registered in Debug.Initialize.

---
+++ Debug.lua
@@ -0,0 +1,114 @@
+TheEyeAddon.Debug = {}
+local this = TheEyeAddon.Debug
+
+local editBox
+local frame
+local isEnabled
+local logs
+local table = table
+
+
+function this.Initialize()
+    frame = CreateFrame("Frame", nil, UIParent,"BasicFrameTemplate")
+    frame:SetSize(500, 300)
+    frame:SetPoint("TOP")
+    frame:Hide()
+
+    frame.ScrollFrame = CreateFrame("ScrollFrame", nil, frame, "InputScrollFrameTemplate")
+    frame.ScrollFrame:SetPoint("TOPLEFT", 8, -30)
+    frame.ScrollFrame:SetPoint("BOTTOMRIGHT", -12, 9)
+
+    editBox = frame.ScrollFrame.EditBox
+    editBox:SetFontObject("ChatFontNormal")
+    editBox:SetAllPoints(true)
+    editBox:SetWidth(frame.ScrollFrame:GetWidth())
+
+    TheEyeAddon.SlashCommands.HandlerRegister(this, "Debug")
+    TheEyeAddon.SlashCommands.FunctionRegister("Debug", "Enable")
+    TheEyeAddon.SlashCommands.FunctionRegister("Debug", "Disable")
+    TheEyeAddon.SlashCommands.FunctionRegister("Debug", "LogsClear")
+    TheEyeAddon.SlashCommands.FunctionRegister("Debug", "LogsGet")
+
+    this.Enable()
+end
+
+function this.Enable()
+    if isEnable ~= true then
+        isEnabled = true
+    end
+end
+
+function this.Disable()
+    if isEnable ~= false then
+        isEnabled = false
+    end
+end
+
+function this.LogEntryAdd(fileName, functionName, uiObject, component, ...)
+    if isEnabled == true then
+        local logEntry =
+        {
+            ["fileName"] = fileName,
+            ["functionName"] = functionName,
+            ["UIObject"] = uiObject,
+            ["Component"] = component,
+            ["values"] = { ... },
+        }
+        if logs == nil then
+            logs = {}
+        end
+        table.insert(logs, logEntry)
+    else
+        print("ERROR: " .. fileName .. " function " .. functionName
+            .. " calling LogEntryAdd while logging has been turned off. Add a state check.")
+    end
+end
+
+function this.LogsClear()
+    for i = #logs, 1, -1 do
+        logs[i] = nil
+    end
+    editBox:SetText("")
+    frame:Hide()
+end
+
+local function LogValueFormat(logValue)
+    if logValue == nil then 
+        return "\t"
+    else
+        return table.concat({ logValue, "\t" })
+    end
+end
+
+local function LogEntryFormat(entryPosition, logEntry)
+    local formattedLogEntry = {}
+    table.insert(formattedLogEntry, LogValueFormat(entryPosition))
+    table.insert(formattedLogEntry, LogValueFormat(logEntry.fileName))
+    table.insert(formattedLogEntry, LogValueFormat(logEntry.functionName))
+    table.insert(formattedLogEntry, LogValueFormat(logEntry.UIObject.key))
+    table.insert(formattedLogEntry, LogValueFormat(logEntry.Component.key))
+
+    local values = logEntry.values
+    for i = 1, #values do
+        if i ~= #values then
+            table.insert(formattedLogEntry, LogValueFormat(values[i]))
+        else
+            table.insert(formattedLogEntry, values[i])
+        end
+    end
+
+    table.insert(formattedLogEntry, "\n")
+    return formattedLogEntry
+end
+
+function this.LogsGet()
+    local formattedLogs = {}
+    for i = 1, #logs do
+        table.insertarray(formattedLogs, LogEntryFormat(i, logs[i]))
+    end
+
+    editBox:SetText(table.concat(formattedLogs))
+    frame:Show()
+    editBox:HighlightText()
+    editBox:SetFocus(true)
+end
--- Initializer.lua
+++ Initializer.lua
@@ -1,3 +1,4 @@
+TheEyeAddon.Debug:Initialize()
 TheEyeAddon.SlashCommands:Initialize()
 TheEyeAddon.GarbageCollector:Initialize()
 TheEyeAddon.UI.Objects:Initialize()
