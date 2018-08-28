754ec196a774e9413cc279d3b4e0211d17c242f1
EdvardXYZ <eddumansky@gmail.com>
2018-08-28 19:54:15 +0300

    Remove unnecessary arguments and function calls from FontString.Create

--- UI/Factories/FontString.lua
+++ UI/Factories/FontString.lua
@@ -2,12 +2,10 @@ TheEyeAddon.UI.Factories.FontString = {}
 local this = TheEyeAddon.UI.Factories.FontString
 
 
-function this.Create(parentFrame, layer, text, fontTemplate)
+function this.Create(parentFrame, layer, fontTemplate)
 	local instance = parentFrame:CreateFontString(nil, layer)
 	
 	fontTemplate.SetFont(instance)
-	instance:SetText(text)
-	instance:SetPoint("CENTER", parentFrame, "CENTER", 0, 0)
 	
 	return instance
 end
