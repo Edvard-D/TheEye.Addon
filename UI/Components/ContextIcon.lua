6f9160ace385e6bb6def453a0d88c513cbaaec3c
EdvardXYZ <eddumansky@gmail.com>
2018-09-02 14:05:31 +0300

    Add support for text display to ContextIcons

--- UI/Components/ContextIcon.lua
+++ UI/Components/ContextIcon.lua
@@ -2,8 +2,11 @@ TheEyeAddon.UI.Components.ContextIcon = {}
 local this = TheEyeAddon.UI.Components.ContextIcon
 local inherited = TheEyeAddon.UI.Components.FrameModifier
 
-local ContextIconDimensionTemplate = TheEyeAddon.Values.DimensionTemplates.Icon.Context
+local DimensionTemplate = TheEyeAddon.Values.DimensionTemplates.Icon.Context
+local FontStringCreate = TheEyeAddon.UI.Factories.FontString.Create
+local FontTemplate = TheEyeAddon.Values.FontTemplates.Icon.Context
 local FrameClaim = TheEyeAddon.Managers.FramePools.FrameClaim
+local NotifyBasedFunctionCallerSetup = TheEyeAddon.UI.Components.Elements.ListenerGroups.NotifyBasedFunctionCaller.Setup
 local TextureCreate = TheEyeAddon.UI.Factories.Texture.Create
 local TextureFileIDGet = TheEyeAddon.Helpers.Files.TextureFileIDGet
 
@@ -13,6 +16,7 @@ local TextureFileIDGet = TheEyeAddon.Helpers.Files.TextureFileIDGet
     #inherited#TEMPLATE#
     iconObjectType = #ICON#TYPE#
     iconObjectID = #ICON#ID#
+    #OPTIONAL#TextValueListenerGroup = TheEyeAddon.UI.Components.Elements.ListenerGroups.NotifyBasedFunctionCaller#TEMPLATE#
 }
 ]]
 
@@ -32,18 +36,46 @@ function this.Setup(
         "context",
         "creator"
     )
+
+    -- TextValueListenerGroup
+    if instance.TextValueListenerGroup ~= nil then
+        instance.OnNotify = this.OnNotify
+
+        NotifyBasedFunctionCallerSetup(
+            instance.TextValueListenerGroup,
+            instance,
+            "OnNotify"
+        )
+    end
 end
 
 function this:Modify(frame)
-    frame.context = FrameClaim(self.UIObject, "Frame", frame, nil, ContextIconDimensionTemplate)
-    
-    frame.context.background = frame.context.background or TextureCreate(frame.context, "BACKGROUND")
+    frame.context = FrameClaim(self.UIObject, "Frame", frame, nil, DimensionTemplate)
+    self.instance = frame.context
+
+    self.instance.background = self.instance.background or TextureCreate(self.instance, "BACKGROUND")
     self.iconTextureFileID = self.iconTextureFileID or TextureFileIDGet(self.iconObjectType, self.iconObjectID)
-    frame.context.background:TextureSet(self.iconTextureFileID)
+    self.instance.background:TextureSet(self.iconTextureFileID)
+
+    if self.TextValueListenerGroup ~= nil then
+        self.instance.text = self.instance.text or FontStringCreate(self.instance)
+        self.instance.text:StyleSet("OVERLAY", FontTemplate, "CENTER")
+        self.TextValueListenerGroup:Activate()
+    end
 end
 
 function this:Demodify(frame)
+    if self.TextValueListenerGroup ~= nil then
+        self.TextValueListenerGroup:Deactivate()
+        self.instance.text:SetText(nil)
+    end
+
     frame.context.background:TextureSet(nil)
     frame.context:Release()
     frame.context = nil
+    self.instance = nil
+end
+
+function this:OnNotify(event, value)
+    self.instance.text:SetText(tostring(value))
 end
