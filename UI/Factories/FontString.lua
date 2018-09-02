485451e99e7bfde35843d21bfca2365f97fa8af1
EdvardXYZ <eddumansky@gmail.com>
2018-09-02 13:00:12 +0300

    Refactor font style setting of FontString into a separate function

--- UI/Factories/FontString.lua
+++ UI/Factories/FontString.lua
@@ -2,10 +2,15 @@ TheEyeAddon.UI.Factories.FontString = {}
 local this = TheEyeAddon.UI.Factories.FontString
 
 
-function this.Create(parentFrame, layer, fontTemplate)
-	local instance = parentFrame:CreateFontString(nil, layer)
+function this.Create(parentFrame)
+	local instance = parentFrame:CreateFontString()
 	
-	fontTemplate.SetFont(instance)
+	instance.StyleSet = this.StyleSet
 	
 	return instance
+end
+
+function this:StyleSet(layer, fontTemplate)
+	self:SetDrawLayer(layer)
+	fontTemplate.SetFont(self)
 end
