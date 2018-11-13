c0b544e8d3a686eeb78fe5cd06fd0efbc3ab3b3f
EdvardXYZ <eddumansky@gmail.com>
2018-11-13 16:04:10 +0200

    Define function to add action values

--- Helpers/Actions.lua
+++ Helpers/Actions.lua
@@ -1,2 +1,11 @@
 TheEyeAddon.Managers.Actions = {}
 local this = TheEyeAddon.Managers.Actions
+
+
+local table = table
+this.values = {}
+
+
+function this.Add(action)
+    table.insert(this.values, action)
+end
