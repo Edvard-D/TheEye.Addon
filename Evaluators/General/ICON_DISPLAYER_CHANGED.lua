e5f0f5c6f283b480855f99c6b423d6e4b942b4a9
EdvardXYZ <eddumansky@gmail.com>
2018-12-08 16:14:48 +0200

    Create ICON_DISPLAYER_CHANGED evaluator

---
+++ Evaluators/General/ICON_DISPLAYER_CHANGED.lua
@@ -0,0 +1,36 @@
+TheEyeAddon.Evaluators.ICON_DISPLAYER_CHANGED = {}
+local this = TheEyeAddon.Evaluators.ICON_DISPLAYER_CHANGED
+
+local DisplayerGet = TheEyeAddon.Managers.Icons.DisplayerGet
+local table = table
+
+
+--[[ #this#TEMPLATE#
+{
+    inputValues =
+    {
+        #LABEL#Spell ID# #SPELL#ID#
+        #LABEL#Displayer ID# #UIOBJECT#ID#
+    }
+}
+]]
+
+
+this.customEvents =
+{
+    "ICON_DISPLAYER_CHANGED",
+}
+
+
+function this:InputGroupSetup(inputGroup)
+    inputGroup.currentValue = DisplayerGet(inputGroup.inputValues[1]) == inputGroup.inputValues[2]
+end
+
+function this:Evaluate(inputGroup, event, iconID, displayerID)
+    local isDisplayer = displayerID == inputGroup.inputValues[2]
+    
+    if inputGroup.currentValue ~= isDisplayer then
+        inputGroup.currentValue = isDisplayer
+        return true, this.key
+    end
+end
