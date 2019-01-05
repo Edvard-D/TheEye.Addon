bd18c1527bebafa5cca77830191c7e45aeb063b4
EdvardXYZ <eddumansky@gmail.com>
2019-01-05 14:39:23 +0200

    Assign listeners to cooldown icons

--- UI/Components/Groups/CooldownGroup.lua
+++ UI/Components/Groups/CooldownGroup.lua
@@ -2,6 +2,8 @@ TheEyeAddon.UI.Components.CooldownGroup = {}
 local this = TheEyeAddon.UI.Components.CooldownGroup
 local inherited = TheEyeAddon.UI.Components.IconGroup
 
+local GetPropertiesOfType = TheEyeAddon.Managers.Icons.GetPropertiesOfType
+
 
 --[[ #this#TEMPLATE#
 {
@@ -21,14 +23,45 @@ function this.Setup(
         instance
     )
 
-    local icons = instance.Icons    
+    local icons = instance.Icons
+    local priorityDisplayers = instance.PriorityDisplayers
     for i = 1, #icons do
         local icon = icons[i]
+        local iconUIObject = icon.UIObject
+        local validKeys = iconUIObject.VisibleState.ValueHandler.validKeys
+        local value = 1
+
+        local CATEGORY = GetPropertiesOfType(icon, "CATEGORY")
         local OBJECT_ID = GetPropertiesOfType(icon, "OBJECT_ID")
 
-        icon.UIObject.Cooldown =
-        {
-            spellID = OBJECT_ID.value,
-        }
+
+        -- UIOBJECT_COMPONENT_STATE_CHANGED (Cooldown)
+        iconUIObject.Cooldown = { spellID = OBJECT_ID.value, }
+
+        value = value * 2
+        validKeys[value] = true
+
+        table.insert(iconUIObject.VisibleState.ListenerGroup.Listeners,
+            {
+                eventEvaluatorKey = "UIOBJECT_COMPONENT_STATE_CHANGED",
+                inputValues = { --[[uiObject]] "#SELF#UIOBJECT#KEY#", --[[componentName]] "Cooldown" },
+                value = value,
+            }
+        )
+
+        -- ICON_DISPLAYER_CHANGED
+        for i = 1, #priorityDisplayers do
+            value = value * 2
+
+            if priorityDisplayers[i] == "ROTATION" and CATEGORY.value == "DAMAGE" then
+                table.insert(iconUIObject.VisibleState.ListenerGroup.Listeners,
+                    {
+                        eventEvaluatorKey = "ICON_DISPLAYER_CHANGED",
+                        inputValues = { --[[spellID]] OBJECT_ID.value, --[[displayerID]] priorityDisplayers[i] },
+                        value = value,
+                    }
+                )
+            end
+        end
     end
 end
