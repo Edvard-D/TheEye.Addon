dfc3d4a3d95ef08e1d65682d021ca27d39abce60
EdvardXYZ <eddumansky@gmail.com>
2019-01-05 22:18:03 +0200

    Add SituationalGroup file

---
+++ UI/Components/Groups/SituationalGroup.lua
@@ -0,0 +1,10 @@
+TheEyeAddon.UI.Components.SituationalGroup = {}
+local this = TheEyeAddon.UI.Components.SituationalGroup
+local inherited = TheEyeAddon.UI.Components.PriorityGroup
+
+
+--[[ #this#TEMPLATE#
+{
+    #inherited#TEMPLATE#
+}
+]]
