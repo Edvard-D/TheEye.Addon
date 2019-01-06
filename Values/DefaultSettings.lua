8096685c4389a247b1ab37c31f3f4f5f5224d102
EdvardXYZ <eddumansky@gmail.com>
2019-01-06 16:12:11 +0200

    Replace Active module files with default setting instance

--- Values/DefaultSettings.lua
+++ Values/DefaultSettings.lua
@@ -9,4 +9,67 @@ TheEyeAddon.Managers.Settings.Account.Default =
 
 TheEyeAddon.Managers.Settings.Character.Default =
 {
+    IconGroups =
+    {
+        {
+            instanceID = "0000001",
+            type = "ACTIVE",
+            Filters =
+            {
+                {
+                    {
+                        type = "CATEGORY",
+                        value = "DAMAGE",
+                        subvalue = "MINION",
+                    },
+                    {
+                        type = "CATEGORY",
+                        value = "DAMAGE",
+                        subvalue = "TOTEM",
+                    },
+                },
+                {
+                    {
+                        type = "AURA_APPLIED",
+                    },
+                    {
+                        type = "CATEGORY",
+                        value = "DEFENSIVE",
+                    },
+                },
+                {
+                    {
+                        type = "CATEGORY",
+                        value = "DAMAGE",
+                        subvalue = "BUFF",
+                    },
+                    {
+                        type = "USAGE_RATE",
+                        value = 1,
+                    },
+                    {
+                        type = "USAGE_RATE",
+                        value = 2,
+                    },
+                    {
+                        type = "USAGE_RATE",
+                        value = 3,
+                    },
+                    {
+                        type = "USAGE_RATE",
+                        value = 4,
+                    },
+                },
+            },
+            Group =
+            {
+                childArranger = "TopToBottom",
+                sortActionName = "SortDescending",
+                sortValueComponentName = "PriorityRank",
+            },
+            grouper = "LEFT",
+            grouperPriority = 2,
+            IconDimensions = TheEyeAddon.Values.DimensionTemplates.Icon.Medium,
+        },
+    },
 }
