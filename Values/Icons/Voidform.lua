7f0f1d3c6434f7538b08c895afef8091ad45ddc7
EdvardXYZ <eddumansky@gmail.com>
2019-01-06 12:50:26 +0200

    Add Voidform icon property file

---
+++ Values/Icons/Voidform.lua
@@ -0,0 +1,39 @@
+TheEyeAddon.Managers.Icons.Add(
+{
+    PriorityRank =
+    {
+        validKeys = { [0] = 12, }
+    },
+    properties =
+    {
+        {
+            type = "AFFECTED",
+            value = "SINGLE",
+        },
+        {
+            type = "AURA",
+        },
+        {
+            type = "CAST_TYPE",
+            value = "TRIGGERED",
+        },
+        {
+            type = "CATEGORY",
+            value = "DAMAGE",
+            subvalue = "BUFF",
+        },
+        {
+            type = "OBJECT_ID",
+            value = 194249,
+        },
+        {
+            type = "OBJECT_TYPE",
+            value = "SPELL",
+        },
+        {
+            type = "USAGE_RATE",
+            value = 4,
+        },
+    },
+}
+)
