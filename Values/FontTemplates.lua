9b0a2bade20eff99e65c41b36e5e32228d62570a
EdvardXYZ <eddumansky@gmail.com>
2019-01-16 11:54:15 +0200

    Standardize value declaration style

--- Values/FontTemplates.lua
+++ Values/FontTemplates.lua
@@ -2,19 +2,18 @@ TheEyeAddon.Values.FontTemplates = {}
 local this = TheEyeAddon.Values.FontTemplates
 
 
-this.Icon = {}
-local Icon = this.Icon
-
-Icon.Default =
+this.Icon =
 {
-    font = "Fonts\\FRIZQT__.TTF",
-    size = 25,
-    flags = "OUTLINE",
-}
-
-Icon.Context =
-{
-    font = "Fonts\\FRIZQT__.TTF",
-    size = 17.5,
-    flags = "OUTLINE",
+    Context =
+    {
+        font = "Fonts\\FRIZQT__.TTF",
+        size = 17.5,
+        flags = "OUTLINE",
+    },
+    Default =
+    {
+        font = "Fonts\\FRIZQT__.TTF",
+        size = 25,
+        flags = "OUTLINE",
+    },
 }
