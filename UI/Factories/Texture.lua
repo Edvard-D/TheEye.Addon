4e227bae0fbbfcb57dd9c534e465a59b67233c47
EdvardXYZ <eddumansky@gmail.com>
2019-09-03 13:18:56 +0300

    Add blend mode setting

--- UI/Factories/Texture.lua
+++ UI/Factories/Texture.lua
@@ -2,9 +2,13 @@ TheEyeAddon.UI.Factories.Texture = {}
 local this = TheEyeAddon.UI.Factories.Texture
 
 
-function this.Create(parentFrame, layer)
+
+function this.Create(parentFrame, layer, blendMode)
 	instance = parentFrame:CreateTexture(nil, layer)
 	instance.TextureSet = this.TextureSet
+	blendMode = blendMode or "DISABLE"
+	instance:SetBlendMode(blendMode)
+
 	return instance
 end
 
