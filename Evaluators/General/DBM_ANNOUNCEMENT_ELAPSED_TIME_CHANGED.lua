0e51dc00a0e498363f6da183e725c85e3141adaa
EdvardXYZ <eddumansky@gmail.com>
2019-09-08 16:17:02 +0300

    Add DBM_ANNOUNCEMENT_ELAPSED_TIME_CHANGED evaluator

---
+++ Evaluators/General/DBM_ANNOUNCEMENT_ELAPSED_TIME_CHANGED.lua
@@ -0,0 +1,61 @@
+TheEyeAddon.Evaluators.DBM_ANNOUNCEMENT_ELAPSED_TIME_CHANGED = {}
+local this = TheEyeAddon.Evaluators.DBM_ANNOUNCEMENT_ELAPSED_TIME_CHANGED
+
+local GetTime = GetTime
+local InputGroupElapsedTimerStart = TheEyeAddon.Helpers.Timers.InputGroupElapsedTimerStart
+local lastEventData
+local lastEventTimeStamp = 0
+local select = select
+
+
+--[[ #this#TEMPLATE#
+{
+    inputValues = nil
+}
+]]
+
+
+this.customEvents =
+{
+    "DBM_ANNOUNCEMENT_ELAPSED_TIMER_END"
+}
+this.dbmEvents =
+{
+    "DBM_Announce",
+}
+
+
+local function TimerStart(inputGroup, elapsedTime)
+    InputGroupElapsedTimerStart(inputGroup, elapsedTime, "DBM_ANNOUNCEMENT_ELAPSED_TIMER_END", inputGroup.eventData)
+end
+
+function this:InputGroupSetup(inputGroup)
+    inputGroup.currentValue = math.huge
+end
+
+function this:GetKey()
+    return "default"
+end
+
+function this:Evaluate(inputGroup, event, ...)
+    if event == "DBM_Announce" then
+        lastEventTimeStamp = GetTime()
+        lastEventData = 
+        {
+            message = select(1, ...),
+            iconFileID = select(2, ...),
+            type = select(3, ...),
+            spellID = select(4, ...),
+            modID = select(5, ...),
+        }
+        inputGroup.eventData = lastEventData
+    end
+
+    local elapsedTime = GetTime() - lastEventTimeStamp
+
+    if elapsedTime ~= inputGroup.currentValue then
+        inputGroup.currentValue = elapsedTime
+        TimerStart(inputGroup, elapsedTime)
+        return true, this.key
+    end
+end
