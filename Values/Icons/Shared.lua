e3a59977718935c6bed5d30762a0ab11529a02dc
EdvardXYZ <eddumansky@gmail.com>
2019-09-10 21:32:14 +0300

    Add Drums of Fury

--- Values/Icons/Shared.lua
+++ Values/Icons/Shared.lua
@@ -56,6 +56,35 @@ TheEyeAddon.Managers.Icons.Add("SHARED",
     },
 })
 
+-- Drums of Fury
+TheEyeAddon.Managers.Icons.Add("SHARED",
+{
+    PriorityRank =
+    {
+        validKeys = { [0] = 0, }
+    },
+    properties =
+    {
+        {
+            type = "CAST_TYPE",
+            value = "TRIGGERED",
+        },
+        {
+            type = "CATEGORY",
+            value = "BUFF",
+            subvalue = "POWER",
+        },
+        {
+            type = "OBJECT_ID",
+            value = 178207,
+        },
+        {
+            type = "OBJECT_TYPE",
+            value = "SPELL",
+        },
+    },
+})
+
 -- Drums of Rage
 TheEyeAddon.Managers.Icons.Add("SHARED",
 {
