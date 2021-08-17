deafea30764b9b7c2c72461d7ff329ee34ca57e1
EdvardXYZ <eddumansky@gmail.com>
2021-08-17 12:25:42 +0300

    Implement CurrentSpellsGet function

--- Helpers/Spells.lua
+++ Helpers/Spells.lua
@@ -1,2 +1,29 @@
 TheEye.Core.Helpers.Spells = {}
 local this = TheEye.Core.Helpers.Spells
+
+local FindSpellOverrideByID = FindSpellOverrideByID
+local GetSpellBookItemInfo = GetSpellBookItemInfo
+local GetSpellTabInfo = GetSpellTabInfo
+local IsPassiveSpell = IsPassiveSpell
+local table = table
+
+
+function this.CurrentSpellsGet(mustBeUseable)
+    local currentSpells = {}
+
+    for i = 2, 3 do
+        _, _, offset, entryCount, _, _ = GetSpellTabInfo(i)
+
+        for j = offset + 1, offset + entryCount do
+            spellType, spellID = GetSpellBookItemInfo(j, "spell")
+            spellID = FindSpellOverrideByID(spellID)
+            
+            if IsPassiveSpell(spellID) == false
+                    and (spellType == "SPELL" or (mustBeUseable == false and spellType == "FUTURESPELL")) then
+                table.insert(currentSpells, spellID)
+            end
+        end
+    end
+
+    return currentSpells
+end
